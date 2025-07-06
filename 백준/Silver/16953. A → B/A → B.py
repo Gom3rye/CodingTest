b2bbf465a39a5aef9a224ec835fe3a73b7783660
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-06 19:41:31 +0900

    [Silver II] Title: A → B, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,30 +1,17 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
 def solution():
-    a, b = map(int, input().split())  # A → B
-    def bfs():
-        q = deque([b])
-        distance = {b: 1}  # 시작 거리 1
-        while q:
-            num = q.popleft()
-            if num == a:
-                return distance[num]
-
-            # 연산 1: 끝이 1이면 (x - 1) // 10
-            if num % 10 == 1:
-                next_num = (num - 1) // 10
-                if next_num >= a and next_num not in distance:
-                    distance[next_num] = distance[num] + 1
-                    q.append(next_num)
-            # 연산 2: 짝수이면 //2
-            if num % 2 == 0:
-                next_num = num // 2
-                if next_num >= a and next_num not in distance:
-                    distance[next_num] = distance[num] + 1
-                    q.append(next_num)
-        return -1
-    print(bfs())
-
+    a, b = map(int, input().split()) # a를 b로 바꾸기
+    count = 1
+    while b != a:
+        if b%10 == 1:
+            b = b//10
+            count += 1
+        elif b%2 == 0 and b != 0:
+            b = b//2
+            count += 1
+        else:
+            count = -1
+            break
+    print(count)
 solution()
