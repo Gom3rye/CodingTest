5fdecced21216071281b39e68ef50ad306d40489
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-07 13:45:45 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 def dist(a, b):
     return sum(x != y for x, y in zip(a, b))
 
@@ -5,19 +8,20 @@ def solution(begin, target, words):
     if target not in words:
         return 0
 
-    min_count = [float('inf')]
+    min_count = int(1e9)
     visited = [False] * len(words)
 
-    def dfs(current, depth):
+    def dfs(current, level):
+        nonlocal min_count
         if current == target:
-            min_count[0] = min(min_count[0], depth)
-            return
+            min_count = min(min_count, level)
+            return 
 
         for i, word in enumerate(words):
             if not visited[i] and dist(current, word) == 1:
                 visited[i] = True
-                dfs(word, depth + 1)
+                dfs(word, level+1)
                 visited[i] = False  # 백트래킹
-
+                
     dfs(begin, 0)
-    return min_count[0] if min_count[0] != float('inf') else 0
+    return min_count
