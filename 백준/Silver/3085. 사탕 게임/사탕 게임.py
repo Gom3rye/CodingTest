939a564da749faa095f060cfa5126eea36f8185e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-08 20:28:43 +0900

    [Silver II] Title: 사탕 게임, Time: 1428 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -4,8 +4,9 @@ def solution():
     # 가장 긴 연속 부분(행 or 열) 고르기
     n = int(input())
     candies = [list(map(str, input().strip())) for _ in range(n)]
+    max_candies = 0
     def count_candies():
-        max_candies = 0
+        nonlocal max_candies
         # 행 탐색
         for i in range(n):
             candy = 1
@@ -24,21 +25,18 @@ def solution():
                 else:   
                     candy = 1
                 max_candies = max(max_candies, candy)
-        
-        return max_candies 
-    
-    max_result = 0
+   
     for i in range(n):
         for j in range(n):
             # 사탕의 오른쪽 탐색
             if j < n-1 and candies[i][j] != candies[i][j+1]: # 서로 다르면 swap
                 candies[i][j], candies[i][j+1] = candies[i][j+1], candies[i][j]
-                max_result = max(max_result, count_candies())
+                count_candies()
                 candies[i][j], candies[i][j+1] = candies[i][j+1], candies[i][j] # 다시 swap 해서 원상복귀
-            # 사탕의 왼쪽 탐색
+            # 사탕의 아래쪽 탐색
             if i < n-1 and candies[i][j] != candies[i+1][j]:
                 candies[i][j], candies[i+1][j] = candies[i+1][j], candies[i][j]
-                max_result = max(max_result, count_candies())
+                count_candies()
                 candies[i][j], candies[i+1][j] = candies[i+1][j], candies[i][j]
-    print(max_result)
+    print(max_candies)
 solution()
