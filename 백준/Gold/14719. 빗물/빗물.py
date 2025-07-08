425e75a93e2e464a574ef4e0ac5f08c36fe9c69c
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-08 22:56:55 +0900

    [Gold V] Title: 빗물, Time: 56 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -5,15 +5,14 @@ def solution():
     blocks = list(map(int, input().split()))
     # 2차원 배열 형태로 블록 세우기
     graph = [[False]*w for _ in range(h)]
-    for i in range(h):
-        for j in range(w):
-            if blocks[j] >= 1:
-                graph[i][j] = True
-                blocks[j] -= 1
+    
     count = 0
     for i in range(h):
         j_idx = [] # 열 좌표 모아둘 리스트
         for j in range(w):
+            if blocks[j] >= 1:
+                graph[i][j] = True
+                blocks[j] -= 1
             if graph[i][j]: # true면
                 j_idx.append(j)
         if len(j_idx) >= 2:
