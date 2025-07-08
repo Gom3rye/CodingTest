0808aead42449d935649de29eb56f7cdbe2eeb1e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-08 10:46:44 +0900

    [Silver II] Title: 차이를 최대로, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,41 +1,15 @@
 import sys
-
+from itertools import permutations
+input = sys.stdin.readline
 def solution():
-    input = sys.stdin.readline
     n = int(input())
     a = list(map(int, input().split()))
-    
-    # 각 인덱스의 사용 여부를 기록할 배열
-    visited = [False] * n
     max_result = 0
-
-    def backtracking(perm):
-        nonlocal max_result
-        
-        # 순열이 완성되면 값을 계산
-        if len(perm) == n:
-            result = 0
-            for i in range(n - 1):
-                result += abs(perm[i] - perm[i+1])
-            max_result = max(max_result, result)
-            return
-
-        # 0번부터 n-1번 인덱스를 순회
-        for i in range(n):
-            # 만약 i번 인덱스를 아직 사용하지 않았다면
-            if not visited[i]:
-                # 사용했다고 표시
-                visited[i] = True
-                perm.append(a[i])
-                
-                # 재귀 호출
-                backtracking(perm)
-                
-                # 탐색이 끝나면 다음 순열을 위해 원래대로 되돌림
-                visited[i] = False
-                perm.pop()
-                
-    backtracking([])
+    # a의 모든 순열(permutation)을 구함
+    for p in permutations(a):
+        result = 0
+        for i in range(n - 1):
+            result += abs(p[i] - p[i+1])
+        max_result = max(max_result, result)
     print(max_result)
-
 solution()
