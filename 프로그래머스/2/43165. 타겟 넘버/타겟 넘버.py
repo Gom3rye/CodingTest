b3f6f1f2c3e6dca130e4e71b8fe2a543016c0ca0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-09 07:50:55 +0900

    [level 2] Title: 타겟 넘버, Time: 336.61 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,15 @@
-from itertools import product
 def solution(numbers, target):
     n = len(numbers)
     count = 0
-    for i in product(['+', '-'], repeat=n):
-        result = 0 # 1 <= target 이니까
-        for idx in range(n):
-            if i[idx] == '+': # 첫 번째가 +라면 숫자만 고려
-                result += numbers[idx]
-            else:
-                result -= numbers[idx]
-        if result == target:
-            count += 1  
+    def backtracking(idx, result):
+        nonlocal count
+        if idx == n:
+            if result == target:
+                count += 1
+            return
+        
+        backtracking(idx+1, result+numbers[idx])
+        backtracking(idx+1, result-numbers[idx])
+        
+    backtracking(0, 0)
     return count
