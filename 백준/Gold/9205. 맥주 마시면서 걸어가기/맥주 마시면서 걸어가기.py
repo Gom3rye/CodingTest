dfd5c2ebbe55ce802e3d257a491954a9e2e38c75
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-10 07:28:24 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 40 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,23 +1,17 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
-def bfs(hx, hy, fx, fy, seven11, n):
-    q = deque([(hx, hy)])
-    visited = [False]*n
-    while q:
-        x, y = q.popleft()
-        if abs(x-fx)+abs(y-fy) <= 1000: # 1000까지의 거리는 20병으로 갈 수 있음
-            print("happy")
-            return
-        # 20 병으로 못 가면 편의점 들려야 한다.
-        for i in range(n):
-            if not visited[i]:
-                nx, ny = seven11[i] # 편의점 좌표 뽑기
-                if abs(x-nx)+abs(y-ny) <= 1000:
-                    visited[i] = True # 편의점 방문
-                    q.append((nx, ny))
-    print("sad")
-    return
+sys.setrecursionlimit(10**6)
+def dfs(hx, hy, fx, fy, seven11, n, visited):
+    if abs(hx-fx)+abs(hy-fy) <= 1000:
+        return True
+    for i in range(n):
+        if not visited[i]:
+            nx, ny = seven11[i]
+            if abs(hx-nx)+abs(hy-ny) <= 1000:
+                visited[i] = True
+                if dfs(nx, ny, fx, fy, seven11, n, visited):
+                    return True
+    return False
 def solution():
     t = int(input())
     for _ in range(t):
@@ -28,5 +22,9 @@ def solution():
             cx, cy = map(int, input().split())
             seven11.append((cx, cy)) # 편의점 좌표
         fx, fy = map(int, input().split()) # 페스티벌 좌표
-        bfs(hx, hy, fx, fy, seven11, n)
+        visited = [False]*n
+        if dfs(hx, hy, fx, fy, seven11, n, visited):
+            print("happy")
+        else:
+            print("sad")
 solution()
