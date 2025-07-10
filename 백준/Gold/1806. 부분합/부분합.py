c3d2ef8bdaf73a0047464f21bcf3a848544ae8db
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-10 18:59:22 +0900

    [Gold IV] Title: 부분합, Time: 68 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -9,9 +9,8 @@ def solution():
     # 최소 길이는 end-start+1 로 구하고 값은 result에 누적하면서 구하기
     result = 0
     for end in range(n):
-        if result < s:
-            result += arr[end]
-            # print(f"result: {result}, end: {end}")
+        result += arr[end]
+        # print(f"result: {result}, end: {end}")
 
         while result >= s:
             min_len = min(min_len, end-start+1)
