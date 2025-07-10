154eea22ab3c2dcd5318fede0e002801e4cd1faf
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-10 13:22:33 +0900

    [Gold IV] Title: 테트로미노, Time: 132 ms, Memory: 38332 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,14 +1,10 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 def solution():
     n, m = map(int, input().split())
     s = [list(map(int, input().split())) for _ in range(n)]
-    # T자 모양을 제외한 나머지 모양들은 backtracking으로 풀 수 있음
-
     biggestscrore = max(max(row) for row in s) # 가장 큰 점수
     directions = [(0,1),(1,0),(-1,0),(0,-1)]
-    visited = [[False]*m for _ in range(n)]
     max_score = 0
     def backtracking(x, y, depth, score):
         nonlocal max_score, biggestscrore
@@ -23,19 +19,21 @@ def solution():
 
         for dx, dy in directions:
             nx, ny = dx+x, dy+y
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+            if 0<=nx<n and 0<=ny<m and s[nx][ny]: # 방문한 곳은 다 0으로 바꿔놨으니까 안 방문한 곳만 들어오도록(숫자가 있는 곳만)
+                origin2 = s[nx][ny]
                 if depth == 2: # T자 모양은 2번째 탐색 위치에서 상하좌우를 돌려야한다.(nx,ny로 돌리면 안됨)
-                    visited[nx][ny] = True
-                    backtracking(x, y, depth+1, score+s[nx][ny])
-                    visited[nx][ny] = False
-                visited[nx][ny] = True
-                backtracking(nx, ny, depth+1, score+s[nx][ny])
-                visited[nx][ny] = False
+                    s[nx][ny] = 0 # 방문 처리하고
+                    backtracking(x, y, depth+1, score+origin2)
+                    s[nx][ny] = origin2 # 원래 값 복원
+                s[nx][ny] = 0
+                backtracking(nx, ny, depth+1, score+origin2)
+                s[nx][ny] = origin2
 
     for i in range(n):
         for j in range(m):
-            visited[i][j] = True
-            backtracking(i, j, 1, s[i][j])
-            visited[i][j] = False # backtracking이 끝나고 나면 시작점도 False로 되돌려 놓기 때문에 if not visited[i][j]:가 없어도 된다.
+            origin1 = s[i][j]
+            s[i][j] = 0 # 0으로 방문 처리하고
+            backtracking(i, j, 1, origin1)
+            s[i][j] = origin1 # backtracking이 끝나고 나면 시작점도 scr로 되돌려 놓기 때문에 if not s[i][j]:가 없어도 된다.
     print(max_score)
 solution()
