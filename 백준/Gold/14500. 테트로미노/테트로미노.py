ee99ec487cab889afdd123a6a63a4c3ab8164c70
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-10 13:01:19 +0900

    [Gold IV] Title: 테트로미노, Time: 148 ms, Memory: 39924 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -12,36 +12,30 @@ def solution():
     max_score = 0
     def backtracking(x, y, depth, score):
         nonlocal max_score, biggestscrore
-        # 각 행의 가장 최고값을 더해도 max_score을 갱신할 수 없다면 넘어가자.
+
+        # 가장 최고 점수를 남은 횟수동안 더해도 max_score을 갱신할 수 없다면 넘어가자.
         if score + (4-depth)*biggestscrore < max_score:
             return
+        
         if depth == 4:
             max_score = max(max_score, score)
             return
-        
+
         for dx, dy in directions:
             nx, ny = dx+x, dy+y
             if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                if depth == 2: # T자 모양은 2번째 탐색 위치에서 상하좌우를 돌려야한다.(nx,ny로 돌리면 안됨)
+                    visited[nx][ny] = True
+                    backtracking(x, y, depth+1, score+s[nx][ny])
+                    visited[nx][ny] = False
                 visited[nx][ny] = True
                 backtracking(nx, ny, depth+1, score+s[nx][ny])
                 visited[nx][ny] = False
-
-    def checkTshape(i, j):
-        nonlocal max_score
-        for tshape in combinations(directions, 3):
-            tscore = s[i][j]
-            for tx, ty in tshape:
-                nx, ny = tx+i, ty+j
-                if 0<=nx<n and 0<=ny<m:
-                    tscore += s[nx][ny]
-            max_score = max(max_score, tscore)
 
     for i in range(n):
         for j in range(m):
             visited[i][j] = True
             backtracking(i, j, 1, s[i][j])
             visited[i][j] = False # backtracking이 끝나고 나면 시작점도 False로 되돌려 놓기 때문에 if not visited[i][j]:가 없어도 된다.
-    
-            checkTshape(i, j)
     print(max_score)
 solution()
