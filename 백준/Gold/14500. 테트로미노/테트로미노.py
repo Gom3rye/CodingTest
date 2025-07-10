7765d4096e6abcb66eb4afcbcf2fa34e9af0e52a
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-10 12:13:27 +0900

    [Gold IV] Title: 테트로미노, Time: 640 ms, Memory: 39916 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,33 +1,47 @@
 import sys
 input = sys.stdin.readline
+from itertools import combinations
 def solution():
     n, m = map(int, input().split())
     s = [list(map(int, input().split())) for _ in range(n)]
-    tetromino = [
-        [(1,0),(2,0),(3,0)], [(0,1),(0,2),(0,3)], # 세로, 가로 일자
-        [(0,1),(1,0),(1,1)], # 정사각형
-        [(1,0),(2,0),(2,1)], [(1,0),(2,0),(2,-1)], [(-1,0),(-2,0),(-2,1)], [(-1,0),(-2,0),(-2,-1)], # ㄱ자 대칭
-        [(0,1),(0,2),(1,2)], [(0,1),(0,2),(-1,2)], [(0,-1),(0,-2),(-1,-2)], [(0,-1),(0,-2),(1,-2)], # ㄱ자 회전
-        [(1,0),(1,1),(2,1)], [(0,1),(-1,1),(-1,2)], [(-1,0),(-1,1),(-2,1)], [(0,1),(1,1),(1,2)], # S자 대칭과 회전 (중복 제거 중요!!)
-        [(-1,0),(-1,-1),(-1,1)], [(1,0),(1,-1),(1,1)], [(0,1),(-1,1),(1,1)], [(0,-1),(-1,-1),(1,-1)] # 뻐큐
-    ]
-    def calculate(x, y, tet):
-        score = s[x][y]
-        for tx, ty in tet:
-            nx, ny = tx+x, ty+y
-            if 0<=nx<n and 0<=ny<m:
-                score += s[nx][ny]
-            else:
-                return 0 # 범위에 벗어난다면 계산 바로 끝내야 한다.
-        return score
+    # T자 모양을 제외한 나머지 모양들은 backtracking으로 풀 수 있음
 
+    biggestscrore = max(max(row) for row in s) # 가장 큰 점수
+    directions = [(0,1),(1,0),(-1,0),(0,-1)]
+    visited = [[False]*m for _ in range(n)]
     max_score = 0
+    def backtracking(x, y, depth, score):
+        nonlocal max_score, biggestscrore
+        # 각 행의 가장 최고값을 더해도 max_score을 갱신할 수 없다면 넘어가자.
+        if score + (4-depth)*biggestscrore < max_score:
+            return
+        if depth == 4:
+            max_score = max(max_score, score)
+            return
+        
+        for dx, dy in directions:
+            nx, ny = dx+x, dy+y
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                visited[nx][ny] = True
+                backtracking(nx, ny, depth+1, score+s[nx][ny])
+                visited[nx][ny] = False
+
+    def checkTshape(i, j):
+        nonlocal max_score
+        for tshape in combinations(directions, 3):
+            tscore = s[i][j]
+            for tx, ty in tshape:
+                nx, ny = tx+i, ty+j
+                if 0<=nx<n and 0<=ny<m:
+                    tscore += s[nx][ny]
+            max_score = max(max_score, tscore)
+
     for i in range(n):
         for j in range(m):
-            for tet in tetromino:
-                score = calculate(i, j, tet)
-                if score == 0:
-                    continue
-                max_score = max(max_score, score)
+            visited[i][j] = True
+            backtracking(i, j, 1, s[i][j])
+            visited[i][j] = False # backtracking이 끝나고 나면 시작점도 False로 되돌려 놓기 때문에 if not visited[i][j]:가 없어도 된다.
+    
+            checkTshape(i, j)
     print(max_score)
 solution()
