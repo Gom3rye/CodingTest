9519139de09f5990fb5ab08a3b878273fb0b4465
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-11 17:45:21 +0900

    [Silver I] Title: 스타트링크, Time: 332 ms, Memory: 41816 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,29 +1,36 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 def solution():
     F, S, G, U, D = map(int, input().split())
-    visited = [False] * (F + 1)
+    
+    def bfs(S):
+        q = deque([(S, 0)])
+        visited = [False] * (F + 1)
+        visited[S] = True
 
-    def bfs(start):
-        queue = deque([(start, 0)])
-        visited[start] = True
+        while q:
+            now, button = q.popleft()
 
-        while queue:
-            floor, presses = queue.popleft()
-            if floor == G:
-                return presses
+            if now == G:
+                print(button)
+                return True
 
-            # 이동 가능 시 처리
-            for next_floor in (floor + U, floor - D):
-                if 1 <= next_floor <= F and not visited[next_floor]:
-                    visited[next_floor] = True
-                    queue.append((next_floor, presses + 1))
+            up = now + U
+            down = now - D
 
-        return -1  # 도달 불가능
+            if 1 <= up <= F and not visited[up]:
+                visited[up] = True
+                q.append((up, button + 1))
 
-    result = bfs(S)
-    print(result if result != -1 else "use the stairs")
+            if 1 <= down <= F and not visited[down]:
+                visited[down] = True
+                q.append((down, button + 1))
+
+        return False
+
+    if not bfs(S):
+        print("use the stairs")
 
 solution()
