5bc0f390940427af2d6192f95ac9e0777623a9f0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-14 07:38:40 +0900

    [Silver I] Title: 음식물 피하기, Time: 48 ms, Memory: 34504 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,43 +1,29 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
-# 상하좌우 이동 방향
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
+sys.setrecursionlimit(10**6)
 def solution():
-    N, M, K = map(int, input().split())
-    graph = [[0]*M for _ in range(N)]
-
-    # 음식물 위치 기록
-    for _ in range(K):
+    n, m, k = map(int, input().split())
+    lake = [[0]*m for _ in range(n)]
+    directions = [(1,0),(-1,0),(0,1),(0,-1)]
+    for _ in range(k):
         r, c = map(int, input().split())
-        graph[r-1][c-1] = 1  # 0-based 인덱스로 저장
+        lake[r-1][c-1] = 1
 
-    def bfs(x, y):
-        queue = deque()
-        queue.append((x, y))
-        graph[x][y] = 0  # 방문 처리
-        size = 1
+    def dfs(x, y):
+        lake[x][y] = 0 # 방문 처리
+        lake_size = 1
+        for dx, dy in directions:
+            nx, ny = dx+x, dy+y
+            if 0<=nx<n and 0<=ny<m and lake[nx][ny] == 1: 
+                lake[nx][ny] = 0 # 방문 처리
+                lake_size += dfs(nx, ny)
+        return lake_size
+    
+    largest_lake = 0
+    for i in range(n):
+        for j in range(m):
+            if lake[i][j] == 1:
+                largest_lake = max(largest_lake, dfs(i,j))
 
-        while queue:
-            x, y = queue.popleft()
-            for d in range(4):
-                nx = x + dx[d]
-                ny = y + dy[d]
-                if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
-                    queue.append((nx, ny))
-                    size += 1
-        return size
-
-    max_size = 0
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 1:
-                max_size = max(max_size, bfs(i, j))
-
-    print(max_size)
-
+    print(largest_lake)
 solution()
