963a423f4a75da16a5767fb7b05b9f5463993a92
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-15 13:03:19 +0900

    [Gold III] Title: Guess, Time: 3028 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.py
+++ 백준/Gold/1248. Guess/Guess.py
@@ -5,40 +5,54 @@ def solution():
     n = int(input())
     signs = input().strip()
 
-    sign_matrix = [[0]*n for _ in range(n)]
+    matrix = [[0] * n for _ in range(n)]
     idx = 0
     for i in range(n):
         for j in range(i, n):
             if signs[idx] == '+':
-                sign_matrix[i][j] = 1
+                matrix[i][j] = 1
             elif signs[idx] == '-':
-                sign_matrix[i][j] = -1
+                matrix[i][j] = -1
             else:
-                sign_matrix[i][j] = 0
+                matrix[i][j] = 0
             idx += 1
 
     result = [0] * n
+    possible_values = [[] for _ in range(n)]
+
+    # 🔽 미리 가능한 값 줄이기
+    for i in range(n):
+        if matrix[i][i] == 0:
+            possible_values[i] = [0]
+        elif matrix[i][i] == 1:
+            possible_values[i] = list(range(1, 11))
+        else:
+            possible_values[i] = list(range(-10, 0))
+
+    prefix_sum = [0] * (n + 1)  # 누적합 배열
 
     def is_valid(pos):
-        total = 0
-        for i in range(pos, -1, -1):
-            total += result[i]
-            if total > 0 and sign_matrix[i][pos] != 1:
+        # 🔍 pos번째까지 확인된 누적합을 이용해 구간합 검사
+        for i in range(pos + 1):
+            total = prefix_sum[pos + 1] - prefix_sum[i]
+            if total > 0 and matrix[i][pos] != 1:
                 return False
-            elif total < 0 and sign_matrix[i][pos] != -1:
+            if total < 0 and matrix[i][pos] != -1:
                 return False
-            elif total == 0 and sign_matrix[i][pos] != 0:
+            if total == 0 and matrix[i][pos] != 0:
                 return False
         return True
 
     def backtrack(pos):
         if pos == n:
-            print(' '.join(map(str, result)))
-            sys.exit()  # 첫 해답만 출력하고 종료
-        for num in range(-10, 11):
-            result[pos] = num
+            print(*result)
+            sys.exit()
+        for val in possible_values[pos]:
+            result[pos] = val
+            prefix_sum[pos + 1] = prefix_sum[pos] + val
             if is_valid(pos):
                 backtrack(pos + 1)
 
     backtrack(0)
+
 solution()
