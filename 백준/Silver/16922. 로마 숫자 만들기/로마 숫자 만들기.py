7eeebc01203ebbe73ef6b2f1de10cc9224548976
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-21 11:21:19 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.py
@@ -1,18 +1,12 @@
 import sys
 input = sys.stdin.readline
+from itertools import combinations_with_replacement
 n = int(input())
 numbers = [1, 5, 10, 50]
 result = set() # 만들 수 있는 수가 중복되면 안되니까.
 
-# 중복 조합 backtracking으로 구현
-def backtracking(s, depth, num):
-    if depth == n:
-        result.add(num)
-        return
-    for i in range(s, len(numbers)):
-        num += numbers[i]
-        backtracking(i, depth+1, num)
-        num -= numbers[i]
+for num in combinations_with_replacement(numbers, n):
+    total = sum(num)
+    result.add(total)
 
-backtracking(0,0,0) # start_idx, depth, total_num
 print(len(result))
