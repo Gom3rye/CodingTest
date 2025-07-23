39a581b208a043aae8a3e8047d7e89fc6a6e42cb
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-23 17:01:43 +0900

    [Gold IV] Title: 램프, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Gold/1034. 램프/램프.py
+++ 백준/Gold/1034. 램프/램프.py
@@ -7,12 +7,14 @@ def solution():
     k = int(input())
 
     # row 별로 같은 패턴이 있는 걸 구하고 하나씩 1로 만들어본다.
-    rpattern = Counter(lamps[i] for i in range(n)) # == Counter(lamps)
+    rpattern = Counter(lamps)
+    # count 기준으로 내림차순 정렬
     stdrpattern = rpattern.most_common()
-    for i in range(len(rpattern)):
-        num_zero = stdrpattern[i][0].count('0')
+
+    for pattern, count in stdrpattern:
+        num_zero = pattern.count('0')
         if k >= num_zero and (k-num_zero)%2==0:
-            print(stdrpattern[i][1])
+            print(count)
             break
     else:
         print(0)
