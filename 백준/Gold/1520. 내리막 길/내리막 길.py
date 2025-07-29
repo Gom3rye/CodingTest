0e8a79e14ebceddfcfc0280705f7e9aea7f2a65a
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-07-29 16:47:34 +0900

    [Gold III] Title: 내리막 길, Time: 116 ms, Memory: 45520 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,25 +1,27 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 def solution():
     # 항상 내리막길로만 이동하는 경로의 개수
     n, m = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[0]*m for _ in range(n)] # dp[i][j]: i,j까지 갈 수 있는 경우의 수
-    # dp 초기화
-    dp[0][0] = 1
-    # 높>낮 으로 진행 -> 낮은 수의 칸을 가는 경우의 수를 구하기 위해선 높은 수의 칸을 가는 경우의 수가 이미 정해져야 한다.
-    # 높은 수대로 정렬 => 그 순서로 돌리자.
-    settings = []
-    for i in range(n):
-        for j in range(m):
-            settings.append((board[i][j], i,j))
-    settings.sort(reverse=True)
-
+    dp = [[-1]*m for _ in range(n)] # dp[i][j]: i,j까지 가는 경우의 수 (미방문 표시를 위해 -1로 초기화)
+    
     directions = [(0,1),(0,-1),(1,0),(-1,0)]
-    for _, x, y in settings:
+    def dfs(x, y):
+        # 종료 조건: 목적지에 도달하면 1개의 경로를 찾은 것
+        if x == n-1 and y == m-1:
+            return 1
+        
+        if dp[x][y] != -1: # memoization (이미 계산된 값 그대로 사용)
+            return dp[x][y]
+        
+        dp[x][y] = 0 # 현재 위치에서 갈 수 있는 경로가 없다고 가정하고 0으로 초기화
         for dx, dy in directions:
             nx, ny = dx+x, dy+y
             if 0<=nx<n and 0<=ny<m and board[nx][ny] < board[x][y]:
-                dp[nx][ny] += dp[x][y]
-    print(dp[n-1][m-1])
+                dp[x][y] += dfs(nx, ny)
+        # 결과 저장 및 반환
+        return dp[x][y]
+    print(dfs(0,0))
 solution()
