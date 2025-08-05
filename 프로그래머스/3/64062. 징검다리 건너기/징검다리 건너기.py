8a9286b269ae89c4662fedb2e9ab0f1a7f5728f0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-05 22:51:35 +0900

    [level 3] Title: 징검다리 건너기, Time: 516.98 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,11 +1,12 @@
 def solution(stones, k):
     answer = 0
-    # 연달아서 0이 k번 나오면 실패
+    # 건널 수 없는 돌(0이하)이 연달아 k개 있으면 실패
     # 구해야 하는 것: 디딤돌을 건널 수 있는 최대 인원 수 -> mid로 두기 mid명이 건널 수 있을까? Y/N 질문
     def can_cross(ppl):
         count = 0
         for stone in stones:
-            if stone-ppl < 0:
+		        # 그 전 친구가 지나간 후 내구도가 1이상이어야지 건널 수 있다.
+            if stone-(ppl-1) <= 0:
                 count += 1
             else:
                 count = 0 # 연속성 끊김
