e59d8266afccfc37a97a736bab050be5073deb97
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-08 08:01:51 +0900

    [Gold II] Title: 초콜릿 괴도 코코 (Sweet), Time: 892 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/31464. 초콜릿 괴도 코코 （Sweet）/초콜릿 괴도 코코 （Sweet）.py
+++ 백준/Gold/31464. 초콜릿 괴도 코코 （Sweet）/초콜릿 괴도 코코 （Sweet）.py
@@ -4,46 +4,61 @@ from collections import deque
 def solution():
     n = int(input())
     board = [list(input().strip()) for _ in range(n)]
+    # 사이클을 가능하게 하는 지점을 떼어내야 조건을 만족할 수 있다.
+
+    # 1. 초콜릿들 위치 저장
     chocolates = []
     for i in range(n):
         for j in range(n):
             if board[i][j] == '#':
                 chocolates.append((i, j))
-    result = []
-    for x, y in chocolates: # 떼어낼 초콜릿 정하기
-        # 검사할 초콜릿 빠른 검색을 위해 set으로 생성
-        choco = set(chocolates)
-        choco.remove((x,y))
-        
-        # 조건1. 한 덩어리인지 탐색
-        start = next(iter(choco))
+    
+    choco = len(chocolates)
+    answers = []
+    # 2. 각 초콜릿을 한 번씩 떼어보는 시뮬레이션
+    for i in range(choco):
+        removed = chocolates[i]
+
+        # 떼어낸 후 남은 초콜릿들의 집합 (빠른 조회를 위해 set사용)
+        remaining = set(chocolates)
+        remaining.remove(removed)
+
+        # 조건1. 연결성 확인 (bfs)
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
+        # remaining이 set이라 순서가 없으니 맨 앞 하나 꺼내려면 이렇게 해야 한다.
+        start = next(iter(remaining))
         q = deque([start])
-        visited = {start} # visited=set(start) 하면 set은 iterable을 만들어서 안됨
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+        visited = {start}
         while q:
-            cx, cy = q.popleft()
+            x, y = q.popleft()
             for dx, dy in directions:
-                nx, ny = dx+cx, dy+cy
-                if (nx, ny) not in visited and (nx, ny) in choco:
+                nx, ny = dx+x, dy+y
+                if (nx, ny) in remaining and (nx, ny) not in visited:
                     visited.add((nx, ny))
                     q.append((nx, ny))
 
-        if len(choco) != len(visited):
-            continue # 한 덩어리가 아니라는 소리니까
+        # bfs 후 방문한 개수가 남은 초콜릿 수와 다르면 조각이 나뉜 것
+        if len(visited) != len(remaining):
+            continue
 
-        # 조건2: 트리 구조인지 탐색
-        v = len(choco)
+        # 조건2. 사이클 확인 (v-e=1)
+        v = len(remaining)
         e = 0
-        for cx, cy in choco:
-            # 중복 탐색을 방지하기 위해 오른쪽, 아래쪽만 탐색
-            if (cx+1, cy) in choco:
+        # 간선 개수 세기 (중복 방지를 위해 오른쪽, 아래쪽만 확인)
+        for x, y in remaining:
+            if (x, y+1) in remaining: # 오른쪽 확인
                 e += 1
-            if (cx, cy+1) in choco:
+            if (x+1, y) in remaining: # 아래쪽 확인
                 e += 1
-        if v-e==1:
-            result.append((x+1, y+1))
+        
+        # 연결 그래프가 트리이려면 v-e=1 이어야 한다.
+        if v-e == 1:
+            # 두 조건 모두 만족시 정답에 추가 (좌표는 1based index)
+            answers.append((removed[0]+1, removed[1]+1))
     
-    print(len(result))
-    for x, y in result:
+    # 최종 출력
+    print(len(answers))
+    for x, y in answers:
         print(x, y)
+
 solution()
