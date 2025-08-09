13a086d943d948b6c7bd13201969aa869d8effd0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-09 23:38:14 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
@@ -2,12 +2,9 @@ import sys
 input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split()) # 싸움 시간, 시전 시간
-    dp = [0] * (n+1) # i초를 만드는 가능한 스킬 조합의 수
-    dp[0] = 1  # 0초를 만드는 경우의 수는 아무것도 안 하는 1가지
+    dp = [1] * (n+1) # i초를 만드는 가능한 스킬 조합의 수
     MOD = 1000000007
-    for i in range(1, n+1):
-        dp[i] += dp[i-1] # 1초 사용
-        if i >= m:
-            dp[i] += dp[i-m] # m초 사용
-    print(dp[n]%MOD)
+    for i in range(m, n+1):
+        dp[i] = (dp[i-1]+dp[i-m])%MOD
+    print(dp[n])
 solution()
