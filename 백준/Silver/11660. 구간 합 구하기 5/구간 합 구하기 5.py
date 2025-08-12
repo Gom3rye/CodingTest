f51cd717b729697e952f6d8b72ac4c99785c7726
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-12 10:55:46 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 800 ms, Memory: 106832 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+def solution():
+    n, m = map(int, input().split()) # 표의 크기, 합을 구해야 하는 횟수
+    board = [list(map(int, input().split())) for _ in range(n)]
+    # dp[i][j]: (1,1)에서 (i,j)까지의 합
+    dp = [[0]*(n+1) for _ in range(n+1)]
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            val = board[i-1][j-1]
+            if i == j == 1:
+                dp[i][j] = val
+            elif i == 1 and j >= 2:
+                dp[i][j] += dp[i][j-1] + val
+            elif i >= 2 and j == 1:
+                dp[i][j] += dp[i-1][j] + val
+            elif i >= 2 and j >= 2:
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1] + val
+    # print(dp)
+    for _ in range(m):
+        x1, y1, x2, y2 = map(int, input().split())
+        print(dp[x2][y2]-dp[x1-1][y2]-dp[x2][y1-1]+dp[x1-1][y1-1])
+solution()
