a1f001956e39d7bd13d886d2c1621a2fcdd7fb35
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-12 11:18:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 704 ms, Memory: 106820 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -7,8 +7,7 @@ def solution():
     dp = [[0]*(n+1) for _ in range(n+1)]
     for i in range(1, n+1):
         for j in range(1, n+1):
-            val = board[i-1][j-1]
-            dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1] + val
+            dp[i][j] = dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1] + board[i-1][j-1]
     # print(dp)
     for _ in range(m):
         x1, y1, x2, y2 = map(int, input().split())
