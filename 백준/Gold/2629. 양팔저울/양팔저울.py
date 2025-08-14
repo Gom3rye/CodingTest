68f1d6c5280fdc0999f6a4b05c311d3122db04d5
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-14 13:30:31 +0900

    [Gold III] Title: 양팔저울, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,34 +1,17 @@
 import sys
 input = sys.stdin.readline
-
 def solution():
-    n = int(input())
-    weights = list(map(int, input().split()))
-    m = int(input())
+    n = int(input()) # 추의 개수
+    balance = list(map(int, input().split()))
+    m = int(input()) # 구슬의 개수
     marbles = list(map(int, input().split()))
-
-    possible = set()
-
-    def dfs(i, weight):
-        if (i, weight) in visited:
-            return
-        visited.add((i, weight))
-        if i == n:
-            possible.add(abs(weight))
-            return
-        # 왼쪽에 추가 올리기 (무게 증가)
-        dfs(i + 1, weight + weights[i])
-        # 오른쪽에 추가 올리기 (무게 감소)
-        dfs(i + 1, weight - weights[i])
-        # 사용 안 함
-        dfs(i + 1, weight)
-
-    visited = set()
-    dfs(0, 0)
-
-    result = []
+    dp = set([0])
+    for chu in balance:
+        temp = set()
+        for i in dp:
+            temp.add(i+chu)
+            temp.add(abs(i-chu))
+        dp |= temp # set union
     for marble in marbles:
-        result.append("Y" if marble in possible else "N")
-    print(" ".join(result))
-
+        print('Y' if marble in dp else 'N', end=' ')
 solution()
