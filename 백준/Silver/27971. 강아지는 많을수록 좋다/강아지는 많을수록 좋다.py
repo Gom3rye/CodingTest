0e63db9039a76f22f903e0b4630fc2491e5e2e67
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-17 13:35:57 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 352 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
@@ -3,9 +3,12 @@ from collections import deque
 input = sys.stdin.readline
 def solution():
     n, m, a, b = map(int, input().split()) # 강아지, 닫힌구간, a,b 생성마법
-    spots = [list(map(int, input().split())) for _ in range(m)]
-    q = deque([(0, 0)])
     visited = [False]*(n+1)
+    for i in range(m):
+        start, end = map(int, input().split())
+        for j in range(start, end+1):
+            visited[j] = True
+    q = deque([(0, 0)])
     visited[0] = True
     while q:
         now, count = q.popleft()
@@ -16,11 +19,7 @@ def solution():
                 print(count+1)
                 return
             if not visited[nxt]:
-                for i in range(m):
-                    if spots[i][0] <= nxt <= spots[i][1]:
-                        break
-                else:
-                    visited[nxt] = True
-                    q.append((nxt, count+1))
+                visited[nxt] = True
+                q.append((nxt, count+1))
     print(-1)
 solution()
