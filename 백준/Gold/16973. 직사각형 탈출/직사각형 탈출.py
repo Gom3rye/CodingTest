4bc83aec0a136ea99354e2fb649a8618c874e58d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-18 16:43:34 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 1000 ms, Memory: 48944 KB -BaekjoonHub

--- 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.py
+++ 백준/Gold/16973. 직사각형 탈출/직사각형 탈출.py
@@ -1,51 +1,34 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
-
 def solution():
-    import sys
-    input = sys.stdin.readline
-
-    N, M = map(int, input().split())
-    grid = [list(map(int, input().split())) for _ in range(N)]
-    H, W, Sr, Sc, Fr, Fc = map(int, input().split())
-
-    # 0-based indexing
-    Sr -= 1
-    Sc -= 1
-    Fr -= 1
-    Fc -= 1
-
-    # 누적합 배열 생성
-    acc = [[0] * (M + 1) for _ in range(N + 1)]
-    for r in range(1, N + 1):
-        for c in range(1, M + 1):
-            acc[r][c] = grid[r - 1][c - 1] + acc[r - 1][c] + acc[r][c - 1] - acc[r - 1][c - 1]
-
-    def is_valid(r, c):
-        r2, c2 = r + H, c + W
-        if r2 > N or c2 > M:
-            return False
-        total = acc[r2][c2] - acc[r2][c] - acc[r][c2] + acc[r][c]
-        return total == 0
-
-    visited = [[-1] * M for _ in range(N)]
-    q = deque()
-    q.append((Sr, Sc))
-    visited[Sr][Sc] = 0
-
-    directions = [(-1,0), (1,0), (0,-1), (0,1)]
-
+    n, m = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    h, w, sr, sc, fr, fc = map(int, input().split())
+    sr -= 1
+    sc -= 1
+    fr -= 1
+    fc -= 1 # 0 based index로 바꾸기
+    directions = [(0,1),(0,-1),(1,0),(-1,0)]
+    q = deque([(sr, sc, 0)])
+    visited = [[False]*m for _ in range(n)]
+    # 벽(1)이어서 지날 수 없는 곳을 미리 visited[i][j] = True로 해놓자.
+    # 벽을 덮을 수 있는 모든 블록의 좌측 상단 좌표를 visited 처리 (오른쪽 아래 기준)
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 1:
+                for x in range(max(0, i-h+1), i+1):
+                    for y in range(max(0, j-w+1), j+1):
+                        visited[x][y] = True
     while q:
-        r, c = q.popleft()
-        if (r, c) == (Fr, Fc):
-            print(visited[r][c])
+        x, y, time = q.popleft()
+        if x == fr and y == fc:
+            print(time)
             return
-
-        for dr, dc in directions:
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < M and visited[nr][nc] == -1:
-                if is_valid(nr, nc):
-                    visited[nr][nc] = visited[r][c] + 1
-                    q.append((nr, nc))
-
+        for dx, dy in directions:
+            nx, ny = dx+x, dy+y
+            if 0<=nx<=n-h and 0<=ny<=m-w and not visited[nx][ny]:
+                visited[nx][ny] = True
+                q.append((nx, ny, time+1))
     print(-1)
 solution()
