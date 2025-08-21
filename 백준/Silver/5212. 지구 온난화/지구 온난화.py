cad3006b3736ce5f2bb403b60e5541471040aa2e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-21 17:48:39 +0900

    [Silver II] Title: 지구 온난화, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -18,16 +18,13 @@ def solution():
                 if danger <= 2:
                     can_save.append((i, j))
 
-    # 50년 후에도 살아 있을 땅 사이즈대로 새롭게 지도 출력                
-    max_x, max_y, min_x, min_y = 0, 0, float('inf'), float('inf')
-    for x, y in can_save:
-        max_x = max(max_x, x)
-        min_x = min(min_x, x)
-        max_y = max(max_y, y)
-        min_y = min(min_y, y)
+    max_x = max(x for x, _ in can_save)
+    min_x = min(x for x, _ in can_save)
+    max_y = max(y for _, y in can_save)
+    min_y = min(y for _, y in can_save)
         
     new_board = [['.']*(max_y-min_y+1) for _ in range(max_x-min_x+1)]
-    # 살아남을 땅만 X 표시 해주고
+    # 살아남을 땅만 X 표시 (줄어든 범위만큼 위로 올라가야 하니까 -min_x, -min_y 해주기)
     for x, y in can_save:
         new_board[x-min_x][y-min_y] = 'X'
 
