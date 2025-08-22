df83594bdc852e189c4ae1a19acf57638ac50287
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-22 12:37:04 +0900

    [Silver I] Title: 후보 추천하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -9,12 +9,9 @@ def solution():
         if stu in result:
             result[stu][0] += 1
         else:
-            if len(result) < n:
-                result[stu] = [1, idx]
-            else:
-                # count가 젤 적고 time이 idx가 젤 작은 사람 out
+            if len(result) == n: # count가 젤 적고 time이 idx가 젤 작은 사람 out
                 ready_to_out_stu = sorted(result.items(), key=lambda x: (x[1][0], x[1][1]))[0][0]
                 del result[ready_to_out_stu]
-                result[stu] = [1, idx]
+            result[stu] = [1, idx]
     print(*sorted(result))
 solution()
