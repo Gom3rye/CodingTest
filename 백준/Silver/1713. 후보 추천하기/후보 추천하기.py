794598077969f5688d08365dcfbc241c86cd7da5
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-22 12:20:05 +0900

    [Silver I] Title: 후보 추천하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -4,26 +4,17 @@ def solution():
     n = int(input())
     rec = int(input())
     students = list(map(int, input().split()))
-    result = dict() # result[ppl] = count
-    # 추천수가 똑같다면 먼저 들어온 순으로 빼야 하니까 순서도 중요
-    for i in range(rec):
-        if len(result) < n:
-            if students[i] in result:
-                result[students[i]] += 1
-            else:
-                result[students[i]] = 1
+    result = dict() # result[stu] = [count, idx]
+    for idx, stu in enumerate(students):
+        if stu in result:
+            result[stu][0] += 1
         else:
-            # 이미 result에 있는 사람이라면 count값만 올려주기
-            if students[i] in result:
-                result[students[i]] += 1
+            if len(result) < n:
+                result[stu] = [1, idx]
             else:
-                min_count = float('inf')
-                for ppl in result:
-                    min_count = min(min_count, result[ppl])
-                for ppl in result:
-                    if result[ppl] == min_count:
-                        del result[ppl]
-                        result[students[i]] = 1
-                        break
+                # count가 젤 적고 time이 idx가 젤 작은 사람 out
+                ready_to_out_stu = sorted(result.items(), key=lambda x: (x[1][0], x[1][1]))[0][0]
+                del result[ready_to_out_stu]
+                result[stu] = [1, idx]
     print(*sorted(result))
 solution()
