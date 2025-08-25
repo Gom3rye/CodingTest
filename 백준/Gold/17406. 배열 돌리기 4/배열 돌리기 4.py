b6c682a2aef03e3d47b039bff6f66acf99afb3c4
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-25 17:49:42 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -1,58 +1,48 @@
 import sys
 from itertools import permutations
-from copy import deepcopy
-
 input = sys.stdin.readline
-
 def solution():
-    N, M, K = map(int, input().split())
-    # 원본 배열 저장
-    original_board = [list(map(int, input().split())) for _ in range(N)]
-    # K개의 회전 연산 정보 저장
-    rotations = [list(map(int, input().split())) for _ in range(K)]
+    n, m, k = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    rotations = [list(map(int, input().split())) for _ in range(k)]
+    min_result = float('inf')
+    def count(grid):
+        return min(map(sum, grid))
+    # 회전하는 함수
+    def rotate(x, y, s, temp):
+        x -=1
+        y -=1 # 0based index로 만들기
+        for size in range(1, s+1):
+            # 꼭짓점 정보를 위한 상하좌우
+            left, right, top, bottom = y-size, y+size, x-size, x+size
 
-    min_value = float('inf')
+            # 시작값 저장
+            tmp = temp[top][left]
 
-    # 1. itertools.permutations를 이용해 모든 회전 순서를 생성
-    for p in permutations(rotations, K):
-        # 2. 각 순서를 시도할 때마다 원본 배열의 복사본으로 시작
-        board = deepcopy(original_board)
-        
-        # 현재 순서(p)에 따라 회전 연산을 차례로 수행
-        for r, c, s in p:
-            r, c = r - 1, c - 1 # 0-based index로 변환
+            # 왼쪽 변 -> 위쪽 변 (아래에서 위로)
+            for row in range(top, bottom):
+                temp[row][left] = temp[row+1][left]
             
-            # (r,c,s)에 해당하는 정사각형들을 회전
-            for i in range(1, s + 1):
-                # 각 정사각형의 가장 왼쪽 위 좌표
-                top, left = r - i, c - i
-                bottom, right = r + i, c + i
-                
-                # 시작점 값 임시 저장
-                temp = board[top][left]
+            # 아래쪽 변 -> 왼쪽 변 (오른쪽에서 왼쪽으로)
+            for col in range(left, right):
+                temp[bottom][col] = temp[bottom][col+1]
+            
+            # 오른쪽 변 -> 아래쪽 변 (위에서 아래로)
+            for row in range(bottom, top, -1):
+                temp[row][right] = temp[row-1][right]
 
-                # 왼쪽 변 -> 위쪽 변 (아래에서 위로)
-                for row in range(top, bottom):
-                    board[row][left] = board[row + 1][left]
-                
-                # 아래쪽 변 -> 왼쪽 변 (오른쪽에서 왼쪽으로)
-                for col in range(left, right):
-                    board[bottom][col] = board[bottom][col + 1]
-                
-                # 오른쪽 변 -> 아래쪽 변 (위에서 아래로)
-                for row in range(bottom, top, -1):
-                    board[row][right] = board[row - 1][right]
+            # 위쪽 변 -> 오른쪽 변 (왼쪽에서 오른쪽으로)
+            for col in range(right, left, -1):
+                temp[top][col] = temp[top][col-1]
+            
+            temp[top][left+1] = tmp
+            
 
-                # 위쪽 변 -> 오른쪽 변 (왼쪽에서 오른쪽으로)
-                for col in range(right, left, -1):
-                    board[top][col] = board[top][col - 1]
-                
-                board[top][left + 1] = temp
-
-        # 3. 현재 순서의 모든 회전이 끝난 후 배열의 '값' 계산
-        for row in board:
-            min_value = min(min_value, sum(row))
-
-    print(min_value)
+    for perm in permutations(rotations, k):
+        temp = [row[:] for row in board]
+        for x, y, s in perm:
+            rotate(x, y, s, temp)
+        min_result = min(min_result, count(temp))
+    print(min_result)    
 
 solution()
