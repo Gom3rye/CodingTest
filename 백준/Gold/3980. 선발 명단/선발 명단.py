d92b5406b3ed4448938026a9a264f41ad9c3c836
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-08-25 19:52:41 +0900

    [Gold V] Title: 선발 명단, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.py
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -6,17 +6,17 @@ def solution():
     for _ in range(t):
         board = [list(map(int, input().split())) for _ in range(11)]
         max_score = 0
-        position = set()
+        position = [False]*11
         def backtracking(n, score):
             nonlocal max_score
             if n == 11:
                 max_score = max(max_score, score)
                 return
             for i in range(11):
-                if i not in position and board[n][i] != 0:
-                    position.add(i)
+                if not position[i] and board[n][i] != 0:
+                    position[i] = True
                     backtracking(n+1, score+board[n][i])
-                    position.remove(i)
+                    position[i] = False
         backtracking(0, 0)
         print(max_score)
 solution()
