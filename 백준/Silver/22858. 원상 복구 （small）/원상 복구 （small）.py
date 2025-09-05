6f29490826cdc5cf3ded5a29d594b558ade01e65
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-05 10:14:01 +0900

    [Silver III] Title: 원상 복구 (small), Time: 484 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.py
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.py
@@ -1,14 +1,15 @@
-from copy import deepcopy
-n, k = map(int, input().split())
-
-s = [0] + list(map(int, input().split()))
-d = [0] + list(map(int, input().split()))
-
-p = [0] * (n + 1)
-
-for _ in range(k):
-    for i in range(1, n + 1):
-        p[d[i]] = s[i]
-    s = deepcopy(p)
-
-print(*p[1:])
+import sys
+input = sys.stdin.readline
+def solution():
+    n, k = map(int, input().split())
+    s = [0]+list(map(int, input().split())) # k번 셔플된 결과
+    d = [0]+list(map(int, input().split())) # 셔플규칙
+    p = [0]*(n+1) # 우리가 구해야 하는 것
+    for _ in range(k):
+        for i in range(1, n+1):
+            p[d[i]] = s[i]
+        s = p[:]
+    print(*p[1:])
+    # p[d[i]]를 s[i]로
+    # 원래 카드 배치는?
+solution()
