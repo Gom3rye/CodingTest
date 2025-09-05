03ed94c5b7e122251d895130c3b5fcc566f32e35
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-05 10:48:00 +0900

    [Silver III] Title: 달팽이, Time: 536 ms, Memory: 71008 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,59 +1,49 @@
 import sys
 input = sys.stdin.readline
+def solution():
+    n = int(input())
+    num = int(input())
+    board = [[0]*n for _ in range(n)]
+    # (0,0), (1,1), (2,2)등 (i,i)에서 하->우->상->좌 로 도는거니까 i,i좌표 기억해놓기
+    def make_board(x, y, length, val):
 
-n = int(input())
-m = int(input())
-snail = [[0]*n for _ in range(n)]
-
-# 가운데 1 채우기
-x = (n - 1) // 2
-y = (n - 1) // 2
-snail[x][y] = 1
-
-# 방향벡터 (상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-i = 2
-start = 3
-
-while x != 0 or y != 0:
-    while i <= start * start:
-        if x == y == (n - 1) // 2: # 시작점이면 cnt 세팅, 한 칸 위로
-            cnt_up, cnt_down, cnt_left, cnt_right = start, start - 1, start - 1, start - 2
-            x += dx[0]
-            y += dy[0]
-            cnt_up -= 1
-
-        elif cnt_right > 0: # 우
-            x += dx[3]
-            y += dy[3]
-            cnt_right -= 1
-
-        elif cnt_down > 0: # 하
-            x += dx[1]
-            y += dy[1]
-            cnt_down -= 1
-
-        elif cnt_left > 0: # 좌
-            x += dx[2]
-            y += dy[2]
-            cnt_left -= 1
-
-        elif cnt_up > 0: # 상
-            x += dx[0]
-            y += dy[0]
-            cnt_up -= 1
-
-        snail[x][y] = i
+        i, j = x, y
+        if i == j == n // 2:
+            board[i][j] = val
+            return
+        # 하
+        while i <= length-1:
+            board[i][j] = val
+            i += 1
+            val -= 1
+        i -= 1
+        # 우
+        j += 1
+        while j <= length-1:
+            board[i][j] = val
+            j += 1
+            val -= 1
+        j -= 1
+        # 상
+        i -= 1
+        while i >= x:
+            board[i][j] = val
+            i -= 1
+            val -= 1
         i += 1
+        # 좌
+        j -= 1
+        while j > y:
+            board[i][j] = val
+            j -= 1
+            val -= 1
+        
+        make_board(x+1, y+1, length-1, val)
 
-    n -= 2
-    start += 2
-
-for j in range(len(snail)):
-    print(*snail[j])
-    if m in snail[j]:
-        mx = j + 1
-        my = snail[j].index(m) + 1
-print(mx, my)
+    make_board(0,0,n,n**2)
+    for idx, row in enumerate(board):
+        if num in row:
+            nx, ny = idx+1, row.index(num)+1
+        print(*row)
+    print(nx, ny)
+solution()
