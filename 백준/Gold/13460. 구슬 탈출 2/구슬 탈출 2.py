5e39aedaf82356c80ce52f0ee607f95b94071604
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-06 19:49:49 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 60 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,68 +1,61 @@
 import sys
-from collections import deque # 최소 횟수 구해야 하니까 bfs
 input = sys.stdin.readline
+from collections import deque
 def solution():
-    directions = [(0,1),(0,-1),(1,0),(-1,0)]
     n, m = map(int, input().split())
     board = [list(input().strip()) for _ in range(n)]
-    # 파란 구슬이 빠지거나 빨간 구슬과 파란 구슬 동시에 빠지면 실패
-    # 10번 초과로 움직여야 하면 -1 출력
-    visited = set()
-    for i in range(n):
-        for j in range(m):
-            if board[i][j] == 'R':
-                srx, sry = i, j
-            elif board[i][j] == 'B':
-                sbx, sby = i, j
-
-    # 벽이나 구슬을 만날 때까지 계속 기울이기
+    for x in range(n):
+        for y in range(m):
+            if board[x][y] == 'R':
+                rx, ry = x, y
+            elif board[x][y] == 'B':
+                bx, by = x, y
+    visited = {(rx, ry, bx, by)}
+    q = deque([(rx, ry, bx, by, 0)]) # 빨간 구슬, 파란 구슬 위치, 이동 횟수
+    directions = [(0,1),(0,-1),(1,0),(-1,0)]
+    
+    # 구슬을 굴리는 함수
     def move(x, y, dx, dy):
-        nx, ny = dx+x, dy+y
-        dist = 1
-        while board[nx][ny] != '#':
-            if board[nx][ny] == 'O':
-                return (nx, ny, dist)
-            nx += dx
-            ny += dy
-            dist += 1
-        # board[nx][ny] == '#' 이니까 그 전 좌표와 거리 주기
-        nx -= dx
-        ny -= dy
-        dist -= 1
-        return (nx, ny, dist)
-
-    q = deque([(srx, sry, sbx, sby, 0)])
-    visited.add((srx, sry, sbx, sby))
+        move_dist = 0
+        # 다음 칸이 벽이 아니고, 현재 칸이 구멍이 아닐 동안 계속 이동
+        while board[x+dx][y+dy] != '#' and board[x][y] != 'O':
+            x += dx
+            y += dy
+            move_dist += 1
+        return x, y, move_dist
+    
     while q:
-        rx, ry, bx, by, cnt = q.popleft()
-        # print(rx, ry, bx, by, cnt)
-        if cnt == 10:
-            print(-1)
-            return
+        rx, ry, bx, by, count = q.popleft()
+        if count >= 10:
+            continue # 해당 상태는 실패이므로 pass
         
+        # 네 방향으로 기울이기
         for dx, dy in directions:
-            nrx, nry, rdist = move(rx, ry, dx, dy)
-            nbx, nby, bdist = move(bx, by, dx, dy)
+            nrx, nry, r_dist = move(rx, ry, dx, dy) # r_dist 필요한 이유: 충돌 때 뒤로 보낼 구슬 판단하기 위해서
+            nbx, nby, b_dist = move(bx, by, dx, dy)
 
-            if (nrx, nry, nbx, nby) in visited:
+            # 파란 구슬이 구멍에 빠지면 실패 (빨간 구슬과 함께 빠져도 실패)
+            if board[nbx][nby] == 'O':
                 continue
 
-            if board[nbx][nby] == 'O':
-                continue # 바로 print(-1) 해버리면 R가 구멍에 들어가는 시간이 좀 더 걸릴 경우를 고려 안하니까 안됨
-
+            # 빨간 구슬만 구멍에 빠지면 성공
             if board[nrx][nry] == 'O':
-                print(cnt+1)
+                print(count+1)
                 return
             
-            if (nrx, nry) == (nbx, nby): # 위치가 곂칠 때
-                if rdist < bdist:
-                    nbx -= dx
-                    nby -= dy
-                else:
+            # 두 구슬이 같은 위치에 멈춘 경우, 더 많이 움직인 구슬을 한 칸 뒤로
+            if nrx == nbx and nry == nby:
+                if r_dist > b_dist:
                     nrx -= dx
                     nry -= dy
+                else:
+                    nbx -= dx
+                    nby -= dy
             
-            q.append((nrx, nry, nbx, nby, cnt+1))
-            visited.add((nrx, nry, nbx, nby))
-    print(-1) # R 사방이 #으로 막혀서 못 갈 때
-solution()
+            # 새로 만들어진 상태가 아직 방문 전이라면 큐에 추가
+            if (nrx, nry, nbx, nby) not in visited:
+                visited.add((nrx, nry, nbx, nby))
+                q.append((nrx, nry, nbx, nby, count+1))
+    # 10번 안에 성공하지 못했다면
+    print(-1)
+solution() 
