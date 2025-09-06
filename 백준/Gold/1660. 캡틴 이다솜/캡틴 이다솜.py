32621104ce2a72a82fa2f05b4852e0bda2a3a9a0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-06 23:46:37 +0900

    [Gold V] Title: 캡틴 이다솜, Time: 2840 ms, Memory: 58896 KB -BaekjoonHub

--- 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.py
+++ 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.py
@@ -2,12 +2,9 @@ import sys
 input = sys.stdin.readline
 def solution():
     n = int(input()) # 대포알 수
-    triangles = [0]*(n+1)
-    for i in range(1, n+1):
-        triangles[i] = triangles[i-1]+i
     tetrahedrons = [0]*(n+1)
     for i in range(1, n+1):
-        tetrahedrons[i] = tetrahedrons[i-1] + triangles[i]
+        tetrahedrons[i] = i*(i+1)*(i+2)//6
     dp = [float('inf')]*(n+1) # dp[i]: i개의 대포알로 만들 수 있는 사면체의 최소 개수
     dp[0] = 0
     for tet in tetrahedrons[1:]:
