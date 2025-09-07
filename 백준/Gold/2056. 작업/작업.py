ec09c6377b575cfd50c8da992bbe423af1aad68c
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-08 07:30:41 +0900

    [Gold IV] Title: 작업, Time: 232 ms, Memory: 55124 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.py
+++ 백준/Gold/2056. 작업/작업.py
@@ -1,39 +1,30 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
-
 def solution():
-    N = int(input())
-    
-    time = [0] * (N + 1)              # 작업 시간
-    in_degree = [0] * (N + 1)         # 선행 작업 수
-    graph = [[] for _ in range(N + 1)] # 인접 리스트 (후속 작업들)
-
-    for i in range(1, N + 1):
+    n = int(input())
+    graph = [[] for _ in range(n+1)]
+    build = [0]*(n+1) # build[i]:i의 작업 시간
+    for i in range(1, n+1):
         data = list(map(int, input().split()))
-        time[i] = data[0]
-        in_degree[i] = data[1]
+        build[i] = data[0]
         for pre in data[2:]:
-            graph[pre].append(i)
+            graph[i].append(pre)
+    
+    dp = [-1]*(n+1) # dp[i]를 마치기 위한 최소 시간
+    def dfs(nxt):
+        if dp[nxt] != -1:
+            return dp[nxt]
+        
+        max_pre = 0
+        # 선행 작업이 있으면
+        for pre in graph[nxt]:
+            max_pre = max(max_pre, dfs(pre))
+        
+        dp[nxt] = max_pre + build[nxt]
+        return dp[nxt]
 
-    # dp[i]: i번 작업을 끝내기 위한 최소 시간
-    dp = [0] * (N + 1)
-    queue = deque()
-
-    # 선행 작업이 없는 작업부터 시작
-    for i in range(1, N + 1):
-        if in_degree[i] == 0:
-            dp[i] = time[i]
-            queue.append(i)
-
-    while queue:
-        curr = queue.popleft()
-        for next_task in graph[curr]:
-            in_degree[next_task] -= 1
-            dp[next_task] = max(dp[next_task], dp[curr] + time[next_task])
-            if in_degree[next_task] == 0:
-                queue.append(next_task)
-
+    # 가장 오래 걸리는 시간이 모든 작업을 수행한 결과이므로
+    for i in range(1, n+1):
+        dfs(i)
     print(max(dp))
-
 solution()
