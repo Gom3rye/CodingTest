f510d6ccecafe931c22b8590ad6a26b28091e119
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-08 23:24:20 +0900

    [Gold IV] Title: 팀 빌딩, Time: 72 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.py
@@ -1,24 +1,16 @@
+import sys
+input = sys.stdin.readline
 def solution():
-    import sys
-    input = sys.stdin.readline
-
-    N = int(input())
+    n = int(input())
     x = list(map(int, input().split()))
-
-    left = 0
-    right = N - 1
-    max_team = 0
-
-    while left < right:
-        # 사이에 있는 개발자 수 = right - left - 1
-        team_power = (right - left - 1) * min(x[left], x[right])
-        max_team = max(max_team, team_power)
-
-        # 능력치가 작은 쪽 포인터를 움직여야 min()이 커질 가능성이 있음
-        if x[left] < x[right]:
-            left += 1
+    start, end = 0, n-1
+    max_score = 0
+    while start < end:
+        score = (end-start-1)*min(x[start], x[end])
+        max_score = max(max_score, score)
+        if x[start] < x[end]:
+            start += 1
         else:
-            right -= 1
-
-    print(max_team)
+            end -= 1
+    print(max_score)
 solution()
