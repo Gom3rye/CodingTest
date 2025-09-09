8cb8e4207634526526a36e2042cd00c327b26a32
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-10 07:45:41 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 108 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.py
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.py
@@ -1,26 +1,16 @@
+import sys
+input = sys.stdin.readline
 def solution():
-    import sys
-    input = sys.stdin.readline
+    n, m, h = map(int, input().split())
+    blocks = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0]*(h+1) for _ in range(n+1)] # dp[i][j]: i번째 학생이 j높이의 탑을 쌓은 경우
+    dp[0][0] = 1 # 아무 학생이 아무 블록은 안 놓은 경우의 수
+    for i in range(1, n+1):
+        for j in range(h+1):
+            dp[i][j] = dp[i-1][j] # 아무것도 안 놓은 경우의 수
+            for b in blocks[i-1]:
+                if j-b >= 0:
+                    dp[i][j] = (dp[i][j] + dp[i-1][j-b])%10007 # 아무것도 안 놓은 경우의 수 + 놓은 경우의 수
 
-    MOD = 10007
-
-    N, M, H = map(int, input().split())
-    blocks = []
-    for _ in range(N):
-        blocks.append(list(map(int, input().split())))
-
-    # dp[i][h]: i번째 학생까지 고려했을 때 높이 h를 만들 수 있는 경우의 수
-    dp = [[0] * (H + 1) for _ in range(N + 1)]
-    dp[0][0] = 1  # 아무 학생도 선택하지 않고 높이 0 만드는 방법 1가지
-
-    for i in range(1, N + 1):
-        for h in range(H + 1):
-            # 1. i번째 학생의 블록을 사용하지 않는 경우
-            dp[i][h] = dp[i - 1][h]
-            # 2. i번째 학생의 블록 중 하나를 사용하는 경우
-            for b in blocks[i - 1]:
-                if h - b >= 0:
-                    dp[i][h] = (dp[i][h] + dp[i - 1][h - b]) % MOD
-
-    print(dp[N][H])
+    print(dp[n][-1])
 solution()
