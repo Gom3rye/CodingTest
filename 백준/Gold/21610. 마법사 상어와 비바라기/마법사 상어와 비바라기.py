41fb6716f25d7c321c9f8dd536e001f506c6fe81
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-10 19:44:57 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 156 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,55 +1,56 @@
-# 방향: ←, ↖, ↑, ↗, →, ↘, ↓, ↙ (1~8)
-dx = [0, -1, -1, -1, 0, 1, 1, 1]
-dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-
-# 대각선 방향 (물복사 버그 전용)
-diag_dx = [-1, -1, 1, 1]
-diag_dy = [-1, 1, -1, 1]
-
-def solve_magician_shark():
-    import sys
-    input = sys.stdin.readline
-
-    N, M = map(int, input().split())
-    A = [list(map(int, input().split())) for _ in range(N)]
-    moves = [tuple(map(int, input().split())) for _ in range(M)]
-
-    # 초기 구름 위치
-    clouds = [(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)]
-
-    for d, s in moves:
-        # 1. 구름 이동
-        new_clouds = []
-        for x, y in clouds:
-            nx = (x + dx[d-1] * s) % N
-            ny = (y + dy[d-1] * s) % N
-            new_clouds.append((nx, ny))
-
-        # 2. 비 내림
-        visited = [[False]*N for _ in range(N)]
-        for x, y in new_clouds:
-            A[x][y] += 1
-            visited[x][y] = True
-
-        # 3. 물복사 버그
-        for x, y in new_clouds:
-            count = 0
-            for k in range(4):
-                nx = x + diag_dx[k]
-                ny = y + diag_dy[k]
-                if 0 <= nx < N and 0 <= ny < N and A[nx][ny] > 0:
-                    count += 1
-            A[x][y] += count
-
-        # 4. 새로운 구름 생성
+import sys
+input = sys.stdin.readline
+def solution():
+    n, m = map(int, input().split()) # n*n격자, m번의 이동
+    a = [list(map(int, input().split())) for _ in range(n)]
+    directions = {1:(0,-1), 2:(-1,-1), 3:(-1,0), 4:(-1,1), 5:(0,1), 6:(1,1), 7:(1,0), 8:(1,-1)}
+    # 행, 열 연결되었으니 % 연산하기
+    dirs = [(-1,-1),(-1,1),(1,-1),(1,1)]
+    def move(a, init_cloud, d, s):
+        # 새로운 구름좌표를 저장해놓는 배열
         clouds = []
-        for i in range(N):
-            for j in range(N):
-                if A[i][j] >= 2 and not visited[i][j]:
-                    clouds.append((i, j))
-                    A[i][j] -= 2
+        for cx, cy in init_cloud:
+            dx, dy = directions[d]
+            nx, ny = (cx+dx*s)%n, (cy+dy*s)%n
+            clouds.append((nx, ny)) # 새로운 구름 표시
+        
+        # 또 다음 새로운 구름좌표에 지금 새로운 구름좌표가 포함되지 않도록
+        visited = [[False]*n for _ in range(n)]
+        for x, y in clouds:
+            visited[x][y] = True
+            a[x][y] += 1 # 비가 내려서 1증가
 
-    # 결과 출력
-    print(sum(map(sum, A)))
+        # 물복사 마법
+        for x, y in clouds:
+            cnt = 0
+            for dx, dy in dirs:
+                nx, ny = x+dx, y+dy
+                if 0<=nx<n and 0<=ny<n and a[nx][ny] != 0:
+                    cnt += 1
+            # 물복사만큼 증가
+            a[x][y] += cnt
+        
+        # 2이상인 새로운 구름 만들기
+        new_clouds = []
+        for i in range(n):
+            for j in range(n):
+                if a[i][j] >= 2 and not visited[i][j]:
+                    new_clouds.append((i, j))
+                    a[i][j] -= 2
+        # print(f"a: {a}, new_clouds: {new_clouds}")
+        return a, new_clouds
 
-solve_magician_shark()  # 제출 시에만 호출
+
+    init_cloud = [(n-1, 0), (n-1, 1), (n-2, 0), (n-2, 1)]
+    for _ in range(m): # 명령
+        d, s = map(int, input().split())
+        new_a, new_clouds = move(a, init_cloud, d, s)
+        init_cloud = new_clouds
+        a = new_a
+
+    # 바구니에 들어있는 물의 양의 합 출력
+    result = 0
+    for row in a:
+        result += sum(row)
+    print(result)
+solution()
