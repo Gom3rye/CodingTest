13562ad5ba5f0197b2fc2d6febfdc38a6c794da8
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-10 12:50:13 +0900

    [Gold V] Title: 진우의 민트초코우유, Time: 4988 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/20208. 진우의 민트초코우유/진우의 민트초코우유.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+def solution():
+    n, m, k = map(int, input().split()) # n*n격자, 초기체력, 민트초코 마셨을때 증가하는 체력
+    board = [list(map(int, input().split())) for _ in range(n)]
+    mintchocos = []
+    # 순서: 이동 후 마셔서 체력 보충
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                hx, hy = i, j # 진우의 집 좌표
+            elif board[i][j] == 2:
+                mintchocos.append((i, j)) # 민초의 좌표
+    mint_n = len(mintchocos)
+    # 방문 처리할 배열
+    visited = [False]*mint_n
+    max_cnt = 0
+    def backtracking(x, y, hp, cnt): # 민트초코 조합으로 뽑는 백트레킹
+        nonlocal max_cnt
+
+        # 집에 다시 돌아올 수 있는 경우에 max_cnt 값 갱신
+        if hp-(abs(hx-x)+abs(hy-y)) >= 0:
+            max_cnt = max(max_cnt, cnt)
+
+        if cnt == mint_n: # 마신 민트 초코 개수가 전체와 같다면
+            return
+        
+        for i in range(mint_n):
+            if not visited[i]:
+                mx, my = mintchocos[i] # mint_x, mint_y
+                needed = abs(x-mx)+abs(y-my)
+                # 지금 당장은 집에 못 돌아가더라도, 앞으로 더 많은 민초를 먹어서 체력을 늘린 후에 집에 돌아갈 수 있다면 괜찮다는 조건 주의!
+                if hp >= needed:
+                    visited[i] = True
+                    backtracking(mx, my, hp-needed+k, cnt+1)
+                    visited[i] = False
+
+    backtracking(hx, hy, m, 0)
+    print(max_cnt)
+solution()
