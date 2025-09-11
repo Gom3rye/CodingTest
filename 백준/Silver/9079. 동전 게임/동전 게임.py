b3d645ae2c33e009cd4ac4efabcf1ce1e0fb98a3
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-11 20:40:02 +0900

    [Silver II] Title: 동전 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9079. 동전 게임/동전 게임.py
+++ 백준/Silver/9079. 동전 게임/동전 게임.py
@@ -1,63 +1,66 @@
+from itertools import combinations
 import sys
-from collections import deque
+input = sys.stdin.readline
 
-# 하나의 테스트 케이스를 푸는 함수
-def solve():
-    # 1. 초기 상태 입력 및 변환
-    initial_grid = [sys.stdin.readline().split() for _ in range(3)]
-    # set에 넣기 위해 불변 자료형인 tuple로 변환
-    initial_state = tuple(map(tuple, initial_grid))
-    
-    # 목표 상태 정의
-    all_H = tuple(tuple('H' for _ in range(3)) for _ in range(3))
-    all_T = tuple(tuple('T' for _ in range(3)) for _ in range(3))
+# 동전 뒤집기 함수
+def flip(c):
+    return 'H' if c == 'T' else 'T'
 
-    # 2. BFS 준비
-    q = deque([(initial_state, 0)]) # (상태, 연산 횟수)
-    visited = {initial_state}
+# 주어진 연산 조합을 board에 적용
+def apply(board, ops):
+    new_board = [row[:] for row in board]  # 깊은 복사
+    for op in ops:
+        if op == 0:  # 행 0
+            for j in range(3):
+                new_board[0][j] = flip(new_board[0][j])
+        elif op == 1:  # 행 1
+            for j in range(3):
+                new_board[1][j] = flip(new_board[1][j])
+        elif op == 2:  # 행 2
+            for j in range(3):
+                new_board[2][j] = flip(new_board[2][j])
+        elif op == 3:  # 열 0
+            for i in range(3):
+                new_board[i][0] = flip(new_board[i][0])
+        elif op == 4:  # 열 1
+            for i in range(3):
+                new_board[i][1] = flip(new_board[i][1])
+        elif op == 5:  # 열 2
+            for i in range(3):
+                new_board[i][2] = flip(new_board[i][2])
+        elif op == 6:  # 대각선 ↘
+            for i in range(3):
+                new_board[i][i] = flip(new_board[i][i])
+        elif op == 7:  # 대각선 ↙
+            for i in range(3):
+                new_board[i][2 - i] = flip(new_board[i][2 - i])
+    return new_board
 
-    # 3. BFS 실행
-    while q:
-        current_state, count = q.popleft()
+# 모든 칸이 같은지 확인
+def is_uniform(board):
+    target = board[0][0]
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != target:
+                return False
+    return True
 
-        # 목표 상태 도달 시 종료
-        if current_state == all_H or current_state == all_T:
-            print(count)
-            return
+def solution():
+    T = int(input())
+    for _ in range(T):
+        board = [input().split() for _ in range(3)]
+        min_moves = -1
 
-        # 8가지 연산을 적용하여 다음 상태 생성
-        # 0~2: 행 뒤집기, 3~5: 열 뒤집기, 6: 주 대각선, 7: 부 대각선
-        for i in range(8):
-            # 다음 상태를 만들기 위해 임시로 list로 변환
-            grid = [list(row) for row in current_state]
+        all_ops = list(range(8))  # 0~7번 연산
 
-            if i < 3: # 행 뒤집기
-                for j in range(3):
-                    grid[i][j] = 'H' if grid[i][j] == 'T' else 'T'
-            elif i < 6: # 열 뒤집기
-                col = i - 3
-                for j in range(3):
-                    grid[j][col] = 'H' if grid[j][col] == 'T' else 'T'
-            elif i == 6: # 주 대각선 뒤집기
-                for j in range(3):
-                    grid[j][j] = 'H' if grid[j][j] == 'T' else 'T'
-            else: # 부 대각선 뒤집기
-                for j in range(3):
-                    grid[j][2-j] = 'H' if grid[j][2-j] == 'T' else 'T'
-            
-            # 다시 tuple로 변환하여 다음 상태 생성
-            next_state = tuple(map(tuple, grid))
-            
-            # 아직 방문하지 않은 상태라면 큐에 추가
-            if next_state not in visited:
-                visited.add(next_state)
-                q.append((next_state, count + 1))
+        # 연산 조합을 0~8개 선택해서 시도
+        for r in range(0, 9):  # 연산 개수
+            for ops in combinations(all_ops, r):
+                temp_board = apply(board, ops)
+                if is_uniform(temp_board):
+                    if min_moves == -1 or r < min_moves:
+                        min_moves = r
 
-    # 큐가 비었는데 목표에 도달 못했다면 불가능
-    print(-1)
+        print(min_moves)
 
-
-# 메인 로직: 테스트 케이스 수만큼 solve 함수 호출
-T = int(sys.stdin.readline())
-for _ in range(T):
-    solve()
+solution()
