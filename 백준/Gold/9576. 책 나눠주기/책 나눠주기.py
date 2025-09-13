2f983535bc1e51c0c8d6ca88600364f03c1dc6b7
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-13 17:20:18 +0900

    [Gold II] Title: 책 나눠주기, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
@@ -1,20 +1,32 @@
 import sys
 input = sys.stdin.readline
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a != b:
+        parent[a] = b
+
 def solution():
     t = int(input())
     for _ in range(t):
-        n, m = map(int, input().split()) # n개의 책, m명에게 나눠줌
-        cnt = 0 # 책을 준 학생 수
-        infos = list(tuple(map(int, input().split())) for _ in range(m))
-        # 최대한 많은 학생들에게 책을 주기 위해 범위가 좁은 것부터 해결
+        n, m = map(int, input().split())
+        parent = [i for i in range(n+2)]  # n+1까지 만들기 (범위 초과 방지)
+
+        infos = [tuple(map(int, input().split())) for _ in range(m)]
         infos.sort(key=lambda x: x[1])
-        visited = [False]*(n+1)
+
+        cnt = 0
         for a, b in infos:
-            for i in range(a, b+1):
-                if not visited[i]:
-                    visited[i] = True
-                    cnt += 1
-                    break
+            available = find(parent, a)
+            if available <= b:
+                union(parent, available, available + 1)
+                cnt += 1
         print(cnt)
-        
+
 solution()
