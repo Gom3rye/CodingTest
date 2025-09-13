173af01acfe25a7b0481849f21c0d1a50cdff9bb
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-13 12:20:56 +0900

    [Silver II] Title: 동전 게임, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9079. 동전 게임/동전 게임.py
+++ 백준/Silver/9079. 동전 게임/동전 게임.py
@@ -1,66 +1,51 @@
-from itertools import combinations
 import sys
+from itertools import combinations
 input = sys.stdin.readline
-
-# 동전 뒤집기 함수
-def flip(c):
-    return 'H' if c == 'T' else 'T'
-
-# 주어진 연산 조합을 board에 적용
-def apply(board, ops):
-    new_board = [row[:] for row in board]  # 깊은 복사
-    for op in ops:
-        if op == 0:  # 행 0
-            for j in range(3):
-                new_board[0][j] = flip(new_board[0][j])
-        elif op == 1:  # 행 1
-            for j in range(3):
-                new_board[1][j] = flip(new_board[1][j])
-        elif op == 2:  # 행 2
-            for j in range(3):
-                new_board[2][j] = flip(new_board[2][j])
-        elif op == 3:  # 열 0
-            for i in range(3):
-                new_board[i][0] = flip(new_board[i][0])
-        elif op == 4:  # 열 1
-            for i in range(3):
-                new_board[i][1] = flip(new_board[i][1])
-        elif op == 5:  # 열 2
-            for i in range(3):
-                new_board[i][2] = flip(new_board[i][2])
-        elif op == 6:  # 대각선 ↘
-            for i in range(3):
-                new_board[i][i] = flip(new_board[i][i])
-        elif op == 7:  # 대각선 ↙
-            for i in range(3):
-                new_board[i][2 - i] = flip(new_board[i][2 - i])
-    return new_board
-
-# 모든 칸이 같은지 확인
-def is_uniform(board):
-    target = board[0][0]
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] != target:
-                return False
-    return True
-
 def solution():
-    T = int(input())
-    for _ in range(T):
+    t = int(input())
+    def flip(board, operations):
+        new_board = [row[:] for row in board] # 다음 연산에서 board 또 써야 하니까 깊은 복사
+        for op in operations:
+            # 행 연산
+            if 0<=op<3:
+                for j in range(3):
+                    new_board[op][j] = 'T' if new_board[op][j] == 'H' else 'H'
+            # 열 연산
+            elif 3<=op<5:
+                op -= 3
+                for i in range(3):
+                    new_board[i][op] = 'T' if new_board[i][op] == 'H' else 'H'
+            # 대각선1
+            elif op == 6:
+                for i in range(3):
+                    new_board[i][i] = 'T' if new_board[i][i] == 'H' else 'H'
+            # 대각선2
+            else:
+                for i in range(3):
+                    new_board[i][2-i] = 'T' if new_board[i][2-i] == 'H' else 'H'
+        return new_board
+
+    def check(board):
+        # 모두 같은 면인지 확인
+        if all(board[i][j]=='H' for i in range(3) for j in range(3)):
+            return True
+        elif all(board[i][j]=='T' for i in range(3) for j in range(3)):
+            return True
+        return False
+
+    for _ in range(t):
         board = [input().split() for _ in range(3)]
-        min_moves = -1
-
-        all_ops = list(range(8))  # 0~7번 연산
-
-        # 연산 조합을 0~8개 선택해서 시도
-        for r in range(0, 9):  # 연산 개수
-            for ops in combinations(all_ops, r):
-                temp_board = apply(board, ops)
-                if is_uniform(temp_board):
-                    if min_moves == -1 or r < min_moves:
-                        min_moves = r
-
-        print(min_moves)
-
+        flag = False
+        for cnt in range(8):
+            for operations in combinations(range(8), cnt): # 연산 횟수0~2: 행 바꾸기, 3~5: 열 바꾸기, 6: 대각선1, 7: 대각선2
+                new_board = flip(board, operations)
+                if check(new_board):
+                    print(cnt)
+                    flag = True
+                    break
+            if flag:
+                break
+        # 모두 같은 면으로 만드는 것이 불가능한 경우
+        if not flag:
+            print(-1)
 solution()
