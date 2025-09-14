4173cc951a459958ed5e1025c33d9ce4a2078ee0
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-14 16:34:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 200 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,26 +1,13 @@
 import sys
-
-# 빠른 입력
-input = sys.stdin.read
-data = input().split()
-
-# 데이터 파싱
-n = int(data[0])
-m = int(data[1])
-numbers = list(map(int, data[2:n+2]))
-queries = data[n+2:]
-
-# 누적합 배열 생성
-prefix_sum = [0] * (n + 1)
-for i in range(n):
-    prefix_sum[i+1] = prefix_sum[i] + numbers[i]
-
-# 결과 처리
-output = []
-for k in range(0, len(queries), 2):
-    i = int(queries[k])
-    j = int(queries[k+1])
-    output.append(str(prefix_sum[j] - prefix_sum[i - 1]))
-
-# 한 번에 출력
-sys.stdout.write('\n'.join(output))
+input = sys.stdin.readline
+def solution():
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    # 구간합 구하기
+    cum = [0]*(n+1)
+    for i in range(1, n+1):
+        cum[i] = cum[i-1]+arr[i-1]
+    for _ in range(m):
+        i, j = map(int, input().split())
+        print(cum[j]-cum[i-1])
+solution()
