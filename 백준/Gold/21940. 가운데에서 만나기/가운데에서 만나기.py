64ba7aa68d507f61f07fdde2cc10ad3f1e2429fe
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-17 13:58:44 +0900

    [Gold IV] Title: 가운데에서 만나기, Time: 952 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.py
+++ 백준/Gold/21940. 가운데에서 만나기/가운데에서 만나기.py
@@ -3,27 +3,26 @@ input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split())
     INF = float('inf')
-    graph = [[INF]*n for _ in range(n)]
+    graph = [[INF]*(n+1) for _ in range(n+1)]
     for _ in range(m):
         a, b, t = map(int, input().split())
-        graph[a-1][b-1] = t
-    for i in range(n):
+        graph[a][b] = t
+    for i in range(n+1):
         graph[i][i] = 0
     
     k = int(input())
     cities = list(map(int, input().split()))
     def floyd():
-        for k in range(n):
-            for a in range(n):
-                for b in range(n):
+        for k in range(1, n+1):
+            for a in range(1, n+1):
+                for b in range(1, n+1):
                     graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
     floyd()
     dist = []
-    for x in range(n): # 0based index
+    for x in range(1, n+1):
         flag = True # 친구<->x<->친구 가 가능해야 하기 때문에 flag 필요
         longest_distance = 0
         for ppl in cities:
-            ppl -= 1 # 0based index
             roundtrip = graph[ppl][x]+graph[x][ppl]
             if graph[ppl][x] == INF or graph[x][ppl] == INF:
                 flag = False
@@ -35,6 +34,6 @@ def solution():
     answer = []
     for idx, x in enumerate(dist):
         if x == min_d:
-            answer.append(idx+1) # 1based index
+            answer.append(idx+1)
     print(*answer) 
 solution()
