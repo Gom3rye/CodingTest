f608e0eb04f8bf5944163890b3d258499fe65a55
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-20 21:10:25 +0900

    [Silver I] Title: 기타콘서트, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.py
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.py
@@ -1,25 +1,40 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
-def solution():
-    n, m = map(int, input().split()) # 기타의 개수, 곡의 개수
-    infos = [input().split() for _ in range(n)]
-    max_play = 0
-    guitar_cnt = 0 # 최소의 기타 개수(정답)
-    for cnt in range(1, n+1):
-        play = 0
-        for chosen in combinations(range(n), cnt):
-            songs = [False]*m
-            for guitar in chosen:
-                can_play = infos[guitar][1]
-                for i in range(m):
-                    if can_play[i] == 'Y' and not songs[i]:
-                        songs[i] = True
-            play = max(play, sum(songs))
-        if play > max_play:
-            max_play = play
-            guitar_cnt = cnt
 
-    print(guitar_cnt if guitar_cnt != 0 else -1)
+def solution():
+    n, m = map(int, input().split())  # 기타 수, 곡 수
+    infos = [input().split()[1] for _ in range(n)]  # Y/N 문자열만 추출
+
+    max_play = 0
+    min_guitar = float('inf')
+
+    def dfs(idx, selected, songs):
+        nonlocal max_play, min_guitar
+
+        # 종료 조건: 모든 기타 확인했을 때
+        if idx == n:
+            play_cnt = songs.count(True)
+            if play_cnt == 0:
+                return
+            if play_cnt > max_play:
+                max_play = play_cnt
+                min_guitar = selected
+            elif play_cnt == max_play:
+                min_guitar = min(min_guitar, selected)
+            return
+
+        # 선택하지 않는 경우
+        dfs(idx + 1, selected, songs[:])
+
+        # 선택하는 경우
+        new_songs = songs[:]
+        for i in range(m):
+            if infos[idx][i] == 'Y':
+                new_songs[i] = True
+        dfs(idx + 1, selected + 1, new_songs)
+
+    dfs(0, 0, [False] * m)
+
+    print(min_guitar if max_play > 0 else -1)
 
 solution()
