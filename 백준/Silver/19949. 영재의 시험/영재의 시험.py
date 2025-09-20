629faaa5696da70cafa5804abba11e6989bf2882
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-20 17:19:17 +0900

    [Silver II] Title: 영재의 시험, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.py
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -1,27 +1,31 @@
 import sys
 input = sys.stdin.readline
+def solution():
+    answer = list(map(int, input().split()))
+    # dp 상태 정의 잘하기 dp[idx][prev1][prev2][score]: 이 상태를 가지고 있는 경우의 수
+    dp = [[[[-1]*11 for _ in range(6)] for _ in range(6)] for _ in range(10)]
+    def dfs(idx, prev1, prev2, score):
+        if idx == 10:
+            if score >= 5:
+                return 1
+            return 0
+        
+        # memoization
+        if dp[idx][prev1][prev2][score] != -1: # 초기 상황이 아니라면
+            return dp[idx][prev1][prev2][score]
+        
+        total = 0
+        for choices in range(1, 6):
+            if prev1 == prev2 == choices:
+                continue
+            if answer[idx] == choices:
+                total += dfs(idx+1, choices, prev1, score+1)
+            else:
+                total += dfs(idx+1, choices, prev1, score)
 
-# 입력 받기
-answer = list(map(int, input().split()))
-count = 0
+        dp[idx][prev1][prev2][score] = total
+        return total
 
-def dfs(pos, picked, score):
-    global count
-    if pos == 10:
-        if score >= 5:
-            count += 1
-        return
-
-    for i in range(1, 6):
-        # 연속 3문제 같은 답이면 안 됨
-        if pos >= 2 and picked[pos - 1] == picked[pos - 2] == i:
-            continue
-        picked.append(i)
-        if i == answer[pos]:
-            dfs(pos + 1, picked, score + 1)
-        else:
-            dfs(pos + 1, picked, score)
-        picked.pop()
-
-dfs(0, [], 0)
-print(count)
+    print(dfs(0, 0, 0, 0)) # idx, prev1, prev2, score
+    
+solution()
