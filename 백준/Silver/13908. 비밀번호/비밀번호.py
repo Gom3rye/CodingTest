a8787ea9ae4347a0ec524d70bee53abe9b078741
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-20 15:04:32 +0900

    [Silver II] Title: 비밀번호, Time: 1296 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13908. 비밀번호/비밀번호.py
+++ 백준/Silver/13908. 비밀번호/비밀번호.py
@@ -1,16 +1,28 @@
 import sys
-from itertools import product
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split())
     numbers = list(map(int, input().split()))
-    # 가능한 모든 비밀번호의 개수를 구하는 것, 중복 순열
+    # 가능한 모든 비밀번호의 개수를 구하는 것, 백트레킹
+    visited = [False]*10
+    for num in numbers:
+        visited[num] = True
     cnt = 0
-    for nums in product(range(10), repeat=n):
-        for num in numbers:
-            if num not in nums:
-                break
-        else:
-            cnt += 1
+    def backtracking(idx, partial):
+        nonlocal cnt
+        if idx == n:
+            if partial == m:
+                cnt += 1
+            return
+        for num in range(10):
+            if not visited[num]:
+                backtracking(idx+1, partial)
+            else:
+                visited[num] = False
+                backtracking(idx+1, partial+1)
+                visited[num] = True
+
+    backtracking(0, 0)
     print(cnt)
 solution()
