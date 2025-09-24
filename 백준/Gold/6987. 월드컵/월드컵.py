59bfee9fff2edf64289bac0ce495186a0c3c8b80
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-24 21:07:19 +0900

    [Gold IV] Title: 월드컵, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.py
+++ 백준/Gold/6987. 월드컵/월드컵.py
@@ -1,78 +1,65 @@
 import sys
 from itertools import combinations
-
-def solve(game_idx):
-    # [성공 조건] 15경기를 모두 성공적으로 배정했다면 가능한 경우
-    if game_idx == 15:
+input = sys.stdin.readline
+def backtracking(idx, game, team_result):
+    if idx == 15: # 모든 경기를 탐색했으면
         return True
-
-    team1, team2 = games[game_idx]
-
-    # 선택 1: team1 승, team2 패
-    if records[team1][0] > 0 and records[team2][2] > 0:
-        records[team1][0] -= 1
-        records[team2][2] -= 1
-        if solve(game_idx + 1):
-            return True
-        # 백트래킹 (원상 복구)
-        records[team1][0] += 1
-        records[team2][2] += 1
-
-    # 선택 2: team1 무, team2 무
-    if records[team1][1] > 0 and records[team2][1] > 0:
-        records[team1][1] -= 1
-        records[team2][1] -= 1
-        if solve(game_idx + 1):
-            return True
-        # 백트래킹 (원상 복구)
-        records[team1][1] += 1
-        records[team2][1] += 1
-
-    # 선택 3: team1 패, team2 승
-    if records[team1][2] > 0 and records[team2][0] > 0:
-        records[team1][2] -= 1
-        records[team2][0] += 1
-        if solve(game_idx + 1):
-            return True
-        # 백트래킹 (원상 복구)
-        records[team1][2] += 1
-        records[team2][0] -= 1
     
-    # 세 가지 선택 모두 실패했다면 불가능
+    i, j = game[idx]
+    wi, ti, li = team_result[i]
+    wj, tj, lj = team_result[j]
+    # i팀이 이겼을 경우
+    if wi > 0 and lj > 0:
+        team_result[i][0] -= 1
+        team_result[j][2] -= 1
+        if backtracking(idx+1, game, team_result):
+            return True
+        team_result[i][0] += 1
+        team_result[j][2] += 1
+    # i팀과 j팀이 비겼을 경우
+    if ti > 0 and tj > 0:
+        team_result[i][1] -= 1
+        team_result[j][1] -= 1
+        if backtracking(idx+1, game, team_result):
+            return True
+        team_result[i][1] += 1
+        team_result[j][1] += 1
+    # i팀이 졌을 경우
+    if li > 0 and wj > 0:
+        team_result[i][2] -= 1
+        team_result[j][0] -= 1
+        if backtracking(idx+1, game, team_result):
+            return True
+        team_result[i][2] += 1
+        team_result[j][0] += 1
     return False
+def solution():
+    # 가능한 결과가 되려면 승과 패의 합이 같아야 하고 무승부의 총합이 짝수여야 한다.
+    for _ in range(4):
+        result = list(map(int, input().split()))
+        # 나라별로
+        team_result = [result[3*i:3*i+3] for i in range(6)]
 
-# 15개의 모든 경기 조합을 미리 생성
-games = list(combinations(range(6), 2))
-answers = []
-
-# 4개의 테스트 케이스에 대해 반복
-for _ in range(4):
-    input_data = list(map(int, sys.stdin.readline().split()))
-    
-    # 입력을 [승, 무, 패] 6팀의 2차원 리스트로 변환
-    records = [input_data[i*3 : i*3+3] for i in range(6)]
-    
-    # 1. 간단한 조건으로 미리 확인
-    is_valid = True
-    total_wins = sum(r[0] for r in records)
-    total_draws = sum(r[1] for r in records)
-    total_losses = sum(r[2] for r in records)
-
-    # 총 승수 != 총 패수 이거나, 총 무승부가 홀수이면 불가능
-    if total_wins != total_losses or total_draws % 2 != 0:
-        is_valid = False
-
-    # 각 팀의 (승+무+패)가 5가 아니면 불가능
-    if is_valid:
-        for r in records:
-            if sum(r) != 5:
-                is_valid = False
+        # '각 나라의 승+무+패 != 5' or '모든 나라의 총 승 != 총 패 or 무승부의 개수가 홀수'면 불가능
+        total_win, total_tie, total_lose = 0, 0, 0
+        for i in range(6):
+            if sum(team_result[i]) != 5:
+                print(0, end=' ')
                 break
-    
-    # 2. 간단한 조건을 통과했다면 백트래킹으로 최종 확인
-    if is_valid and solve(0):
-        answers.append("1")
-    else:
-        answers.append("0")
+            total_win += team_result[i][0]
+            total_tie += team_result[i][1]
+            total_lose += team_result[i][2]
 
-print(" ".join(answers))
+        else:
+            if (total_win != total_lose) or (total_tie%2 != 0):
+                print(0, end=' ')
+                continue
+
+            # 백트레킹으로 15개의 모든 경기 탐색
+            game = list(combinations(range(6), 2))
+            if backtracking(0, game, team_result):
+                print(1, end=' ')
+            else:
+                print(0, end=' ')
+
+solution()
