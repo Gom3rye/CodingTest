8273a8691bd4ed8c140b12417fbdf1bb6a2d782a
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-25 19:58:22 +0900

    [Gold IV] Title: 미친 로봇, Time: 784 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.py
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.py
@@ -3,29 +3,21 @@ input = sys.stdin.readline
 def solution():
     n, ep, wp, sp, np = map(int, input().split()) # 이동횟수, 동,서,남,북으로 이동할 확률
     probs = [ep/100, wp/100, sp/100, np/100]
-    directions = [(0,1),(0,-1),(1,0),(-1,0)] # 동북서남
     visited = [[False]*29 for _ in range(29)] # 동북서남으로 14칸씩 갈 수 있으니까
     total_prob = 0.0 # probabilities of simple paths
     def backtracking(idx, x, y, prob):
         nonlocal total_prob
+        if visited[x][y]: # 이미 방문한 곳이라면 simple path 못되니까 가지치기
+            return
         if idx == n:
             total_prob += prob
             return
-        
-        for i, (dx, dy) in enumerate(directions):
-            nx, ny = dx+x, dy+y
-            # 갈 확률이 0이면 패쓰
-            if probs[i] == 0:
-                continue
-            # 이미 간 곳이면 패쓰 (simple path가 되기 위해서는 안 간 곳만 가야 함)
-            if visited[nx][ny]:
-                continue
-            # 이동
-            visited[nx][ny] = True
-            backtracking(idx+1, nx, ny, prob*probs[i])
-            visited[nx][ny] = False
-
-    visited[14][14] = True # make start point True
-    backtracking(0, 14, 14, 1.0) # 첫 시작 좌표는 (14, 14)로 중간에서 시작, initial prob = 1.0
+        visited[x][y] = True
+        backtracking(idx+1, x, y+1, prob*probs[0])
+        backtracking(idx+1, x, y-1, prob*probs[1])
+        backtracking(idx+1, x+1, y, prob*probs[2])
+        backtracking(idx+1, x-1, y, prob*probs[3])
+        visited[x][y] = False
+    backtracking(0, 14, 14, 1.0)
     print(total_prob)
 solution()
