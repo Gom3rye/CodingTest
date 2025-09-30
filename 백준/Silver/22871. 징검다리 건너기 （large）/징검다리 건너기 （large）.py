63cf6b9ea48f6e5ee6db94201855ca94f6231e5f
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-30 16:44:32 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 3088 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
@@ -1,16 +1,14 @@
 import sys
 input = sys.stdin.readline
- 
-n = int(input())
-stones = list(map(int, input().split()))
- 
-def power(i, j):
-    return (i - j)*(1 + abs(stones[i]-stones[j]))
- 
-dp = [int(1e9)] * n
-dp[0] = 0
-for i in range(1, n):
-    for j in range(i):
-        # j번째 돌을 밟고 i번째 돌로 가기
-        dp[i] = min(max(dp[j], power(i, j)), dp[i])
-print(dp[n-1])
+def solution():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    INF = float('inf')
+    dp = [INF]*n # dp[i]: 0번 돌에서 i번 돌까지 가는 최소 비용
+    dp[0] = 0 # 시작점 비용은 0
+    for i in range(n):
+        for j in range(i+1, n):
+            k = (j-i)*(1+abs(arr[i]-arr[j]))
+            dp[j] = min(dp[j], max(dp[i], k))
+    print(dp[-1])
+solution()
