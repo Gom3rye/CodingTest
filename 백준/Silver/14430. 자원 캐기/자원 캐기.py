3ffb028889e4788542217a0f4014dab83deda24f
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-30 11:15:22 +0900

    [Silver II] Title: 자원 캐기, Time: 60 ms, Memory: 34720 KB -BaekjoonHub

--- 백준/Silver/14430. 자원 캐기/자원 캐기.py
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -1,28 +1,11 @@
 import sys
-
-# 입력을 빠르게 받기 위한 설정
 input = sys.stdin.readline
-
-# --- 입력 처리 ---
-N, M = map(int, input().split())
-
-# DP 테이블과 광석 정보를 저장할 그리드
-# (N+1) x (M+1) 크기로 만들어 0-based 인덱싱의 번거로움을 피함
-grid = [[0] * (M + 1)]
-for _ in range(N):
-    grid.append([0] + list(map(int, input().split())))
-
-# dp[i][j]: (1,1)에서 (i,j)까지 이동하며 얻는 최대 자원 수
-dp = [[0] * (M + 1) for _ in range(N + 1)]
-
-# --- DP 계산 ---
-# (1,1)부터 (N,M)까지 순회하며 DP 테이블을 채움
-for i in range(1, N + 1):
-    for j in range(1, M + 1):
-        # 위쪽에서 오는 경우와 왼쪽에서 오는 경우 중 더 큰 값을 선택하고,
-        # 현재 위치의 자원을 더함
-        max_from_prev = max(dp[i-1][j], dp[i][j-1])
-        dp[i][j] = max_from_prev + grid[i][j]
-
-# 최종 목적지 (N, M)에 저장된 값이 정답
-print(dp[N][M])
+def solution():
+    n, m = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0]*(m+1) for _ in range(n+1)] # i,j까지 오면서 얻을 수 있는 최대 자원 수
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])+board[i-1][j-1]
+    print(dp[-1][-1])
+solution()
