37447c49912a19b8d2848669ac0cda34f7c7fc36
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-30 15:08:28 +0900

    [Silver I] Title: 기타리스트, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.py
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -3,19 +3,24 @@ input = sys.stdin.readline
 def solution():
     n, s, m = map(int, input().split()) # 곡 수, 시작 볼륨, 최대 볼륨
     v = list(map(int, input().split()))
-    dp = {s}
-    for vol in v:
-        nxt_dp = set()
-        for volume in dp:
-            up = volume+vol
-            down = volume-vol
-            if 0<=up<=m:
-                nxt_dp.add(up)
-            if 0<=down<=m:
-                nxt_dp.add(down)
-        dp = nxt_dp
-    if dp:
-        print(max(dp))
+    dp = [[False]*(m+1) for _ in range(n+1)] # dp[0]에서 시작해서 dp[n]까지 가야 하니까
+    # dp[i][vol]: i번째 곡을 시작할 때 vol이 가능한지 여부
+    dp[0][s] = True
+    for i in range(n):
+        for vol in range(m+1):
+            if dp[i][vol]:
+                # up
+                if vol+v[i] <= m:
+                    dp[i+1][vol+v[i]] = True
+                # down
+                if vol-v[i] >= 0:
+                    dp[i+1][vol-v[i]] = True
+
+    for vol in range(m, -1, -1):
+        if dp[n][vol]:
+            print(vol)
+            break
     else:
         print(-1)
+
 solution()
