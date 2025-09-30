4fe34b2727aa4088521f555bd40890506dd36e8c
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-09-30 16:51:33 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 3060 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
@@ -6,7 +6,7 @@ def solution():
     INF = float('inf')
     dp = [INF]*n # dp[i]: 0번 돌에서 i번 돌까지 가는 최소 비용
     dp[0] = 0 # 시작점 비용은 0
-    for i in range(n):
+    for i in range(n-1):
         for j in range(i+1, n):
             k = (j-i)*(1+abs(arr[i]-arr[j]))
             dp[j] = min(dp[j], max(dp[i], k))
