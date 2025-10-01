37e197a378aef7f5d6c08f42c1958a004df61cdd
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-01 13:21:40 +0900

    [Silver II] Title: 민균이의 계략, Time: 40 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
@@ -1,13 +1,19 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
 def solution():
     n = int(input())
     arr = list(map(int, input().split()))
     # 순증가 최장 길이 구하기 (최장 증가 부분 수열 LIS)
-    dp = [1]*n # dp[i]: i까지의 LIS
-    for i in range(1, n):
-        for j in range(i):
-            if arr[i] > arr[j]:
-                dp[i] = max(dp[i], dp[j]+1)
-    print(max(dp))
+    lis = [] # 현재까지 만들어진 LIS의 가장 작은 꼬리값들을 저장
+    for num in arr:
+        idx = bisect_left(lis, num)
+        # print(idx)
+        if idx == len(lis):
+            lis.append(num)
+            # print(lis)
+        else:
+            lis[idx] = num
+    print(len(lis))
+    # print(lis)
 solution()
