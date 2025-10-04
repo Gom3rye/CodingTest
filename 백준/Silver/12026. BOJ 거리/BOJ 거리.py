921d103b62aec2f71eee8304a9e08e4b6464add8
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-04 13:36:12 +0900

    [Silver I] Title: BOJ 거리, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.py
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -1,20 +1,15 @@
-INF = float('inf')
-
-N = int(input())
-board = input().strip()
-
-# B -> 0, O -> 1, J -> 2 로 매핑
-def next_char(c):
-    return {'B': 'O', 'O': 'J', 'J': 'B'}[c]
-
-dp = [INF] * N
-dp[0] = 0  # 시작점
-
-for i in range(N):
-    for j in range(i + 1, N):
-        if board[j] == next_char(board[i]):
-            cost = (j - i) ** 2
-            dp[j] = min(dp[j], dp[i] + cost)
-
-# 결과
-print(dp[N - 1] if dp[N - 1] != INF else -1)
+import sys
+input = sys.stdin.readline
+def solution():
+    n = int(input())
+    s = input().strip()
+    dp = [float('inf')]*n # dp[i]: i번째 블럭을 갔을 때의 드는 최소 에너지
+    dp[0] = 0
+    rule = {"B":0, "O":1, "J":2}
+    for i in range(1, n): # i칸까지 오는 최소 비용 dp[i] 구하기
+        for j in range(i): # 이전에 도달 가능한 칸들 j에서 i로 점프할 수 있는지 확인하고 가능하면 dp[i] 업데이트
+            if (rule[s[j]]+1) % 3 != rule[s[i]]:
+                continue
+            dp[i] = min(dp[i], dp[j]+(i-j)**2)
+    print(dp[-1] if dp[-1] != float('inf') else -1)
+solution()
