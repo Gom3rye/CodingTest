90497193c7bca40973d43c86a6210e21dd363a68
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-04 13:38:26 +0900

    [Silver I] Title: BOJ 거리, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.py
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -2,13 +2,19 @@ import sys
 input = sys.stdin.readline
 def solution():
     n = int(input())
-    s = input().strip()
+    s = list(input().strip())
+    for i in range(n):
+        if s[i] == 'B':
+            s[i] = 0
+        elif s[i] == 'O':
+            s[i] = 1
+        else:
+            s[i] = 2
     dp = [float('inf')]*n # dp[i]: i번째 블럭을 갔을 때의 드는 최소 에너지
     dp[0] = 0
-    rule = {"B":0, "O":1, "J":2}
     for i in range(1, n): # i칸까지 오는 최소 비용 dp[i] 구하기
         for j in range(i): # 이전에 도달 가능한 칸들 j에서 i로 점프할 수 있는지 확인하고 가능하면 dp[i] 업데이트
-            if (rule[s[j]]+1) % 3 != rule[s[i]]:
+            if (s[j]+1) % 3 != s[i]:
                 continue
             dp[i] = min(dp[i], dp[j]+(i-j)**2)
     print(dp[-1] if dp[-1] != float('inf') else -1)
