7e43fb5cddec3193c36a1b54f574645918b1ba0e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-06 17:08:28 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 5004 ms, Memory: 240332 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,13 +1,18 @@
-n = int(input())
-arr = list(map(int, input().split()))
- 
-# arr i번째까지의 수열과 arr의 역순수열의 j번째까지의 수열의 최장공통부분수열(LCS) 길이 저장
-dp = [[0] * (n+1) for _ in range(n+1)]  
- 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if arr[i-1] == arr[n-j]:
-            dp[i][j] = dp[i-1][j-1] + 1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-print(n-dp[n][n])
+import sys
+input = sys.stdin.readline
+def solution():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    rra = arr[::-1]
+    # 팰린드롬 만들기위해 필요한 최소 개수 = len(arr)-LCS
+    # LCS 계산을 위한 DP 테이블 초기화 (크기는 (n+1)x(n+1))
+    dp = [[0]*(n+1) for _ in range(n+1)] # dp[i][j]: arr의 i번째까지와 rra의 j번째까지의 LCS 길이
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            # 원본 수열의 i번째 문자와 뒤집은 수열의 j번째 문자가 같다면
+            if arr[i-1] == rra[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    print(n-dp[-1][-1])
+solution()
