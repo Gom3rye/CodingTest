d69dfdaafa96aad36fc45194f903712f235eaaa3
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-08 18:34:55 +0900

    [Gold III] Title: 수확, Time: 940 ms, Memory: 141524 KB -BaekjoonHub

--- 백준/Gold/1823. 수확/수확.py
+++ 백준/Gold/1823. 수확/수확.py
@@ -1,17 +1,23 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000000)
-
-n = int(input())
-rice = [int(input()) for i in range(n)]
-
-dp =[[0 for i in range(n)] for i in range(n)]
-def get_maxvalue(s, e, cnt):
-    # base case
-    if s==e : return cnt*rice[s]
-    if dp[s][e] : return dp[s][e]
-    # step
-    dp[s][e] = max(get_maxvalue(s+1, e, cnt+1)+cnt*rice[s], get_maxvalue(s, e-1, cnt+1)+cnt*rice[e])
-    return dp[s][e]
-
-print(get_maxvalue(0, n-1, 1))
+def solution():
+    n = int(input())
+    rice = [int(input()) for _ in range(n)]
+    # 가장자리에만 벼를 수확해서 얻는 최대 이익
+    dp = [[0]*n for _ in range(n)] # dp[i][j]: i번째부터 j번째 벼가 남았을 때의 최대 이익
+    for length in range(1, n+1): # length: 계산할 구간의 길이 (남아있는 벼의 개수)
+        # 시작점
+        for i in range(n-length+1):
+            # 끝점
+            j = i+length-1
+            # k: 현재 수확 순서
+            k = n-(j-i) # (전체 n개 - 남은 length개)만큼 수확했으므로 +1
+            if i == j:
+                dp[i][j] = rice[i]*k
+                continue
+            # 왼쪽, 오른쪽 수확
+            left = rice[i]*k+dp[i+1][j]
+            right = rice[j]*k+dp[i][j-1]
+            dp[i][j] = max(left, right)
+    print(dp[0][n-1])
+solution()
