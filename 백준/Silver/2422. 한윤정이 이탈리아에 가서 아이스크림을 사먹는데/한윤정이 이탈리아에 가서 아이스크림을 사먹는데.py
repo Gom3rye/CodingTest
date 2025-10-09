5ccfbab5ede8e82a1dbf014d7ea06fea5c2d2fbf
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-09 18:43:30 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -3,21 +3,20 @@ input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split()) # n: 아이스크림 종류의 수, m: 섞어먹으면 안되는 조합의 수
     cnt = 0
-    forbidden = [set() for _ in range(n+1)]
+    forbidden = [[False]*(n+1) for _ in range(n+1)]
     for _ in range(m):
         a, b = map(int, input().split())
-        forbidden[a].add(b)
-        forbidden[b].add(a) # 금지 관계는 서로 양방향인거 주의하기!
-    # print(forbidden)
+        forbidden[a][b] = True
+        forbidden[b][a] = True # 금지 관계는 서로 양방향인거 주의하기!
     # 1-23, 3-4끼리 섞어 먹으면 안된다. -> 145 235 245
     for i in range(1, n-1):
         for j in range(i+1, n):
-            if j in forbidden[i]:
+            if forbidden[i][j]:
                 continue
             for k in range(j+1, n+1):
-                if k in forbidden[i]:
+                if forbidden[i][k]:
                     continue
-                if k in forbidden[j]:
+                if forbidden[j][k]:
                     continue
                 cnt += 1
                 # print(i, j, k)
