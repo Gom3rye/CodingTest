9902ff0f0b3a4cc4692e9d101d5a41814d95d4c3
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-09 20:05:01 +0900

    [Silver IV] Title: DNA, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -5,28 +5,15 @@ def solution():
     n, m = map(int, input().split()) # dna 수, 문자열의 길이
     dnas = [input().strip() for _ in range(n)]
     answer = []
+    hamming_distance = 0
     for j in range(m):
         count = defaultdict(int) # 기본으로 0값
         for i in range(n):
             count[dnas[i][j]] += 1
-        # 가장 많이 나온 알파벳 채택(동점이면 사전순으로 가장 앞서는 것)
-        max_cnt = -1
-        result_alpha = 'Z'
-        for alpha, cnt in count.items():
-            if cnt > max_cnt:
-                max_cnt = cnt
-                result_alpha = alpha
-            elif cnt == max_cnt and result_alpha > alpha:
-                result_alpha = alpha # 사전 순 정렬
-        answer.append(result_alpha)
-    
-    hamming_distance = 0
-    for j in range(m):
-        d = 0
-        for i in range(n):
-            if dnas[i][j] != answer[j]:
-                d += 1
-        hamming_distance += d
-    print(''.join(answer))
+        max_char = max(count, key=lambda x: (count[x], -ord(x))) # ord: 문자->숫자 (사전 순으로 정렬하기 위해서 -붙여주기)
+        hamming_distance += (n - count[max_char])
+        # 또는 hamming_distance += sum(count[x] for x in count if x != max_char)
+        answer.append(max_char)
+    print("".join(answer))
     print(hamming_distance)
 solution()
