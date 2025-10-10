f7fca8c108d7fa5bd7c3c4cf27c726f8890a6063
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-10 15:05:07 +0900

    [Gold V] Title: A와 B 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,15 +1,27 @@
 import sys
-S=list(input())
-T=list(input())
-def dfs(t): # 문자열 T 리스트를 입력받아
-    if t==S:
+input = sys.stdin.readline
+def solution():
+    start = input().strip()
+    target = input().strip()
+    # 이미 완성된 target -> start로 가는게 start -> target으로 가는 것보다 쉬우므로 반대로 계산
+    def dfs(t):
+        if t == start:
+            return True
+        if len(t) < len(start): # t길이가 start보다 짧아지면 실패
+            return False
+        # A로 끝나는 경우
+        if t[-1] == 'A':
+            if dfs(t[:-1]):
+                return True # 'A'추가 -> 'A' 빼고 다시 호출
+        # B로 시작하는 경우 (B를 끝에 추가하고 뒤집은 상태니까 B는 처음에 있을 것)
+        if t[0] == 'B':
+            if dfs(t[::-1][:-1]): # 'B'추가, 뒤집기 -> 뒤집기, B빼기
+                return True
+        
+        return False
+
+    if dfs(target):
         print(1)
-        sys.exit()
-    if len(t)==0:
-        return 0
-    if t[-1]=='A': # 마지막이 A이면 
-        dfs(t[:-1]) # 제거해서 재귀
-    if t[0]=='B': # 처음이 B이면
-        dfs(t[1:][::-1]) # B제거하고, 뒤집어서 재귀
-dfs(T)
-print(0)
+    else:
+        print(0)
+solution()
