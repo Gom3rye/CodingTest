d3da45babf447678120b6f78e9125e0b1372ae6e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-11 16:24:59 +0900

    [Silver V] Title: 수 이어가기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,15 +1,18 @@
-N = int(input())
-result = []
-for i in range(1, N+1):          # 양의 정수 1부터 N을 포함하는 정수 들 (!!N포함 중요 안할 시 틀림)
-    tmp = [N, i]
-    while True:
-        val = tmp[-2] - tmp[-1]  # 다음 값은 인덱스 -2에서 -1을 뺀 값
-        if val >= 0:             # 이 값이 정수라면 넣어주고
-            tmp.append(val)
-        else:                    # 아니면 break
-            break
-    if len(tmp) > len(result):   # tmp의 길이가 result보다 클 시
-        result = tmp
-print(len(result))
-for i in result:
-    print(i, end=' ')
+import sys
+input = sys.stdin.readline
+def solution():
+    n = int(input())
+    answer = []
+    for second in range(1, n+1): # 가능한 모든 두 번째 수 시도(조건이 양의 정수이므로 두 번째 수는 첫 번째 수보다 작거나 같아야 한다.)
+        seq = [n, second]
+        while True:
+            third = seq[-2] - seq[-1]
+            if third >= 0:
+                seq.append(third)
+            else:
+                break
+        if len(answer) < len(seq):
+            answer = seq
+    print(len(answer))
+    print(*answer)  
+solution()
