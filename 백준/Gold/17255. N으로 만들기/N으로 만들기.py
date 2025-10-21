647ff13c4f26fd80d941e627b1f2e0a285e75234
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-21 20:20:06 +0900

    [Gold IV] Title: N으로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+def solution():
+    n = input().strip()
+    l = len(n)
+    answer = []
+
+    def dfs(s, left, right, process): # string, left, right index, process
+        if s == n and left == 0 and right == l-1:
+            if process not in answer:
+                answer.append(process)
+            return
+        
+        if left > 0: # 왼쪽으로 갈 수 있음
+            dfs(n[left-1]+s, left-1, right, process+n[left-1]+s)
+        if right < l-1: # 오른쪽으로 갈 수 있음
+            dfs(s+n[right+1], left, right+1, process+s+n[right+1])
+
+    for i in range(l):
+        dfs(n[i], i, i, n[i]) # 초반에는 left, right 인덱스를 같이 주기
+    print(len(answer))
+solution()
