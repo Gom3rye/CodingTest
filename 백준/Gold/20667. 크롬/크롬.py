7e7532310b377884c52ff2694796db9353967973
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-26 16:44:04 +0900

    [Gold I] Title: 크롬, Time: 2400 ms, Memory: 37968 KB -BaekjoonHub

--- 백준/Gold/20667. 크롬/크롬.py
+++ 백준/Gold/20667. 크롬/크롬.py
@@ -1,53 +1,35 @@
 import sys
-
 input = sys.stdin.readline
-INF = float('inf')
+def solution():
+    n, m, k = map(int, input().split()) # 총 크롬 탭 수, 목표 cpu 사용량, 목표 메모리 할당량
+    # dp[cpu][mem] = pri 로 하면 m이 최대 십만, c가 천 -> 10^8로 메모리 초과 및 시간 초과
+    # -> 차원을 뒤집어서 m을 상태에서 빼고 값으로 관리하자. dp[pri][cpu] = max mem
+    # O(N*P*M) DP 떠올리기, => 최소 pri를 구하는 문제 -> 중요도 합 pri가 주어졌을 때 cpu m이상, mem k이상을 만족할 수 있는가로 뒤집기
+    tabs = []
+    total_pri = 0
+    for _ in range(n):
+        cpu, mem, pri = map(int, input().split())
+        total_pri += pri
+        tabs.append((cpu, mem, pri))
+    # 도달 불가능한 상태(-1)로 초기화 dp[pri][cpu] = max mem
+    dp = [[-1]*(m+1) for _ in range(total_pri+1)]
+    # 초기화
+    dp[0][0] = 0
+    for cpu, mem, pri in tabs:
+        for p in range(total_pri, pri-1, -1):
+            # 이전 상태를 순회하는 c는 0~m까지의 모든 상태를 확인해야 한다.
+            for c in range(m, -1, -1):
+                if dp[p-pri][c] != -1: # 도달 가능 상태라면
+                    # c+cpu가 m을 넘어가면 모두 m번 인덱스에 저장
+                    new_c = min(m, c+cpu)
+                    # max(현재값, 이전 상태에서 탭을 추가한 값)
+                    dp[p][new_c] = max(dp[p][new_c], dp[p-pri][c]+mem)
 
-n, m, k = map(int, input().split())
-tabs = []
-max_p_sum = 0
-for _ in range(n):
-    c, mem, p = map(int, input().split())
-    tabs.append((c, mem, p))
-    max_p_sum += p # 가능한 최대 중요도 합
+    ans = float('inf')
+    for p in range(total_pri+1):
+        if dp[p][m] >= k:
+            ans = p
+            break # 최소 p를 찾았으므로 즉시 종료
 
-# dp[p][c] = max_memory
-# "중요도 합 p, CPU *최소* c를 확보했을 때의 최대 메모리"
-# -1은 도달 불가능한 상태를 의미
-dp = [[-1] * (m + 1) for _ in range(max_p_sum + 1)]
-
-# 초기값: 중요도 0, CPU 0일 때 메모리 0 확보
-dp[0][0] = 0
-
-# 0/1 냅색 (3중 반복문)
-for cpu, mem, pri in tabs:
-    # 1. 중요도 (역방향)
-    for p in range(max_p_sum, pri - 1, -1):
-        # 2. CPU (역방향)
-        # 0부터 M까지 모든 상태를 확인
-        for c in range(m, -1, -1):
-            
-            # 이전 상태 (p - pri, c)가 도달 가능했다면
-            if dp[p - pri][c] != -1:
-                
-                # "최소 M" 트릭: 
-                # c + cpu가 M을 넘어가면 모두 M번 인덱스에 저장
-                new_c = min(m, c + cpu)
-                
-                # 점화식: max(현재 값, 이전 상태에서 탭을 추가한 값)
-                dp[p][new_c] = max(dp[p][new_c], dp[p - pri][c] + mem)
-
-# 정답 찾기
-min_ans = INF
-
-# 중요도 p를 0부터 순회 (최소 p를 찾아야 하므로)
-for p in range(max_p_sum + 1):
-    # dp[p][m] : 중요도 p, CPU '최소' m을 만족하는 상태
-    if dp[p][m] >= k:
-        min_ans = p
-        break # 최소 p를 찾았으므로 즉시 종료
-
-if min_ans == INF:
-    print(-1)
-else:
-    print(min_ans)
+    print(ans if ans != float('inf') else -1)           
+solution()
