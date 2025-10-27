c090a9cb695929d78946a632431302e5ae42c183
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-27 13:00:39 +0900

    [Silver I] Title: 나무꾼 이다솜, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
@@ -1,20 +1,25 @@
-N, C, W = map(int, input().split())
-lengths = [int(input()) for _ in range(N)]
-
-max_len = max(lengths)
-max_profit = 0
-
-for L in range(1, max_len + 1):
-    profit = 0
-    for length in lengths:
-        pieces = length // L
-        if pieces == 0:
-            continue
-        cuts = pieces if length % L != 0 else pieces - 1
-        money = pieces * L * W
-        cost = cuts * C
-        if money > cost:
-            profit += (money - cost)
-    max_profit = max(max_profit, profit)
-
-print(max_profit)
+import sys
+input = sys.stdin.readline
+def solution():
+    n, c, w = map(int, input().split()) # 나무의 개수, 자르는 비용, 한 단위의 가격
+    # 버는 돈: k(개)*l(길이)*w(한 단위 가격) - c*m(자르는 횟수) -> 이를 위해 알아야 하는 것: 나무의 길이(길이가 정해지면 개수와 자르는 횟수도 정해진다.)
+    trees = [int(input()) for _ in range(n)]
+    max_len = max(trees)
+    answer = 0
+    # 길이만 정하면 되니까 완탐으로 구하기
+    for l in range(1, max_len+1): # <= 10000
+        total_profit = 0
+        for tree in trees: # <= 50 따라서 완탐 가능
+            k = tree//l # 개수
+            if k == 0: # 1조각도 안 나오면 스킵
+                continue
+            if tree%l == 0: # 딱 길이로 나눠떨어지면
+                m = k-1 # 자르는 횟수
+            else:
+                m = k
+            profit = k*l*w - c*m
+            if profit > 0:
+                total_profit += profit
+        answer = max(answer, total_profit)
+    print(answer)
+solution()
