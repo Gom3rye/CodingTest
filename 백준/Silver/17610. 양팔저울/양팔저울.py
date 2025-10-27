e48ef6d184199f374814d43d7e6ad4e4f2a96718
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-27 20:36:31 +0900

    [Silver I] Title: 양팔저울, Time: 212 ms, Memory: 125096 KB -BaekjoonHub

--- 백준/Silver/17610. 양팔저울/양팔저울.py
+++ 백준/Silver/17610. 양팔저울/양팔저울.py
@@ -4,7 +4,6 @@ def solution():
     k = int(input()) # 추의 개수
     gs = list(map(int, input().split()))
     total = sum(gs) # <=13*20만(260만)
-    cant = 0 # 측정할 수 없는 경우의 수
     dp = set([0])
     for g in gs:
         temp = set()
@@ -12,8 +11,5 @@ def solution():
             temp.add(dp_v+g)
             temp.add(abs(dp_v-g))
         dp.update(temp)
-    for i in range(1, total+1):
-        if i not in dp:
-            cant += 1
-    print(cant)
+    print(total-len(dp)+1)
 solution()
