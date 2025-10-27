b5af28613d6be3515ca739a13475fc755e995214
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-10-27 13:34:18 +0900

    [Silver I] Title: 피아노 체조, Time: 180 ms, Memory: 44820 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.py
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.py
@@ -1,21 +1,19 @@
 import sys
 input = sys.stdin.readline
-
-N = int(input())
-A = list(map(int, input().split()))
-Q = int(input())
-
-mistake = [0] * N
-for i in range(N - 1):
-    if A[i] > A[i + 1]:
-        mistake[i + 1] = 1  # i와 i+1 사이에서 실수 발생
-
-# 누적합 계산
-prefix = [0] * (N)
-for i in range(1, N):
-    prefix[i] = prefix[i - 1] + mistake[i]
-
-# 쿼리 처리
-for _ in range(Q):
-    x, y = map(int, input().split())
-    print(prefix[y - 1] - prefix[x - 1])
+def solution():
+    n = int(input()) # 악보 개수
+    levels = [0]+list(map(int, input().split())) # 난이도 1based index
+    q = int(input()) # 질문
+    # n <= 십만으로 크므로 누적합 쓰기!
+    mistakes = [0]*(n+1)
+    for i in range(1, n+1):
+        if levels[i] < levels[i-1]:
+            mistakes[i] = 1
+    prefix_sum = [0]*(n+1)
+    for i in range(1, n+1): # 1based index
+        prefix_sum[i] = prefix_sum[i-1]+mistakes[i]
+    for _ in range(q):
+        x, y = map(int, input().split())
+        mistake = prefix_sum[y]-prefix_sum[x]
+        print(mistake)
+solution()
