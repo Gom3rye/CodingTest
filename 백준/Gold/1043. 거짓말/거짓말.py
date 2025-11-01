592e33d4924fa05805ff355519278d2d2dfc13fd
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-01 13:38:38 +0900

    [Gold IV] Title: 거짓말, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -1,41 +1,50 @@
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+import sys
+input = sys.stdin.readline
+def solution():
+    # 진실을 아는 사람과 연결되지 않은 파티들만 거짓말 가능
+    # 진실을 아는 사람들 연결 -> Union Find 문제!!
+    n, m = map(int, input().split()) # 사람 수, 파티 수
+    knows = list(map(int, input().split()))
+    if knows[0] == 0:
+        print(m)
+        return
+    else:
+        knows = set(knows[1:])
+    parents = list(range(n+1)) # 1based index니까
+    def find(x):
+        if parents[x] != x:
+            parents[x] = find(parents[x])
+        return parents[x]
+    def union(a, b):
+        pa = find(a)
+        pb = find(b)
+        if pa < pb:
+            parents[pb] = pa
+        else:
+            parents[pa] = pb
+            
+    parties = []
+    for _ in range(m):
+        ppl = list(map(int, input().split()))[1:]
+        parties.append(ppl)
+        # 같은 파티 사람들은 모두 연결
+        for i in range(len(ppl)-1):
+            union(ppl[i], ppl[i+1])
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a != b:
-        parent[b] = a
-
-# 입력
-N, M = map(int, input().split())
-data = list(map(int, input().split()))
-truth_count = data[0]
-truth_people = data[1:]
-
-parties = []
-parent = [i for i in range(N+1)]
-
-# 파티 정보 입력 및 유니온 처리
-for _ in range(M):
-    arr = list(map(int, input().split()))
-    people = arr[1:]
-    parties.append(people)
-    # 같은 파티 사람들은 모두 연결
-    for i in range(len(people) - 1):
-        union(people[i], people[i+1])
-
-# 진실을 아는 집합의 대표자들
-truth_roots = set(find(p) for p in truth_people)
-
-# 각 파티마다 검사
-ans = 0
-for party in parties:
-    # 진실 집합과 연결된 사람 있는지 확인
-    if any(find(p) in truth_roots for p in party):
-        continue  # 진실 말해야 함
-    ans += 1  # 거짓말 가능
-
-print(ans)
+    # 각각의 부모들 업데이트
+    for i in range(1,n+1):
+        parents[i] = find(i)
+    # print(parents)
+    new_knows = []
+    for i in knows:
+        new_knows.append(find(i))
+    # print(parents)
+    cnt = 0
+    for party in parties:
+        for p in party:
+            if parents[p] in new_knows:
+                break
+        else:
+            cnt += 1
+    print(cnt)
+solution()
