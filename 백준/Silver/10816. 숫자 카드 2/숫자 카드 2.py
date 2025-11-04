1af804b268c5af115775544c0281909c29e0bcc4
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-04 19:12:05 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 648 ms, Memory: 116032 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,14 +1,14 @@
 import sys
-from bisect import bisect_left, bisect_right
+from collections import Counter
 input = sys.stdin.readline
 def solution():
     n = int(input()) # <= 500,000
-    nums = sorted(map(int, input().split()))
+    nums = list(map(int, input().split()))
     m = int(input()) # <= 500,000
     cnts = list(map(int, input().split()))
+    counter = Counter(nums)
     answer = [0]*m
     for i in range(m):
-        number = cnts[i]
-        answer[i] = bisect_right(nums, number)-bisect_left(nums, number)
+        answer[i] = counter[cnts[i]]
     print(*answer)
 solution()
