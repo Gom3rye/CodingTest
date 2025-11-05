2ef1856c9ccd9b3e1b2cc2e9a5a793acc77753a6
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-05 20:31:05 +0900

    [Silver I] Title: 물약 구매, Time: 8352 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/24954. 물약 구매/물약 구매.py
+++ 백준/Silver/24954. 물약 구매/물약 구매.py
@@ -1,26 +1,45 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 def solution():
-    n = int(input()) # 물약 <=10 (10! ~> 3백만, 완탐 가능)
-    costs = list(map(int, input().split())) # 가격 <=1000
-    potions = [[] for _ in range(n)] # potion[i] = (a, d) 포션i를 사면 포션a를 d만큼 할인해준다.
+    n = int(input())
+    cost = list(map(int, input().split()))
+    discount = [[] for _ in range(n)]
     for i in range(n):
-        m = int(input()) # 할인 정보 <=n-1
-        if m == 0:
-            continue
-        for _ in range(m):
-            a, d = map(int, input().split()) # 물약번호, 할인되는 가격
-            potions[i].append((a-1, d)) # potion index, discount
-    
-    min_cost = float('inf')
-    for perm in permutations(range(n), n):
-        cost = 0
-        new_costs = costs[:]
-        for p in perm:
-            cost += new_costs[p]
-            for idx, discount in potions[p]:
-                new_costs[idx] = max(1, new_costs[idx]-discount)
-        min_cost = min(min_cost, cost)
-    print(min_cost)
+        p = int(input())
+        for _ in range(p):
+            a, d = map(int, input().split())
+            discount[i].append((a - 1, d))
+
+    min_total = float('inf')
+
+    def dfs(bought, cur_costs, total):
+        nonlocal min_total
+        
+        # 모든 물약을 구매한 경우
+        if len(bought) == n:
+            min_total = min(min_total, total)
+            return
+        
+        # 가지치기 (현재까지의 합이 이미 최소값보다 크면 의미 없음)
+        if total >= min_total:
+            return
+
+        for i in range(n):
+            if i in bought:
+                continue
+
+            # i번째 물약 구매
+            new_cost = cur_costs[i]
+            new_total = total + new_cost
+
+            # 할인 적용
+            next_costs = cur_costs[:]
+            for (a, d) in discount[i]:
+                next_costs[a] = max(1, next_costs[a] - d)
+
+            dfs(bought + [i], next_costs, new_total)
+
+    dfs([], cost, 0)
+    print(min_total)
+
 solution()
