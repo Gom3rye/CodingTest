0e4f611978fa39ec07fdfae8e39e24cecf977a0d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-05 19:31:55 +0900

    [Silver II] Title: 나무 자르기, Time: 380 ms, Memory: 150120 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,21 +1,22 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import Counter
 input = sys.stdin.readline
 def solution():
     n, m = map(int, input().split()) # 나무수 <=백만, 나무길이 <=2억
     arr = list(map(int, input().split()))
+    counter = Counter(arr)
     start, end = 0, max(arr)
     answer = 0
     while start <= end:
         total = 0 # 총 나무 길이
         mid = (start+end)//2
-        for height in arr:
+        for height, cnt in counter.items():
             if height > mid:
-                total += (height-mid)
+                total += (height-mid)*cnt
         
         if total >= m: # 필요한 나무 미터보다 큰 경우
-            answer = mid
-            start = mid+1
+            answer = mid # 일단 mid 결과로 저장
+            start = mid+1 # 더 큰 것도 가능한지 보기
         elif total < m:
             end = mid-1
     print(answer)
