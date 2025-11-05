174f50b617a5c05e4080c500a9ae68f8d5c8cdf9
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-05 20:44:16 +0900

    [Silver I] Title: 물약 구매, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/24954. 물약 구매/물약 구매.py
+++ 백준/Silver/24954. 물약 구매/물약 구매.py
@@ -1,26 +1,50 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
-def solution():
-    n = int(input()) # 물약 <=10
-    costs = list(map(int, input().split())) # 가격 <=1000
-    discount = [[] for _ in range(n)] # discount[i] = (a, d) 포션i를 사면 포션a를 d만큼 할인해준다.
-    for i in range(n):
-        m = int(input()) # 할인 정보 <=n-1
-        for _ in range(m):
-            a, d = map(int, input().split()) # 물약번호, 할인되는 가격
-            discount[i].append((a-1, d)) # (potion index, discount)
+sys.setrecursionlimit(10000)
 
-    min_cost = float('inf')
-    for perm in permutations(range(n), n):
-        cost = 0
-        new_costs = costs[:]
-        for p in perm:
-            cost += new_costs[p]
-            if cost > min_cost:
-                break
-            for idx, discnt in discount[p]:
-                new_costs[idx] = max(1, new_costs[idx]-discnt)
-        min_cost = min(min_cost, cost)
-    print(min_cost)
+def solution():
+    n = int(input())
+    base_cost = list(map(int, input().split()))
+    discount = [[] for _ in range(n)]
+
+    for i in range(n):
+        p = int(input())
+        for _ in range(p):
+            a, d = map(int, input().split())
+            discount[i].append((a - 1, d))
+
+    memo = {}
+
+    def dfs(bought, cur_costs):
+        # bought: frozenset(이미 산 물약 번호들)
+        # cur_costs: 현재 시점의 물약 가격 리스트
+        if len(bought) == n:
+            return 0  # 모든 물약 구매 완료
+
+        key = (bought, tuple(cur_costs))
+        if key in memo:
+            return memo[key]
+
+        res = float('inf')
+
+        for i in range(n):
+            if i in bought:
+                continue
+
+            # i번 물약 구매
+            new_cost = cur_costs[i]
+
+            # 현재 물약 구매 후 할인 적용
+            next_costs = cur_costs[:]
+            for (a, d) in discount[i]:
+                next_costs[a] = max(1, next_costs[a] - d)
+
+            new_bought = bought | {i}
+            res = min(res, new_cost + dfs(new_bought, next_costs))
+
+        memo[key] = res
+        return res
+
+    print(dfs(frozenset(), base_cost))
+
 solution()
