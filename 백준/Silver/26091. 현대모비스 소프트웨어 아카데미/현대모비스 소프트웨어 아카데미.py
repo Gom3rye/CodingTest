8c92978a43961223848a09a46822db4505b17170
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-06 13:38:16 +0900

    [Silver I] Title: 현대모비스 소프트웨어 아카데미, Time: 84 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Silver/26091. 현대모비스 소프트웨어 아카데미/현대모비스 소프트웨어 아카데미.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+def solution():
+    n, m = map(int, input().split()) # 학회원 수<=100,000, 최소 능력치<=10^9
+    powers = sorted(map(int, input().split()))
+    # 견학 보낼 수 있는 최대 팀 수 출력 (팀원은 두 명, 팀의 능력치가 m이상)
+    if n == 1:
+        print(0)
+        return
+    # greedy + two pointer (최대 팀을 만드려면 제일 작은 거+큰거로 해야 하고 n이 크니까 two pointer)
+    # 3 3 5 5 6 7
+    start, end = 0, n-1
+    cnt = 0
+    while start < end:
+        if powers[start] + powers[end] >= m:
+            cnt += 1
+            start += 1
+            end -= 1
+        if powers[start] + powers[end] < m:
+            start += 1
+    print(cnt)
+solution()
