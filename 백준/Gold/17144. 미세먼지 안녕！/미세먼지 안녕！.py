17517ee59dd006dd49bc534fd374860f180f5ffc
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-26 14:55:22 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 2360 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,70 +1,70 @@
 import sys
-R,C,T = map(int, sys.stdin.readline().split())
-pan = [list(map(int, sys.stdin.readline().split())) for _ in range(R)]
-# 미세먼지 확산시키는 함수 동시에 일어나야 함
-# 공기청정기 돌리는 함수
-# T번 돌리고 미세먼지 총량 출력
-cur = []
-cleaner = []
-def spread():
-    while cur:
-        x,y,value = cur.pop()
-        count = 0
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx<0 or ny<0 or nx>=R or ny>=C:
-                continue
-            if (nx,ny) in cleaner:
-                continue
-            pan[nx][ny]+=value//5
-            count+=1
-        pan[x][y] -= (value//5)*count
-def clean(a,b):
-    # 위에 회전
-    x, y = a, 1
-    index = 1 # 우부터 시작 우 상 좌 하
-    temp = 0 # 공기청정기에서 나오는 바람
-    while True:
-        nx = x+dx[index]
-        ny = y+dy[index]
-        if nx==R or ny == C or nx==-1 or ny==-1: # 벽에 닿았을 때
-            index = (index-1)%4
-            continue
-        if x==a and y==0: # 공기청정기로 다시 돌아옴
-            break
-        pan[x][y], temp = temp, pan[x][y] # swap
-        x,y = nx, ny
-    # 아래 회전
-    x, y = b, 1
-    index = 1 # 우부터 시작 우 하 좌 상
-    temp = 0 # 공기청정기에서 나오는 바람
-    while True:
-        nx = x+dx[index]
-        ny = y+dy[index]
-        if nx==R or ny == C or nx==-1 or ny==-1: # 벽에 닿았을 때
-            index = (index+1)%4
-            continue
-        if x==b and y==0: # 공기청정기로 다시 돌아옴
-            break
-        pan[x][y], temp = temp, pan[x][y] # swap
-        x,y = nx, ny
+input = sys.stdin.readline
+def solution():
+    r, c, t = map(int, input().split()) # x, y, 몇 초 후
+    board, cleaner = [], []
+    for i in range(r):
+        row = list(map(int, input().split()))
+        board.append(row)
+        if row[0] == -1:
+            cleaner.append(i)
+    directions = [(0,1),(0,-1),(1,0),(-1,0)]
+    def spread(): # 동시에 확산해야 하므로 temp 만들기
+        temp = [[0]*c for _ in range(r)]
+        for x in range(r):
+            for y in range(c):
+                # 미세먼지일 때
+                if board[x][y] > 0:
+                    cnt = 0
+                    for dx, dy in directions:
+                        nx, ny = dx+x, dy+y
+                        if 0<=nx<r and 0<=ny<c and board[nx][ny] != -1:
+                            cnt += 1
+                            temp[nx][ny] += board[x][y]//5
+                    temp[x][y] += board[x][y]-board[x][y]//5*cnt
+        # temp를 이용해 board 업데이트 시키기
+        for x in range(r):
+            for y in range(c):
+                if board[x][y] != -1:
+                    board[x][y] = temp[x][y]
+    def purify():
+        up, down = cleaner
+        # 위쪽 반시계 방향부터 반대로 shifting 해주기
+        # 1. 아래 -> 위
+        for row in range(up-2, -1, -1):
+            board[row+1][0] = board[row][0]
+        # 2. 왼 -> 오
+        for col in range(1, c):
+            board[0][col-1] = board[0][col]
+        # 3. 위 -> 아래
+        for row in range(1, up+1):
+            board[row-1][c-1] = board[row][c-1]
+        # 4. 오 -> 왼
+        for col in range(c-2, 0, -1):
+            board[up][col+1] = board[up][col]
+        # 새로운 공기 생성
+        board[up][1] = 0
 
-for i,line in enumerate(pan):
-        for j, value in enumerate(line):
-            if value>0:
-                cur.append((i,j,value))
-            if value==-1:
-                cleaner.append((i,j))
-dx = [-1,0,1,0] # 상 우 하 좌
-dy = [0,1,0,-1]           
-a = cleaner[0][0] # 공기청정기의 윗부분
-b = cleaner[1][0] # 공기청정기의 아랫부분
-for t in range(T):
-    spread()
-    clean(a,b)
-    for i,line in enumerate(pan):
-        for j, value in enumerate(line):
-            if value>0:
-                cur.append((i,j,value))
-print(sum(cur[i][2] for i in range(len(cur))))
+        # 아래쪽 시계 방향 반대로 shifting
+        # 1. 위 -> 아래
+        for row in range(down+2, r):
+            board[row-1][0] = board[row][0]
+        # 2. 왼 -> 오
+        for col in range(1, c):
+            board[r-1][col-1] = board[r-1][col]
+        # 3. 아래 -> 위
+        for row in range(r-2, down-1, -1):
+            board[row+1][c-1] = board[row][c-1] # c-1: 0based index
+        # 4. 오 -> 왼
+        for col in range(c-2, 0, -1):
+            board[down][col+1] = board[down][col]
+        # 새로운 깨끗한 공기 생성
+        board[down][1] = 0
+
+    for times in range(t):
+        spread()
+        purify()
+
+    answer = sum(sum(row) for row in board)+2 # 공기청정기 -2 해준거 상쇄
+    print(answer)
+solution()
