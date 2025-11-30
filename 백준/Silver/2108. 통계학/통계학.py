b8767a38eb16b835125812160d7d2d920ca5d9e2
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-11-30 18:23:09 +0900

    [Silver II] Title: 통계학, Time: 320 ms, Memory: 55236 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,35 +1,18 @@
 import sys
-input=sys.stdin.readline
-
-n=int(input())
-arr=[]
-
-for i in range(n):
-    arr.append(int(input()))
-
-arr.sort()#중앙값을 구하기 위해 정렬
-
-print(round(sum(arr)/len(arr)))#1) 산술평균
-print(arr[len(arr)//2])#2) 중앙값
-
-#최빈값
-dic=dict()
-for i in arr:#빈도수 구하기
-    if i in dic:
-        dic[i]+=1
-    else:
-        dic[i]=1
-        
-mx=max(dic.values())#빈도수 중 최대값 구하기
-mx_dic=[]#최빈값 숫자를 저장할 배열
-
-for i in dic:#빈도수 딕셔너리에서
-    if mx==dic[i]:#최빈값의 key저장
-        mx_dic.append(i)
-
-if len(mx_dic)>1:#최빈값이 여러개라면
-    print(mx_dic[1])#두번째로 작은 값  3)최빈값
-else:#하나라면
-    print(mx_dic[0])#해당 값 출력  3)최빈값
-    
-print(max(arr)-min(arr))#4) 범위
+from collections import Counter
+input = sys.stdin.readline
+def solution():
+    n = int(input())
+    nums = sorted(int(input()) for _ in range(n))
+    mean = round(sum(nums)/n) # round로 반올림 해주기
+    print(mean)
+    median = nums[n//2]
+    print(median)
+    counter = Counter(nums).most_common()
+    mode_cnt = counter[0][1]
+    # print(counter)
+    mode = counter[1][0] if ((len(counter) > 1) and (counter[1][1] == mode_cnt)) else counter[0][0]
+    print(mode)
+    nums_range = nums[-1]-nums[0]
+    print(nums_range)
+solution()
