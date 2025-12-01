f4f3661824b7b63553a8dc0ef23feca1c7a85184
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-02 07:21:47 +0900

    [Gold III] Title: 중량제한, Time: 204 ms, Memory: 49576 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,46 +1,33 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+def solution():
+    # 한 번의 이동에서 옮길 수 있는 물품들의 중량의 최댓값을 구하기
+    n, m = map(int, input().split()) # 섬<=10000 다리<=100,000
+    edges = []
+    for _ in range(m):
+        a, b, c = map(int, input().split()) # c<=1,000,000,000
+        edges.append((c, a, b))
+    edges.sort(reverse=True) # Max-min이니까 내림차순 정렬
+    start, end = map(int, input().split()) # 공장 시작, 끝
+    parents = list(range(n+1))
+    def find(x):
+        if parents[x] != x:
+            parents[x] = find(parents[x])
+        return parents[x]
+    def union(a, b):
+        a = find(a)
+        b = find(b)
+        if a < b:
+            parents[b] = a
+        else:
+            parents[a] = b
 
-N, M = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-max_w = 0
-
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-    graph[b].append((a, c))
-    max_w = max(max_w, c)
-
-start, end = map(int, input().split())
-
-
-def can_go(limit):
-    visited = [False]*(N+1)
-    q = deque([start])
-    visited[start] = True
-
-    while q:
-        now = q.popleft()
-
-        for nxt, w in graph[now]:
-            if not visited[nxt] and w >= limit:
-                visited[nxt] = True
-                q.append(nxt)
-
-    return visited[end]
-
-
-low, high = 1, max_w
-answer = 0
-
-while low <= high:
-    mid = (low + high) // 2
-
-    if can_go(mid):
-        answer = mid
-        low = mid + 1
-    else:
-        high = mid - 1
-
-print(answer)
+    # 목표: start와 end가 언제 연결되느냐만 중요
+    for cost, a, b in edges:
+        if find(a) != find(b): # 사이클 방지 & 불필요한 union 호출 줄여서 성능 향상
+            union(a, b) # 이미 같은 집합이면 아무 변화 없고(parents 갱신 안됨), 달라야만 집합이 합쳐진다.
+            # 두 공장이 연결되는 순간 멈추기
+            if find(start) == find(end):
+                print(cost)
+                break  
+solution()
