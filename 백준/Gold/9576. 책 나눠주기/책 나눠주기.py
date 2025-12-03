aaa50f1e91186d66b40692891f072f9ac5fe48f1
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-03 15:57:51 +0900

    [Gold II] Title: 책 나눠주기, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
@@ -2,23 +2,23 @@ import sys
 input = sys.stdin.readline
 def solution():
     t = int(input())
-    def find(x):
-        if x >= n or parent[x] == x:
-            return parent[x]
-        parent[x] = find(parent[x])
-        return parent[x]
-
     for _ in range(t):
-        n, m = map(int, input().split()) # n개의 책, m명에게 나눠줌
-        parent = list(range(n+1)) # 각 책의 대표 부모 초기화
-        cnt = 0 # 책을 준 학생 수
-        infos = list(tuple(map(int, input().split())) for _ in range(m))
-        # 최대한 많은 학생들에게 책을 주기 위해 범위가 좁은 것부터 해결
+        n, m = map(int, input().split()) # 책번호 <=1000, 인원수 <=1000
+        infos = [list(map(int, input().split())) for _ in range(m)]
+        # 최대한 많은 학생들을 주기 위해 upper limit이 가장 적은 순으로 나열
         infos.sort(key=lambda x: x[1])
+        parents = list(range(n+2)) # 관리해야 할 노드만큼 만들기
+        def find(x): # x이하의 사용가능한 가장 작은 책 찾기
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+        
+        cnt = 0
         for a, b in infos:
-            aparent = find(a) # a부터 가능한 가장 작은 책 번호 찾기
-            if aparent <= b:
-                parent[aparent] += 1 # aparent책은 사용됐으므로 다음 책으로 대표 부모를 옮김
-                cnt += 1
+            assign = find(a)
+            if assign > b : # 요청한 구간보다 큰 책은 할당할 수 없음
+                continue
+            cnt += 1
+            parents[assign] += 1
         print(cnt)
 solution()
