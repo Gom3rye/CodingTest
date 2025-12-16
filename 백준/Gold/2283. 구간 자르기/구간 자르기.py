58da40786db52653973af8a0af6e3eb22ccedf32
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-16 10:06:10 +0900

    [Gold II] Title: 구간 자르기, Time: 264 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.py
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.py
@@ -4,19 +4,17 @@ def solution():
     n, k = map(int, input().split()) # 구간 <=1000, 남는 길이의 총합 <=10^9
     # 딱 k가 되도록 만드는 a, b 출력 & if not, 0 0 출력
     # 중요한 점: 양 끝점의 위치는 0 이상 1,000,000 이하의 정수이다.
-    MAX = 1000002
-    vertical = [0]*MAX
-    diff = [0]*MAX
-    for _ in range(n):
+    MAX = 1000001
+    vertical = [0]*(MAX+1)
+    for _ in range(n): # [start, end)
         start, end = map(int, input().split())
-        diff[start] += 1
-        diff[end] -= 1
-    # print(diff[:20])
+        vertical[start] += 1
+        vertical[end] -= 1 # end-1+1
+    # print(vertical[:20])
     # print()
-    vertical[0] = diff[0]
     # 양 끝점이 최대 1,000,000 니까 누적합으로
     for i in range(1, MAX):
-        vertical[i] = vertical[i-1]+diff[i]
+        vertical[i] += vertical[i-1]
     # print(vertical[:20])
 
     # 범위가 10^9니까 O(n)으로 해야 함 -> 투 포인터
@@ -27,7 +25,8 @@ def solution():
             length -= vertical[start]
             start += 1
         if length == k:
-            print(start, end+1) # [a,b) 상황이니까 end+1로 출력해서 [a,b]로 만들기
+            print(start, end+1)
             return
     print("0 0")
+
 solution()
