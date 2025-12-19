253387b4f98e2cf6c23dbded671dcb62d97805d6
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-19 09:30:59 +0900

    [Gold II] Title: 피자판매, Time: 536 ms, Memory: 168864 KB -BaekjoonHub

--- 백준/Gold/2632. 피자판매/피자판매.py
+++ 백준/Gold/2632. 피자판매/피자판매.py
@@ -1,28 +1,37 @@
 import sys
-from collections import defaultdict
-def input(): return sys.stdin.readline().rstrip()
+from collections import Counter
+input = sys.stdin.readline
+def solution():
+    pizza_size = int(input()) # 피자크기 <=2,000,000
+    m, n = map(int, input().split()) # a, b 피자조각 개수 <=1000
+    a = list(int(input()) for _ in range(m))
+    b = list(int(input()) for _ in range(n))
+    # a의 누적합, b의 누적합 구하기
+    def find_sum(arr):
+        n = len(arr)
+        arr2 = arr*2 # 원형에서 연속으로 이어진 구간의 합을 구해야 하니까
+        prefix = [0]
+        for x in arr2:
+            prefix.append(prefix[-1] + x) # 누적합 갱신
+        # 길이 1 ~ n-1
+        sum_list = [0]
+        for length in range(1, n):
+            for start in range(n):
+                sum_list.append(prefix[start+length] - prefix[start])
+        # 총합도 꼭 잊지 말고 포함시켜주기!
+        sum_list.append(sum(arr))
+        return sum_list
 
-def find_case(pizza, length):
-    case = defaultdict(int)
-    for i in range(length):
-        temp = pizza[i:] + pizza[:i]
-        pre = 0
-        for num in temp:
-            pre += num
-            case[pre] += 1
-    case[sum(pizza)] = 1
-    return case
+    sum_a = find_sum(a)
+    sum_b = find_sum(b)
 
-k = int(input())
-n, m = map(int,input().split())
-pizza_a = [int(input()) for _ in range(n)]
-pizza_b = [int(input()) for _ in range(m)]
+    count_a = Counter(sum_a)
+    count_b = Counter(sum_b)
 
-case1 = find_case(pizza_a, n)
-case2 = find_case(pizza_b, m)
+    answer = 0 # 총 경우의 수
+    for amount, cnt in count_a.items():
+        left = pizza_size - amount
+        answer += count_b[left]*cnt # cnt: count_a[amount]
+    print(answer)
 
-result = case1.get(k, 0) + case2.get(k, 0)
-for num in case1:
-    if k-num in case2:
-        result += case1[num] * case2[k-num]
-print(result)
+solution()
