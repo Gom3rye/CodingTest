41083ded4cf270377bbe036c0df791736cec4455
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-23 09:24:46 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 756 ms, Memory: 116428 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,28 +1,25 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 def solution():
-    directions = [(0,1),(0,-1),(1,0),(-1,0)]
-    n = int(input())
-    bamboo = [list(map(int, input().split())) for _ in range(n)]
-    # dfs로 가장 큰 덩어리 구하기 문제
-    # But, 시작점을 모르니까 bruteforce로 하되 시간 초과 나지 않기 위해서 memoization을 해야 한다.
-    dp = [[-1]*n for _ in range(n)] # dp[i][j]: (i,j)까지 가는 최대 덩어리 수
+    n = int(input()) # <=500
+    board = [list(map(int, input().split())) for _ in range(n)]
+    # 모든 칸에서 dfs를 돌려버리면 4^250000, 시간초과 -> dp로 각 칸마다 최대 4번만 비교해야 한다.
+    # dp이용 -> 시간복잡도: 4*500*500 (백만)
+    dp = [[-1]*n for _ in range(n)] # dp[i][j]: (i,j)까지 얻을 수 있는 최대 칸 수
+    
     def dfs(x, y):
-        # memoization
         if dp[x][y] != -1:
             return dp[x][y]
-        
         dp[x][y] = 1
-        for dx, dy in directions:
+        for dx, dy in [(0,1),(0,-1),(1,0),(-1,0)]:
             nx, ny = dx+x, dy+y
-            if 0<=nx<n and 0<=ny<n and bamboo[nx][ny] > bamboo[x][y]:
-                dp[x][y] = max(dp[x][y], dfs(nx,ny)+1)
-
+            if 0<=nx<n and 0<=ny<n and board[nx][ny] > board[x][y]:
+                dp[x][y] = max(dp[x][y], dfs(nx, ny)+1)
         return dp[x][y]
-    max_count = 0
+    
     for i in range(n):
         for j in range(n):
-            max_count = max(max_count, dfs(i,j))
-    print(max_count)
+            dfs(i,j)
+    print(max(max(row) for row in dp))
 solution()
