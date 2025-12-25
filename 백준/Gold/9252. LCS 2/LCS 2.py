0de2877ba1ae23d632cf136c36034ec22caa3c7d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-25 16:22:33 +0900

    [Gold IV] Title: LCS 2, Time: 524 ms, Memory: 420068 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,19 +1,20 @@
 import sys
 input = sys.stdin.readline
-
-A = [""] + list(input().rstrip())
-B = [""] + list(input().rstrip())
-LCS = [[""]*len(B) for _ in range(len(A))]
-
-for i in range(1, len(A)):
-    for j in range(1, len(B)):
-        if A[i] == B[j]:
-            LCS[i][j] = LCS[i-1][j-1] + A[i]
-        else:
-            if len(LCS[i-1][j]) >= len(LCS[i][j-1]):
-                LCS[i][j] = LCS[i-1][j]
+def solution():
+    s1 = input().strip() # len(s) <=100
+    s2 = input().strip()
+    n1 = len(s1)
+    n2 = len(s2)
+    dp = [['']*(n2+1) for _ in range(n1+1)]
+    for i in range(1, n1+1):
+        for j in range(1, n2+1):
+            if s1[i-1] == s2[j-1]: # 0based index
+                dp[i][j] = dp[i-1][j-1]+s1[i-1]
             else:
-                LCS[i][j] = LCS[i][j-1]
-
-result = LCS[-1][-1]
-print(len(result), result, sep="\n")
+                if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i][j-1]
+    print(len(dp[-1][-1]))
+    print(dp[-1][-1])
+solution()
