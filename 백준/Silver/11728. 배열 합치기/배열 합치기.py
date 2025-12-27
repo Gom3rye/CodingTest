77fd0260289f776a4afb7d4fb46361f9c24f1327
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2025-12-27 17:08:03 +0900

    [Silver V] Title: 배열 합치기, Time: 1384 ms, Memory: 188024 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -5,19 +5,17 @@ def solution():
     a = list(map(int, input().split())) # 이미 정렬 되어 있음
     b = list(map(int, input().split()))
     a_idx, b_idx = 0, 0
-    answer = [0]*(n+m)
-    for i in range(n+m):
+    answer = []
+    while a_idx < n and b_idx < m:
         if a[a_idx] <= b[b_idx]:
-            answer[i] = a[a_idx]
+            answer.append(a[a_idx])
             a_idx += 1
-            if a_idx == n:
-                answer[i+1:] = b[b_idx:]
-                break
         else: # a[a_idx] > b[b_idx]:
-            answer[i] = b[b_idx]
+            answer.append(b[b_idx])
             b_idx += 1
-            if b_idx == m:
-                answer[i+1:] = a[a_idx:]
-                break
+    if a_idx == n:
+        answer.extend(b[b_idx:])
+    elif b_idx == m:
+        answer.extend(a[a_idx:])
     print(*answer)
 solution()
