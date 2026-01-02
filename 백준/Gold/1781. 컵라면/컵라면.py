d6bb3d48f151a0ec4d1eb8b77a3d1345c70ee222
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-02 21:06:41 +0900

    [Gold II] Title: 컵라면, Time: 428 ms, Memory: 64576 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -1,14 +1,17 @@
 import sys, heapq
 input = sys.stdin.readline
 def solution():
-    n = int(input())
-    info = [list(map(int, input().split())) for _ in range(n)]
-    # 데드라인 안에서 받을 수 있는 최대 컵라면 수를 구하기
-    # 데드라인 순으로 정렬 한 후 하나씩 q에 넣으면서 q 길이가 데드라인보다 길어지면 제일 작은거(q의 맨 앞에 저장되어 있음) pop하기
-    info.sort()
+    # “데드라인 있는 작업 스케줄링” -> 미래 선택 때문에 과거 선택을 버려야 할 수도 있기 때문에 stack 안됨!
+    # -> 가장 손해인 것을 버리는 구조 (여기서는 작은 컵라면 수를 버리자)
+    n = int(input()) # #숙제 <=200,000
+    ramen = [tuple(map(int, input().split())) for _ in range(n)]
+    ramen.sort() # 데드라인별로 정렬
+    # 받을 수 있는 최대 컵라면 수 출력
+    # 최대 수라 dp를 생각했지만 과거에서부터 일직선으로 정보를 갱신하는 게 아니기 때문에 포기
     q = []
-    for deadline, ramen in info:
-        heapq.heappush(q, ramen)
+    for deadline, cnt in ramen:
+        # 계속 추가하다가 조건을 어기면 최악을 버리기
+        heapq.heappush(q, cnt)
         if len(q) > deadline:
             heapq.heappop(q)
     print(sum(q))
