36635fbee25de5be94beba9676096ed115ffb2fe
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-03 19:01:32 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -2,27 +2,11 @@ import sys
 input = sys.stdin.readline
 def solution():
     n = int(input()) # #사람 <=10
-    # cnt의 idx: idx+1번째 사람의 왼쪽에 자신보다 큰 사람이 있는 횟수
+    # cnt[i]: 키가 i+1인 사람이 기억한 자기보다 큰 사람이 왼쪽에 있는 횟수
     cnt = list(map(int, input().split()))
-    if sum(cnt) == 0:
-        print(*range(1, n+1))
-        return
-
-    placement = [-1]*n
-    def backtracking(idx):
-        if idx == 0:
-            print(*placement)
-            sys.exit()
-
-        for pos in range(n):
-            if placement[pos] == -1:
-                bigger = 0
-                for i in range(pos):
-                    if placement[i] != -1:
-                        bigger += 1
-                if bigger == cnt[idx-1]:
-                    placement[pos] = idx
-                    backtracking(idx-1)
-                    placement[pos] = -1
-    backtracking(n)
+    # 키가 큰 사람부터 줄을 세우면 이미 서 있는 사람들은 전부 나보다 키가 큰 사람들이다.
+    answer = []
+    for height in range(n, 0, -1):
+        answer.insert(cnt[height-1], height)
+    print(*answer)
 solution()
