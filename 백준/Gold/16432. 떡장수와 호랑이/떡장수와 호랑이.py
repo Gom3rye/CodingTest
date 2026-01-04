ce52026c86170f963a584d9d5afa33c46fe4deea
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-04 23:48:00 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 36 ms, Memory: 33592 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
@@ -1,35 +1,27 @@
-# DFS
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
-N = int(input())
-
-lst = list()
-check_dict_lst = [[False]*10 for _ in range(N)]
-
-for i in range(N):
-    temp_lst = list(map(int ,input().split()))
-    lst.append(temp_lst[1:])
-
-def dfs(day, ricecake_lst):
-    global lst
-    global check_dict_lst
-
-    if day == N:
-        for ricecake in range(1, len(ricecake_lst)):
-            print(ricecake_lst[ricecake])
-        exit(0)
-        return
-
-    for i in range(len(lst[day])):
-        if ricecake_lst[-1] == lst[day][i]:
-            continue
-        if check_dict_lst[day][lst[day][i]]:
-            continue
-        check_dict_lst[day][lst[day][i]] = True
-        ricecake_lst.append(lst[day][i])
-        dfs(day+1, ricecake_lst)
-        ricecake_lst.pop()
-    return
-dfs(0, [-1])
-print(-1)
+def solution():
+    n = int(input()) # #day <=1000 -> 완탐은 8^1000으로 말도 안됨, dp-> 1000*9 쓰자.
+    visited = [[False]*10 for _ in range(n)] # i번째 날에 t떡을 줄 수 있는지/없는지
+    tteoks = []
+    for _ in range(n):
+        temp = list(map(int, input().split()))
+        tteoks.append(temp[1:])
+    placement = [0]*n
+    def backtracking(idx, prev):
+        # 마지막까지 무사히 떡을 다 준 경우
+        if idx == n:
+            return True
+        for t in tteoks[idx]:
+            if prev != t and not visited[idx][t]:
+                placement[idx] = t
+                if backtracking(idx+1, t):
+                    return True
+                visited[idx][t] = True
+        return False
+    if backtracking(0, 0): # 0떡은 불가능한 경우이므로 더미값으로 넣어주기
+        print(*placement, sep='\n')
+    else:
+        print(-1)
+solution()
