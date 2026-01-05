a2cb6df8f75997b480deb71f009118967807ff67
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-06 00:00:00 +0900

    [Platinum V] Title: 주유소, Time: 6076 ms, Memory: 280544 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.py
+++ 백준/Platinum/13308. 주유소/주유소.py
@@ -1,30 +1,28 @@
-import sys, heapq
-from math import log2
+from collections import defaultdict
+from heapq import *
+import sys
 input = sys.stdin.readline
-def solution():
-    n, m = map(int, input().split()) # #도시 <=2500, #도로 <=4000
-    gas = [0]+list(map(int, input().split())) # 1based index, 리터당 가격은 최대 2500
-    graph = [[] for _ in range(n+1)]
-    max_gas = max(gas)
-    for _ in range(m):
-        a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-    # 상태를 확장한 다익스트라!-> dp[i][j]: i노드에 j의 최소기름가격으로 도착한 비용
-    INF = float('inf')
-    dp = [[INF]*(max_gas+1) for _ in range(n+1)]
-    q = []
-    dp[1][gas[1]] = 0
-    heapq.heappush(q, (0, 1, gas[1])) # dp[i][j], now, j
-    while q:
-        dist, now, min_price = heapq.heappop(q)
-        if dist > dp[now][min_price]:
-            continue
-        for nxt, ndist in graph[now]:
-            cost = dist+ndist*min_price
-            nmin_price = min(min_price, gas[nxt])
-            if cost < dp[nxt][nmin_price]:
-                dp[nxt][nmin_price] = cost
-                heapq.heappush(q, (cost, nxt, nmin_price))
-    print(min(dp[n]))
-solution()
+MAX = float('inf')
+MAXCOST = 2501
+N, M = map(int, input().split())
+fuel_cost = list(map(int, input().split()))
+edge_dict = defaultdict(list)
+for _ in range(M) :
+  a, b, c = map(int, input().split())
+  edge_dict[a-1].append((b-1, c))
+  edge_dict[b-1].append((a-1, c))
+
+visited = [[MAX]*MAXCOST for _ in range(N)]
+visited[0][-1] = 0
+q = [(0, MAXCOST, 0)]
+
+while q :
+  tot_cost, min_cost, node = heappop(q)
+  if node == N-1 :
+    print(tot_cost)
+    break
+  min_cost = min(min_cost, fuel_cost[node])
+  for nxt, nxt_dist in edge_dict[node] :
+    if visited[nxt][min_cost] > tot_cost + min_cost*nxt_dist :
+      visited[nxt][min_cost] = tot_cost + min_cost*nxt_dist
+      heappush(q, (tot_cost + min_cost*nxt_dist, min_cost, nxt))
