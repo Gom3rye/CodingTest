3d6b40ae97596f13f49ac88abb6b70b2c1561fe9
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-07 12:09:55 +0900

    [Silver I] Title: 회전 초밥, Time: 48 ms, Memory: 35560 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,34 +1,33 @@
 import sys
+from math import log2
 input = sys.stdin.readline
 def solution():
-    n, d, k, c = map(int, input().split()) # 접시 수, 초밥 수, 연속해서 먹는 접시 수, 쿠폰 번호
-    sushi = [int(input()) for _ in range(n)]
-    # 종류별 개수 저장
-    count = [0]*(d+1)
-    kind = 0
-    # 초기 윈도우 구성
+    n, d, k, c = map(int, input().split()) # #접시 <=30000, 초밥종류 <=3000, 윈도우 <=3000, 쿠폰번호
+    # 최대 초밥 가짓수
+    sushis = [int(input()) for _ in range(n)]
+    # 슬라이딩 윈도우 초기 구성
+    count = [0]*(d+1) # count[sushi]: 윈도우에 있는 sushi의 개수
+    count[c] += 1 # 공짜 스시 저장
+    kind = 1
     for i in range(k):
-        if count[sushi[i]] == 0:
+        if count[sushis[i]] == 0:
             kind += 1
-        count[sushi[i]] += 1
-    # max_kind 초기화
-    max_kind = kind + (1 if count[c] == 0 else 0)
+        count[sushis[i]] += 1
 
-    # 슬라이딩 윈도우
-    for start in range(1, n):
-        # 왼쪽 초밥 제거
-        left = sushi[start-1]
-        count[left] -= 1
-        if count[left] == 0:
+    # 윈도우 한칸씩 돌려가면 계산
+    max_kind = kind
+    for i in range(n-1):
+        remove = sushis[i]
+        count[remove] -= 1
+        if count[remove] == 0:
             kind -= 1
-        # 오른쪽 초밥 추가
-        right = sushi[(start+k-1)%n] # mod로 회전 처리
-        if count[right] == 0:
+        add = sushis[(i+k)%n]
+        if count[add] == 0:
             kind += 1
-        count[right] += 1
-
-        # 쿠폰 초밥 확인
-        now = kind + (1 if count[c] == 0 else 0)
-        max_kind = max(max_kind, now)
+        count[add] += 1
+        # kind 갱신
+        max_kind = max(max_kind, kind)
+        if max_kind == k+1:
+            break
     print(max_kind)
 solution()
