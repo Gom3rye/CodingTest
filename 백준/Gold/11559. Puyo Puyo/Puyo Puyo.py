2f96f781da2e392c873d40085035bf2082985ad2
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-10 16:28:23 +0900

    [Gold IV] Title: Puyo Puyo, Time: 52 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -3,53 +3,53 @@ from collections import deque
 input = sys.stdin.readline
 def solution():
     board = [list(input().strip()) for _ in range(12)]
-    # 터질 수 있는 뿌요가 여러 그룹이 있다면 동시에 터져야 하고 여러 그룹이 터지더라도 한번의 연쇄가 추가된다.
-    # 좌표들을 다 저장해서 구현해야 생각했는데 이는 더 복잡하고 그냥 열별로 아래부터 보면서 위로 올라가면 gravity 함수를 보다 쉽게 작성할 수 있다.
-    def gravity(): # 연쇄작용이 있을때 호출하는 함수
-        for col in range(6):
-            for row in range(11, -1, -1):
-                if board[row][col] != '.':
-                    color = board[row][col]
-                    board[row][col] = '.' # 추락 표시
-                    r = row
-                    while r+1 < 12 and board[r+1][col] == '.':
-                        r += 1 # 내려가기
-                    board[r][col] = color # 추락 완료
-    
+    # 터질 수 있는 뿌요가 여러 그룹이 있다면 동시에 터져야 하고 여러 그룹이 터지더라도 한번의 연쇄가 추가된다 -> 탐색/삭제 로직 []로 담아놨다가 분리하기
+    # 한 그룹인지 파악하는 함수
     def is_group(x, y):
-        q = deque([(x, y)])
-        visited[x][y] = True
         color = board[x][y]
-        disappear = [(x, y)] # 사라질 예정인 것들
+        visited[x][y] = True
+        group = [(x, y)]
+        q = deque([(x, y)])
         while q:
             x, y = q.popleft()
             for nx, ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
-                if 0<=nx<12 and 0<=ny<6 and not visited[nx][ny]:
-                    if board[nx][ny] == color:
-                        visited[nx][ny] = True
-                        disappear.append((nx, ny))
-                        q.append((nx, ny))
-        if len(disappear) >= 4:
-            return disappear
-        return None
-        
-    answer = 0 # 총 연쇄 횟수
-    while True: # 연쇄반응이 없을때까지 반복
-        total_disappear = []
-        visited = [[False]*6 for _ in range(12)]
+                if 0<=nx<12 and 0<=ny<6 and not visited[nx][ny] and board[nx][ny] == color:
+                    visited[nx][ny] = True
+                    group.append((nx, ny))
+                    q.append((nx, ny))
+        return group
+    
+    def gravity():
+        # 열 별로 아래 행부터 검사
+        for col in range(6):
+            fall = deque()
+            for row in range(11, -1, -1):
+                if board[row][col] != '.':
+                    fall.append(board[row][col]) # 색깔들만 모아서
+            # 재배치
+            for row in range(11, -1, -1):
+                if fall:
+                    board[row][col] = fall.popleft() # 왼쪽에서 꺼냄!
+                else:
+                    board[row][col] = '.'
+                    
+    time = 0
+    while True: # 한 사이클: 보드 한 번 훑어서 연쇄작용 몇 번 있는지 파악하기
+        visited = [[False]*6 for _ in range(12)] # 한 사이클이 끝나면 새 보드가 탄생하니까 이에 맞춰 방문처리 초기화
+        disappear = []
         for i in range(12):
             for j in range(6):
-                if board[i][j] != '.':
-                    disappear = is_group(i, j)
-                    if disappear == None:
-                        continue
-                    total_disappear.extend(disappear)
-        if total_disappear:
-            answer += 1
-            for x, y in total_disappear:
-                board[x][y] = '.'
+                if board[i][j] != '.' and not visited[i][j]:
+                    group = is_group(i, j)
+                    if len(group) >= 4:
+                        disappear.extend(group)
+        if disappear:
+            time += 1 # 연쇄 작용 1번 추가
+            for x, y in disappear:
+                board[x][y] = '.' # 터짐
+                # 내려가기 로직
             gravity()
         else:
             break
-    print(answer)
+    print(time)
 solution()
