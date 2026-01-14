ba649e9455b51e2f30760a08886baf2184ebf55b
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-14 23:52:57 +0900

    [Gold IV] Title: 우유 도시, Time: 380 ms, Memory: 57592 KB -BaekjoonHub

--- 백준/Gold/14722. 우유 도시/우유 도시.py
+++ 백준/Gold/14722. 우유 도시/우유 도시.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
 def solution():
-    n = int(input())
+    n = int(input()) # 영역 크기 <=1000
     board = [list(map(int, input().split())) for _ in range(n)]
-    # 딸->초->바->딸 (0->1->2->0)
-    dp = [[0]*(n+1) for _ in range(n+1)] # dp[i][j]: (i-1, j-1)까지 마신 최대 개수
-    
+    # 0:딸기, 1:초코, 2:바나나, 0>1>2>0>1>2... 반복
+    # (0,0)~(n-1,n-1)까지 가는데 마실 수 있는 최대 우유 개수 구하기
+    dp = [[0]*(n+1) for _ in range(n+1)] # 오른쪽, 아래쪽으로만 갈 수 있다.
+    # 다음에 마실 우유 = (마신 개수 % 3)
     for i in range(1, n+1):
         for j in range(1, n+1):
-            # dp[i][j]는 왼쪽에서 온 최대 개수, 위에서 온 최대 개수 중 더 큰 것 (안 먹었을 경우)
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-            # 먹었을 경우
-            if dp[i][j]%3 == board[i-1][j-1]: # board는 0based index
+            # 안 마시고 가는 경우
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            # 마시는 경우
+            if (dp[i][j]%3) == board[i-1][j-1]:
                 dp[i][j] += 1
     print(dp[-1][-1])
 solution()
