e869beb5518f1060bc09f8fdcf4483d5691b63b2
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-17 11:27:36 +0900

    [Gold IV] Title: 수 묶기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,47 +1,34 @@
 import sys
-
+input = sys.stdin.readline
 def solution():
-    input = sys.stdin.read().split()
-    if not input: return
-    n = int(input[0])
-    nums = list(map(int, input[1:]))
-
-    pos = []  # 1보다 큰 양수
-    neg = []  # 0을 포함한 음수
-    ones = 0  # 1의 개수 (1은 무조건 따로 더함)
-
-    for num in nums:
-        if num > 1:
-            pos.append(num)
-        elif num <= 0:
-            neg.append(num)
-        else:
+    # 수열을 적절히 묶어서 그 합이 최대가 되도록 하는 프로그램
+    n = int(input()) # 수열의 크기 <=50
+    arr = list(int(input()) for _ in range(n))
+    # 1,음수(0),양수로 나누기
+    ones = 0 # zeros는 왜 없어도 되냐면 0은 더해도 결과에 아무 영향을 주지 않기 때문
+    positive, negative = [], []
+    for num in arr:
+        if num == 1: # 더하기가 무조건 낫다.
             ones += 1
-
-    # 양수는 큰 것끼리 곱해야 하므로 내림차순 정렬
-    pos.sort(reverse=True)
-    # 음수는 절대값이 큰 것(작은 수)끼리 곱해야 하므로 오름차순 정렬
-    neg.sort()
-
-    max_sum = 0
-
-    # 1. 양수 처리
-    for i in range(0, len(pos), 2):
-        if i + 1 < len(pos):
-            max_sum += (pos[i] * pos[i+1])
+        elif num <= 0:
+            negative.append(num)
+        else: # num > 0
+            positive.append(num)
+    positive.sort(reverse=True)
+    negative.sort()
+    max_result = 0
+    pn = len(positive)
+    for i in range(0, pn, 2): # 0~positive끝까지 2의 간격으로 탐색
+        if i+1 < pn:
+            max_result += positive[i]*positive[i+1]
+        else: # 끝에 홀로 남은 마지막 원소
+            max_result += positive[i]
+    nn = len(negative)
+    for i in range(0, nn, 2):
+        if i+1 < nn:
+            max_result += negative[i]*negative[i+1]
         else:
-            max_sum += pos[i]
-
-    # 2. 음수 처리 (0 포함)
-    for i in range(0, len(neg), 2):
-        if i + 1 < len(neg):
-            max_sum += (neg[i] * neg[i+1])
-        else:
-            max_sum += neg[i]
-
-    # 3. 1 처리
-    max_sum += ones
-
-    print(max_sum)
-
+            max_result += negative[i]
+    max_result += ones
+    print(max_result)
 solution()
