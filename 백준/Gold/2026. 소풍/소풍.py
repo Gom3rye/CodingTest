c041bbce7a71da9640b1503b44fb4ce2b7edae1c
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-18 13:38:14 +0900

    [Gold II] Title: 소풍, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2026. 소풍/소풍.py
+++ 백준/Gold/2026. 소풍/소풍.py
@@ -3,20 +3,14 @@ input = sys.stdin.readline
 def solution():
     k, n, f = map(int, input().split()) # #소풍갈 학생 <=62, #학생 <=900, #친구관계 <=5600
     # 평균 차수 계산: 2*간선//정점 -> 여기선 12.44정도로 작음, 백트레킹에서는 평균 차수가 분기 수를 결정하기 때문에 백트레킹 가능
-    friend = [[] for _ in range(n+1)]
+    friend = [set() for _ in range(n+1)]
     degree = [0]*(n+1)
     for _ in range(f):
         a, b = map(int, input().split())
-        friend[a].append(b)
-        friend[b].append(a) # 양방향
+        friend[a].add(b)
+        friend[b].add(a) # 양방향
         degree[a] += 1 # 차수가 k-1개보다 작으면 소풍갈 학생이 될 수 없다.
         degree[b] += 1
-
-    def is_valid(nxt):
-        for mem in picnic:
-            if not nxt in friend[mem]:
-                return False
-        return True
 
     picnic = []
     def backtracking(now):
@@ -24,15 +18,17 @@ def solution():
             print(*picnic, sep='\n')
             sys.exit()
 
-        for nxt in range(now+1, n+1):
+        for nxt in range(now, n+1):
             if degree[nxt] < k-1:
                 continue
-            if is_valid(nxt):
+            # if len(picnic)+(n-nxt+1) < k: # 남은 학생 다 합쳐도 k명이 안 되면 가지치기
+            #     return
+            if all(nxt in friend[mem] for mem in picnic):
                 picnic.append(nxt)
                 backtracking(nxt)
                 picnic.pop()
                         
     # backtracking으로 소풍갈 친구들의 "조합" 구하기!
-    backtracking(0)
+    backtracking(1)
     print(-1)
 solution()
