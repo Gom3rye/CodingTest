8facbe0910e56328a9a3dfb49048839d10f4b0a7
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-19 15:33:03 +0900

    [Gold I] Title: 창업, Time: 212 ms, Memory: 42880 KB -BaekjoonHub

--- 백준/Gold/16890. 창업/창업.py
+++ 백준/Gold/16890. 창업/창업.py
@@ -6,8 +6,8 @@ def solution():
     a_temp = list(input().strip())
     n = len(a_temp)
     # 각자 사용할 문자 남기기
-    a = deque(sorted(ord(ch) for ch in a_temp)[:(n+1)//2]) # 최대 맨 앞 (n+1)//2개만 필요하다.
-    b = deque(sorted([ord(ch) for ch in input().strip()], reverse=True)[:n//2]) # 맨 앞 n//2개만 필요
+    a = deque(sorted(ch for ch in a_temp)[:(n+1)//2]) # 최대 맨 앞 (n+1)//2개만 필요하다.
+    b = deque(sorted([ch for ch in input().strip()], reverse=True)[:n//2]) # 맨 앞 n//2개만 필요
     # 맨앞, 맨뒤를 표시하기 위한 포인터 생성
     left, right = 0, n-1
     answer = ['']*n
@@ -15,21 +15,21 @@ def solution():
         if i%2==0: # a차례일때
             # a의 가장 작은 수가 b의 가장 큰 수보다 작은 경우
             if b and a[0] < b[0]:
-                answer[left] = chr(a.popleft())
+                answer[left] = a.popleft()
                 left += 1
             # a의 가장 작은 수가 b의 가장 큰 수보다 크거나 같은 경우
             else:
                 # a의 가장 큰수를 맨 뒤에 배치 (어차피 모든 a의 수가 b보다 작을 일은 없으므로)
-                answer[right] = chr(a.pop())
+                answer[right] = a.pop()
                 right -= 1
         else:
             # b의 가장 큰 수가 a의 가장 작은 수보다 큰 경우
             if a and b[0] > a[0]:
-                answer[left] = chr(b.popleft())
+                answer[left] = b.popleft()
                 left += 1
             # b의 가장 큰 수가 a의 가장 작은 수보다 작거나 같은 경우
             else:
-                answer[right] = chr(b.pop()) # b의 가장 작은 수를 맨 끝에 놓기
+                answer[right] = b.pop() # b의 가장 작은 수를 맨 끝에 놓기
                 right -=1
     print(''.join(answer))
 solution()
