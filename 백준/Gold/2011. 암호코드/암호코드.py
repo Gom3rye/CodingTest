ea828492f6a23b0a37d8acb43d19d37eeabc182b
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-21 20:40:41 +0900

    [Gold V] Title: 암호코드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
 def solution():
-    MOD = 1000000
-    secret = input().strip() # 5000자리 이하의 암호
-    if secret[0] == '0': # 암호가 잘못된 경우
+    s = '-1'+input().strip() # 1based index로 만들어주기 위해
+    if s[1] == '0': # 암호를 해독할 수 없는 경우
         print(0)
         return
-    n = len(secret)
-    dp = [0]*(n+1) # dp[i]: i번째 암호를 완성하는 경우
-    dp[0] = 1 # 아무 글자도 없는 경우 1 초기화
-    dp[1] = 1
-    for i in range(2, n+1):
-        num1 = int(secret[i-1])
-        num2 = int(secret[i-2:i])
-        if 1<=num1<10:
+    n = len(s)
+    MOD = 1000000
+    dp = [0]*n
+    dp[1] = 1 # 첫 번째 알파벳 경우는 1
+    for i in range(2, n):
+        if 1<=int(s[i])<=9:
             dp[i] += dp[i-1]
-        if 10<=num2<27:
+        if 10<=int(s[i-1:i+1])<=26:
             dp[i] += dp[i-2]
-        dp[i]%=MOD
+        dp[i] %= MOD
     print(dp[-1])
 solution()
