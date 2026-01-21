de705171f48b5eacaa8f008e0c6579be335041e9
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-21 19:50:50 +0900

    [Gold V] Title: 암호코드, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,30 +1,22 @@
 import sys
 input = sys.stdin.readline
-
 def solution():
-    MOD = 1_000_000
-    s = input().strip()
-    n = len(s)
-
-    if s[0] == '0':
+    MOD = 1000000
+    secret = input().strip() # 5000자리 이하의 암호
+    if secret[0] == '0': # 암호가 잘못된 경우
         print(0)
         return
-
-    dp = [0] * (n + 1)
-    dp[0] = 1  # 빈 문자열은 해석 1가지
-    dp[1] = 1  # 첫 글자가 0이 아닌 경우
-
-    for i in range(2, n + 1):
-        one = int(s[i-1])       # 한 글자
-        two = int(s[i-2:i])     # 두 글자
-
-        if 1 <= one <= 9:
+    n = len(secret)
+    dp = [0]*(n+1) # dp[i]: i번째 암호를 완성하는 경우
+    dp[0] = 1 # 아무 글자도 없는 경우 1 초기화
+    dp[1] = 1
+    for i in range(2, n+1):
+        num1 = int(secret[i-1])
+        num2 = int(secret[i-2:i])
+        if 1<=num1<10:
             dp[i] += dp[i-1]
-
-        if 10 <= two <= 26:
+        if 10<=num2<27:
             dp[i] += dp[i-2]
-
-        dp[i] %= MOD
-
-    print(dp[n])
+        dp[i]%=MOD
+    print(dp[-1])
 solution()
