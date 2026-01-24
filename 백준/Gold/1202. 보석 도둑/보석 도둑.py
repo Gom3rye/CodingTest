81be5c93a55bdff07c973d79f02d19c3e674552c
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-24 11:39:08 +0900

    [Gold II] Title: 보석 도둑, Time: 1256 ms, Memory: 118916 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,22 +1,20 @@
 import sys, heapq
 input = sys.stdin.readline
 def solution():
-    n, k = map(int, input().split()) # 보석 개수, 가방 개수
-    # m, v (무게, 가격)
-    jewelry = [list(map(int, input().split())) for _ in range(n)]
-    bags = [int(input()) for _ in range(k)]
-    jewelry.sort() # 무게 기준 오름차순
-    bags.sort() # 용량 기준 오름차순
-    result = 0
-    q = [] # 가격 저장용 최대 힙
-    j = 0 # 보석 인덱스
+    n, k = map(int, input().split()) # 보석 개수, 가방 개수 <=300,000
+    # 훔칠 수 있는 보석의 최대 가격 구하기-> 
+    jewels = [list(map(int, input().split())) for _ in range(n)] # 무게, 가격
+    jewels.sort()
+    bags = sorted(int(input()) for _ in range(k))
+    # 최대 가격을 담아야 하니까 maxheap 사용해서 큰 후보자들 담아놓고 배정하기
+    q = []
+    idx = 0
+    answer = 0
     for bag in bags:
-        # 현재 가방에 넣을 수 있는 보석을 힙에 추가
-        while j < n and jewelry[j][0] <= bag:
-            heapq.heappush(q, -jewelry[j][1]) # 최대 가격을 구해야 하니까 max_heap을 이용 -> 음수로 저장
-            j += 1
-        # 가장 가치가 높은 보석을 담는다.
-        if q:
-            result += -heapq.heappop(q)
-    print(result)
+        while idx < n and jewels[idx][0] <= bag:
+            heapq.heappush(q, -jewels[idx][1])
+            idx += 1
+        if q: # 후보가 있다면 가방에 할당
+            answer += -heapq.heappop(q)
+    print(answer)
 solution()
