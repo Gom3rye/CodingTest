db2a1f13cfb03de5d56ac58d9b7bee56f3923b4d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-25 12:54:17 +0900

    [Gold I] Title: 수영장 만들기, Time: 44 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -1,42 +1,27 @@
 import sys, heapq
 input = sys.stdin.readline
 def solution():
-    N, M = map(int, input().split())
-    board = [list(map(int, list(input().strip()))) for _ in range(N)]
-    
-    visited = [[False] * M for _ in range(N)]
-    pq = []
-    
-    # 1. 모든 가장자리 칸을 우선순위 큐에 삽입
-    for i in range(N):
-        for j in range(M):
-            if i == 0 or i == N-1 or j == 0 or j == M-1:
-                # (높이, 행, 열) 순서로 삽입
-                heapq.heappush(pq, (board[i][j], i, j))
+    n, m = map(int, input().split()) # <=50
+    board = [list(map(int, input().strip())) for _ in range(n)]
+    # 자신보다 작은 칸을 만나면 총 양에 더하는 방식! (물 빼내며 관리)
+    visited = [[False]*m for _ in range(n)]
+    q = []
+    # 물이 고일 수 없는 모든 가장자리 칸들을 q에 넣고 자신보다 낮은 칸을 만나면 그 기준을 q에 넣으며 탐색
+    total = 0
+    for i in range(n):
+        for j in range(m):
+            if i == 0 or i == n-1 or j == 0 or j == m-1:
+                heapq.heappush(q, (board[i][j], i, j))
                 visited[i][j] = True
-                
-    total_water = 0
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
-    
-    # 2. 가장 낮은 벽부터 꺼내어 인접한 안쪽 칸 탐색
-    while pq:
-        h, r, c = heapq.heappop(pq)
-        
-        for i in range(4):
-            nr, nc = r + dr[i], c + dc[i]
-            
-            if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
-                visited[nr][nc] = True
-                
-                # 인접한 안쪽 칸의 높이가 현재 벽(h)보다 낮다면 물이 고임
-                if board[nr][nc] < h:
-                    total_water += (h - board[nr][nc])
-                    # 물이 고인 후의 높이를 새로운 벽의 높이로 간주
-                    heapq.heappush(pq, (h, nr, nc))
-                else:
-                    # 안쪽 칸이 더 높다면 그 칸 자체가 새로운 벽이 됨
-                    heapq.heappush(pq, (board[nr][nc], nr, nc))
-                    
-    print(total_water)
+    while q:
+        h, x, y = heapq.heappop(q)
+        for nx, ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                visited[nx][ny] = True # 방문 처리 해놓고
+                if board[nx][ny] < h: # 작으면 물이 고일 수 있음
+                    total += (h-board[nx][ny])
+                    heapq.heappush(q, (h, nx, ny))
+                else: # 같거나 크면 고일 수 없음
+                    heapq.heappush(q, (board[nx][ny], nx, ny))
+    print(total)
 solution()
