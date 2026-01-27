6146548dd8bae1a8e685603dfaad490f2ad18a4e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-27 23:53:33 +0900

    [Silver III] Title: 배열 복원하기, Time: 136 ms, Memory: 40044 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -0,0 +1,16 @@
+H, W, X, Y = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(H + X)]
+
+# A 배열 초기화
+A = [[0] * W for _ in range(H)]
+
+for i in range(H):
+    for j in range(W):
+        if i >= X and j >= Y:
+            A[i][j] = B[i][j] - A[i - X][j - Y]
+        else:
+            A[i][j] = B[i][j]
+
+# 출력
+for row in A:
+    print(*row)
