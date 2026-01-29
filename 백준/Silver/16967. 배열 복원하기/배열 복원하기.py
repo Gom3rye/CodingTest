81c554f90154335394ffe2e03e4728022226a4ad
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-29 17:38:49 +0900

    [Silver III] Title: 배열 복원하기, Time: 84 ms, Memory: 40048 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -1,16 +1,16 @@
-H, W, X, Y = map(int, input().split())
-B = [list(map(int, input().split())) for _ in range(H + X)]
-
-# A 배열 초기화
-A = [[0] * W for _ in range(H)]
-
-for i in range(H):
-    for j in range(W):
-        if i >= X and j >= Y:
-            A[i][j] = B[i][j] - A[i - X][j - Y]
-        else:
-            A[i][j] = B[i][j]
-
-# 출력
-for row in A:
-    print(*row)
+import sys
+input = sys.stdin.readline
+def solution():
+    h, w, x, y = map(int, input().split()) # <=300, 아래로 이동시킨 횟수, 오른쪽으로 이동시킨 횟수 <=h,w
+    b = [list(map(int, input().split())) for _ in range(h+x)]
+    a = [[0]*w for _ in range(h)]
+    # a를 구해야 하니까 a크기만큼 for문 돌리기
+    for i in range(h):
+        for j in range(w):
+            if i >= x and j >= y: # 모두에 포함되는 경우
+                a[i][j] = b[i][j]-a[i-x][j-y]
+            else:
+                a[i][j] = b[i][j]
+    for row in a:
+        print(*row)
+solution()
