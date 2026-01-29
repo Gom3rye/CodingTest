c92a0148dfb8c30184b81b0ff20423969d5ffd25
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-29 14:37:50 +0900

    [Gold V] Title: Fruit Feast, Time: 52 ms, Memory: 32800 KB -BaekjoonHub

--- 백준/Gold/11964. Fruit Feast/Fruit Feast.py
+++ 백준/Gold/11964. Fruit Feast/Fruit Feast.py
@@ -1,40 +1,29 @@
 import sys
 input = sys.stdin.readline
-
-T, A, B = map(int, input().split())
-
-# dp[i] = 물 마시기 전, 포만감 i 가능?
-dp = bytearray(T + 1)
-dp[0] = 1
-
-# 1️⃣ 물 마시기 전 DP
-for i in range(T + 1):
-    if dp[i]:
-        if i + A <= T:
-            dp[i + A] = 1
-        if i + B <= T:
-            dp[i + B] = 1
-
-# dp2[i] = 물 마신 후, 포만감 i 가능?
-dp2 = bytearray(T + 1)
-
-# 2️⃣ 물을 한 번 마신 상태 세팅
-for i in range(T + 1):
-    if dp[i]:
-        dp2[i // 2] = 1
-
-# 3️⃣ 물 마신 후 DP
-for i in range(T + 1):
-    if dp2[i]:
-        if i + A <= T:
-            dp2[i + A] = 1
-        if i + B <= T:
-            dp2[i + B] = 1
-
-# 4️⃣ 최대 포만감 찾기
-ans = 0
-for i in range(T + 1):
-    if dp[i] or dp2[i]:
-        ans = i
-
-print(ans)
+def solution():
+    t, a, b = map(int, input().split()) # maximum fullness <=5,000,000, orange increase, lemon increase <=t
+    # 물 최대 1번 마실 수 있고 물 마시면 포만감 //2 됨
+    # 무제한 냅색! -> 물 마신경우 / 안 마신경우 각각 나올 수 있는 포만감 다르니까 둘 다 구분해서 탐색
+    no_water_dp = [False]*(t+1) # if dp[i]: i의 포만감을 가질 수 있다.
+    no_water_dp[0] = True # 초기화
+    for i in range(t+1):
+        if no_water_dp[i]:
+            if i+a <= t:
+                no_water_dp[i+a] = True
+            if i+b <= t:
+                no_water_dp[i+b] = True
+    water_dp = [False]*(t+1)
+    for i in range(t+1):
+        if no_water_dp[i]:
+            water_dp[i//2] = True
+    for i in range(t+1):
+        if water_dp[i]:
+            if i+a <= t:
+                water_dp[i+a] = True
+            if i+b <= t:
+                water_dp[i+b] = True
+    for i in range(t, -1, -1):
+        if water_dp[i]:
+            print(i)
+            break
+solution()
