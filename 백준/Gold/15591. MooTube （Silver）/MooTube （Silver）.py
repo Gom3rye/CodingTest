36c67d1fcf7eb2cbd07e3dee373167a10834a87d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-29 16:33:52 +0900

    [Gold V] Title: MooTube (Silver), Time: 2524 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,27 +1,25 @@
-from collections import deque, defaultdict
-
-def bfs(k, v):
-    visited = [0] * (n + 1)
-    visited[v] = 1
-    result = 0
-    queue = deque([(v, 1e9)])
-    while queue:
-        v, usado = queue.pop()
-        for n_v, n_usado in graph[v]:
-            n_usado = min(usado, n_usado)
-            if n_usado >= k and not visited[n_v]:
-                result += 1
-                queue.append((n_v, n_usado))
-                visited[n_v] = 1
-    return result
-
-
-n, q = map(int, input().split())
-graph = defaultdict(list)
-for _ in range(n - 1):
-    a, b, w = map(int, input().split())
-    graph[a].append((b, w))
-    graph[b].append((a, w))
-for _ in range(q):
-    k, v = map(int, input().split())
-    print(bfs(k, v))
+import sys
+from collections import deque
+input = sys.stdin.readline
+def solution():
+    n, q = map(int, input().split()) # #동영상 <=5000, q <=5000
+    graph = [[] for _ in range(n+1)]
+    for _ in range(n-1):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+    for _ in range(q):
+        k, v = map(int, input().split()) # <=1,000,000,000, <=n
+        visited = [False]*(n+1)
+        visited[v] = True
+        q = deque([v])
+        cnt = 0
+        while q:
+            now = q.popleft()
+            for nxt, score in graph[now]:
+                if not visited[nxt] and score >= k:
+                    cnt += 1
+                    visited[nxt] = True
+                    q.append(nxt)
+        print(cnt)      
+solution()
