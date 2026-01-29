c187536a1c539076217fc9bfaa09644b51bbf332
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-29 23:28:37 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
@@ -1,21 +1,17 @@
 import sys
 input = sys.stdin.readline
-
 def solution():
-    N, M, L, K = map(int, input().split())
-    stars = [tuple(map(int, input().split())) for _ in range(K)]
-
-    max_hit = 0
-
-    # 트램펄린의 왼쪽 아래 좌표를 별 좌표 기준으로만 잡는다
-    for x, _ in stars:
-        for _, y in stars:
+    # 최대한 많은 별똥별을 튕겨내도록 배치했을 때 지구에 떨어지는 별똥별 수
+    m, n, l, k = map(int, input().split()) # col, row <=500,000, 트램펄린 길이 <=100,000, #별똥별 <=100
+    # 별똥별의 개수가 최대 100으로 충분히 작으므로 완탐!
+    stars = [list(map(int, input().split())) for _ in range(k)]
+    max_cnt = 0
+    for sx, _ in stars:
+        for _, sy in stars:
             cnt = 0
-            for sx, sy in stars:
-                if x <= sx <= x + L and y <= sy <= y + L:
+            for x, y in stars:
+                if sx<=x<=sx+l and sy<=y<=sy+l:
                     cnt += 1
-            max_hit = max(max_hit, cnt)
-
-    print(K - max_hit)
-
+            max_cnt = max(max_cnt, cnt)
+    print(k-max_cnt)
 solution()
