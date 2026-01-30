36d42c625c5fcfaefb8a63bd1f26be4167ea1c11
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-30 19:05:30 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 3072 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
@@ -1,72 +1,54 @@
 import sys
 from collections import deque
-
-def solve():
-    input_data = sys.stdin.read().split()
-    ptr = 0
-    
-    while ptr < len(input_data):
-        n = int(input_data[ptr])
-        ptr += 1
-        if n == 0: break
-        
-        rooms = []
+input = sys.stdin.readline
+def solution():
+    while True:
+        n = int(input()) # #방 <=1000
+        if n == 0:
+            break
+        # 필요한 변수: 방이름, 요구하는 금액, 이동 가능 방번호
+        # 이음새=> 인덱스! 몇 번째 방이고 그 방에 대한 정보를 인덱스로 관리할 수 있다!
+        rooms = ['']*n
+        money = [0]*n
+        adj = [] # 0based index로
         for i in range(n):
-            type_char = input_data[ptr]
-            amount = int(input_data[ptr+1])
-            ptr += 2
-            
-            edges = []
-            while True:
-                e = int(input_data[ptr])
-                ptr += 1
-                if e == 0: break
-                edges.append(e)
-            rooms.append((type_char, amount, edges))
-            
-        # dist[i]는 i번 방에 도달했을 때의 최대 소지금
-        dist = [-1] * (n + 1)
-        
-        # 1번 방 초기화
-        type1, amt1, _ = rooms[0]
-        start_money = 0
-        if type1 == 'L':
-            start_money = amt1
-        elif type1 == 'T':
-            if amt1 > 0: # 시작부터 트롤에게 낼 돈이 없으면 불가능
-                print("No")
-                continue
-        
-        dist[1] = start_money
-        queue = deque([1])
-        
-        possible = False
-        while queue:
-            curr = queue.popleft()
-            curr_money = dist[curr]
-            
-            if curr == n:
-                possible = True
+            data = list(input().split()[:-1])
+            rooms[i] = data[0]
+            money[i] = int(data[1])
+            adj.append([int(r)-1 for r in data[2:]]) # 0based index
+        start_coin = 0
+        if rooms[0] == 'L':
+            start_coin = money[0]
+        elif rooms[0] == 'T' and money[0] > 0:
+            print("No")
+            continue
+        # 재방문 막기 위한 중복처리 관리: 방 번호별이 아니라 금액별! 금액이 작아도 다른 방 한 번더 가서 금액 충전해올 수 있기 때문
+        q = deque([(0, start_coin)]) # 0번째 방에서 0원을 가지고 n-1방에 갈 수 있는지 없는지 탐색
+        max_money = [-1]*n # 현재 coin이 max_money[방번호]보다 많을 때만 전진
+        max_money[0] = start_coin
+        while q:
+            now, coin = q.popleft()
+            if now == n-1:
+                print("Yes")
                 break
-                
-            for next_room in rooms[curr-1][2]:
-                ntype, namt, _ = rooms[next_room-1]
-                nmoney = curr_money
-                
-                if ntype == 'L':
-                    nmoney = max(curr_money, namt)
-                elif ntype == 'T':
-                    if curr_money >= namt:
-                        nmoney = curr_money - namt
+            for nxt in adj[now]:
+                # L인 경우-> money[nxt]로 채우거나 패쓰
+                if rooms[nxt] == 'L':
+                    ncoin = max(coin, money[nxt])
+                # T인 경우-> 빼기
+                elif rooms[nxt] == 'T':
+                    if coin >= money[nxt]:
+                        ncoin = coin-money[nxt]
                     else:
-                        continue # 돈 부족으로 못 감
-                
-                # 기존에 이 방에 왔을 때보다 돈을 더 많이 들고 올 수 있다면 갱신
-                if nmoney > dist[next_room]:
-                    dist[next_room] = nmoney
-                    queue.append(next_room)
-                    
-        print("Yes" if possible else "No")
+                        continue # 이동 불가능이므로 cut
+                # E인 경우-> 값 그대로
+                else:
+                    ncoin = coin
+                # max_money 갱신
+                if ncoin > max_money[nxt]:
+                    max_money[nxt] = ncoin
+                    q.append((nxt, ncoin))
+        else:
+            print("No")
 
-if __name__ == "__main__":
-    solve()
+solution()
