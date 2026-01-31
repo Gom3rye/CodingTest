10317e48891ff093d267b350b5115557b4008c8e
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-31 14:28:15 +0900

    [Silver V] Title: Ski Course Design, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9881. Ski Course Design/Ski Course Design.py
+++ 백준/Silver/9881. Ski Course Design/Ski Course Design.py
@@ -1,21 +1,23 @@
-N = int(input())
-hills = [int(input()) for _ in range(N)]
-
-INF = float('inf')
-answer = INF
-
-# 가능한 최소 높이 L을 전부 시도
-for L in range(0, 84):  # L + 17 <= 100
-    cost = 0
-    low = L
-    high = L + 17
-    
-    for h in hills:
-        if h < low:
-            cost += (low - h) ** 2
-        elif h > high:
-            cost += (h - high) ** 2
-    
-    answer = min(answer, cost)
-
-print(answer)
+import sys
+input = sys.stdin.readline
+def solution():
+    n = int(input()) # #hills <=1000
+    hills = [int(input()) for _ in range(n)] # range(0, 101)
+    min_hill, max_hill = min(hills), max(hills)
+    if max_hill-min_hill <= 17:
+        print(0)
+        return
+    # 최종적으로 모든 언덕 높이가 어떤 [h, h+17]안에 들어가야 함
+    # 가능한 높이가 최대 100까지로 충분히 작으므로 h로 완탐돌리기!
+    min_cost = float('inf')
+    for low in range(0, 84):
+        cost = 0
+        high = low+17
+        for hill in hills:
+            if hill < low:
+                cost += (low-hill)**2
+            elif hill > high:
+                cost += (hill-high)**2
+        min_cost = min(min_cost, cost)
+    print(min_cost)
+solution()
