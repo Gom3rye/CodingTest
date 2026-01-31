eeffd188afe2f082a116bf5525c0d5a032a31540
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-31 19:33:07 +0900

    [Gold III] Title: 좋은 수, Time: 1268 ms, Memory: 41632 KB -BaekjoonHub

--- 백준/Gold/5624. 좋은 수/좋은 수.py
+++ 백준/Gold/5624. 좋은 수/좋은 수.py
@@ -1,18 +1,19 @@
 import sys
 input = sys.stdin.readline
-
-N = int(input())
-arr = list(map(int,input().split()))
-dp = [0]*400003
-tmp = 200001
-
-answer = 0
-for i in range(N):
-    for j in range(i):
-        if dp[arr[i] - arr[j]+tmp]==1:
-            answer+=1
-            break
-    for k in range(i+1):
-        dp[arr[i]+arr[k]+tmp] = 1
-    
-print(answer)
+def solution():
+    n = int(input()) # <=5000
+    a = list(map(int, input().split()))
+    # a+b+c = a[d] -> a+b = a[d]-c
+    two_sum = set()
+    cnt = 0
+    for i in range(n):
+        # i번째 수가 좋은 수인지 확인
+        for j in range(i):
+            if a[i]-a[j] in two_sum:
+                cnt += 1
+                break # 좋은 수 판단 완료
+        # two_sum 갱신 (a[i]를 사용해서)
+        for k in range(i+1): # 같은 수 여러번 더해도 되니까 i+1까지해서 i*2도 가능하도록!
+            two_sum.add(a[i]+a[k])
+    print(cnt)
+solution()
