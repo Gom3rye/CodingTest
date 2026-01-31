3ddb275bd4fc682679601aaab8b091c83ad39771
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-01-31 11:52:02 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 80 ms, Memory: 33924 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 def solution():
     while True:
@@ -8,47 +8,44 @@ def solution():
             break
         # 필요한 변수: 방이름, 요구하는 금액, 이동 가능 방번호
         # 이음새=> 인덱스! 몇 번째 방이고 그 방에 대한 정보를 인덱스로 관리할 수 있다!
-        rooms = ['']*n
-        money = [0]*n
-        adj = [] # 0based index로
+        rooms = []
         for i in range(n):
             data = list(input().split()[:-1])
-            rooms[i] = data[0]
-            money[i] = int(data[1])
-            adj.append([int(r)-1 for r in data[2:]]) # 0based index
-        start_coin = 0
-        if rooms[0] == 'L':
-            start_coin = money[0]
-        elif rooms[0] == 'T' and money[0] > 0:
-            print("No")
-            continue
-        # 재방문 막기 위한 중복처리 관리: 방 번호별이 아니라 금액별! 금액이 작아도 다른 방 한 번더 가서 금액 충전해올 수 있기 때문
-        q = deque([(0, start_coin)]) # 0번째 방에서 0원을 가지고 n-1방에 갈 수 있는지 없는지 탐색
-        max_money = [-1]*n # 현재 coin이 max_money[방번호]보다 많을 때만 전진
-        max_money[0] = start_coin
-        while q:
-            now, coin = q.popleft()
-            if now == n-1:
-                print("Yes")
-                break
-            for nxt in adj[now]:
-                # L인 경우-> money[nxt]로 채우거나 패쓰
-                if rooms[nxt] == 'L':
-                    ncoin = max(coin, money[nxt])
-                # T인 경우-> 빼기
-                elif rooms[nxt] == 'T':
-                    if coin >= money[nxt]:
-                        ncoin = coin-money[nxt]
-                    else:
-                        continue # 이동 불가능이므로 cut
-                # E인 경우-> 값 그대로
-                else:
-                    ncoin = coin
-                # max_money 갱신
-                if ncoin > max_money[nxt]:
-                    max_money[nxt] = ncoin
-                    q.append((nxt, ncoin))
+            room = data[0]
+            money = int(data[1])
+            adj = [int(r)-1 for r in data[2:]] # 0based index
+            rooms.append((room, money, adj))
+
+        visited = [False]*n
+        visited[0] = True
+        def dfs(idx, coin):
+            room, money, adj = rooms[idx]
+            
+            if room == 'L':
+                ncoin = max(money, coin)
+            elif room == 'T':
+                if coin < money:
+                    return False # 이 방에서 출발하면 안됨
+                ncoin = coin-money
+            else:
+                ncoin = coin # E방이면 코인 변화 없음
+            
+            # n-1번방에 도착하면 성공
+            if idx == n-1:
+                return True
+            
+            for nxt in adj:
+                if not visited[nxt]:
+                    visited[nxt] = True
+                    if dfs(nxt, ncoin):
+                        return True
+                    visited[nxt] = False
+
+            return False
+
+        # 0번방에 들어가기 전에 0원을 가지고 있다.  
+        if dfs(0, 0): # dfs는 ‘방에 들어가기 직전 상태’를 인자로 받기 때문에 시작을 항상 (1, 0)으로 해도 첫 방의 효과가 자동으로 반영된다.
+            print("Yes")
         else:
             print("No")
-
 solution()
