9dfeb306a3f44f3ffa54717d34f8dfe24bcc7c23
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-02 23:49:14 +0900

    [Gold III] Title: 두 도로, Time: 2580 ms, Memory: 47204 KB -BaekjoonHub

--- 백준/Gold/26125. 두 도로/두 도로.py
+++ 백준/Gold/26125. 두 도로/두 도로.py
@@ -19,6 +19,7 @@ def solution():
     floyd()
     # 간선이 2개만 추가되는 상황이므로 추가된 간선에 따라 최단 경로가 될 수 있는 경우의 수를 모두 체크하자.
     # 전체 floyd 다 돌리면 q가 최대 100,000으로 시간초과
+    answer = []
     for _ in range(q):
         a1, b1, c1, a2, b2, c2 = map(int, input().split())
         # 출발과 도착이 같은 경우도 있다고 했으니 이때 거리가 0으로 잘 들어가게 해야 한다.
@@ -37,7 +38,7 @@ def solution():
         one_two = graph[s][a1]+c1+graph[b1][a2]+c2+graph[b2][t]
         # 5. 1,2도로 다 쓰되, 2->1순서인 경우
         two_one = graph[s][a2]+c2+graph[b2][a1]+c1+graph[b1][t]
-        answer = min(origin,one,two,one_two,two_one)
-        print(answer if answer < INF else -1)
-
+        ans = min(origin,one,two,one_two,two_one)
+        answer.append(ans if ans < INF else -1) # for문 안에서 출력해버리면 q가 최대 십만이기 때문에 시간 많이 느려질 수 있다.
+    print('\n'.join(map(str, answer)))
 solution()
