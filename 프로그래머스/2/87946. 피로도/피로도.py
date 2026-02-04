8c0ec88cfcb7d6d174772477315143788a456b5d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-04 20:36:30 +0900

    [level 2] Title: 피로도, Time: 40.84 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,15 @@
 import sys
-from itertools import permutations
 input = sys.stdin.readline
 def solution(k, dungeons):
-    # 최대 던전의 수가 8개이므로 완탐 가능! 8! = 40320
+    # 최대 던전의 수가 8개이므로 백트레킹 가능(순열)
     n = len(dungeons)
-    answer = -1
-    for perm in permutations(range(n)):
-        cnt = 0
-        hp = k
-        for p in perm:
-            at_least, consumption = dungeons[p]
-            if hp >= at_least:
-                hp -= consumption
-                cnt += 1
-        answer = max(answer, cnt)
-    return answer
+    visited = [False]*n
+    def backtracking(hp, cnt):
+        max_cnt = cnt
+        for i in range(n):
+            if not visited[i] and dungeons[i][0] <= hp:
+                visited[i] = True
+                max_cnt = max(max_cnt, backtracking(hp-dungeons[i][1], cnt+1))
+                visited[i] = False
+        return max_cnt
+    return backtracking(k, 0)
