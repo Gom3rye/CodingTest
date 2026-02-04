901668dc7d3901ef85b2d247ced2f95a25e46e02
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-04 20:38:54 +0900

    [level 2] Title: 피로도, Time: 42.18 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -4,12 +4,14 @@ def solution(k, dungeons):
     # 최대 던전의 수가 8개이므로 백트레킹 가능(순열)
     n = len(dungeons)
     visited = [False]*n
+    max_cnt = 0
     def backtracking(hp, cnt):
-        max_cnt = cnt
+        nonlocal max_cnt
+        max_cnt = max(max_cnt, cnt)
         for i in range(n):
             if not visited[i] and dungeons[i][0] <= hp:
                 visited[i] = True
-                max_cnt = max(max_cnt, backtracking(hp-dungeons[i][1], cnt+1))
+                backtracking(hp-dungeons[i][1], cnt+1)
                 visited[i] = False
-        return max_cnt
-    return backtracking(k, 0)
+    backtracking(k, 0)     
+    return max_cnt
