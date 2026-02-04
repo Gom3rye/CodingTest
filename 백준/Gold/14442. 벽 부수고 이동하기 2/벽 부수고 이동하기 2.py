3df6c36549c16c01d00fd0a65e5e98d49f928ef9
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-04 14:55:59 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 5456 ms, Memory: 470192 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -5,21 +5,22 @@ INF = float('inf')
 def solution():
     n, m, k = map(int, input().split()) # <=1000, <=1000, <=10
     board = [list(map(int, input().strip())) for _ in range(n)]
-    q = deque([(0,0,0)])
-    distance = [[[INF]*(k+1) for _ in range(m)] for _ in range(n)]
-    distance[0][0][0] = 1
+    q = deque([(0,0,0,1)])
+    visited = [[[False]*(k+1) for _ in range(m)] for _ in range(n)]
+    visited[0][0][0] = True
     while q:
-        x, y, cnt = q.popleft()
-        if (x, y) == (n-1, m-1):
-            print(distance[x][y][cnt])
+        x, y, cnt, dist = q.popleft()
+        if (x, y) == (n-1, m-1): # 가장 먼저 도착점에 도착한 길이가 최단 거리다!
+            print(dist)
             return
         for nx, ny in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
             if 0<=nx<n and 0<=ny<m:
-                if board[nx][ny] == 0 and distance[nx][ny][cnt] == INF:
-                    distance[nx][ny][cnt] = distance[x][y][cnt]+1
-                    q.append((nx, ny, cnt))
-                elif board[nx][ny] == 1 and cnt+1 <= k and distance[nx][ny][cnt+1] == INF:
-                    distance[nx][ny][cnt+1] = distance[x][y][cnt]+1
-                    q.append((nx, ny, cnt+1))
+                # 각각 0인 경우와 1인 경우 나눠서 탐색해야 한다. (각각 상태가 달라지니까)
+                if board[nx][ny] == 0 and not visited[nx][ny][cnt]:
+                    visited[nx][ny][cnt] = True
+                    q.append((nx, ny, cnt, dist+1))
+                elif board[nx][ny] == 1 and cnt+1 <= k and not visited[nx][ny][cnt+1]:
+                    visited[nx][ny][cnt+1] = True
+                    q.append((nx, ny, cnt+1, dist+1))
     print(-1)
 solution()
