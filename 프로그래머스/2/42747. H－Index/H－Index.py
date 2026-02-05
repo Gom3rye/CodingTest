3ce3c61926e17ff6b22c706f9dfac9e160f0024a
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-05 21:54:57 +0900

    [level 2] Title: H-Index, Time: 0.10 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,18 @@
+import sys
+from bisect import bisect_right, bisect_left
+def solution(citations):
+    citations.sort() # len(citations) <=1000
+    n = len(citations)
+    # parametric search로 h찾기!
+    answer, start, end = 0, 0, 10000
+    while start <= end:
+        mid = (start+end)//2 # h의 최댓값
+        less, more = bisect_right(citations, mid), n-bisect_left(citations, mid)
+        if more >= mid and less <= mid:
+            answer = mid
+            start = mid+1
+        elif more < mid:
+            end = mid-1
+        elif less > mid:
+            start = mid+1
+    return answer
