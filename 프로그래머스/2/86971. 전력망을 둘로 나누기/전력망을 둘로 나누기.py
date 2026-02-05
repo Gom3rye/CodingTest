d1001c41c00f1ec3004fb1b33816b615d05dd07d
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-05 10:41:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.07 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,31 +1,25 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-def bfs(start, banned, n, graph):
-    q = deque([start])
-    visited = [False]*(n+1)
-    visited[start] = True
-    size = 1 # start 한개 가지고 있음(자기자신)
-    while q:
-        now = q.popleft()
-        for nxt in graph[now]:
-            # 막힌 간선인지 체크
-            if (nxt, now) == banned or (now, nxt) == banned:
-                continue
-            if not visited[nxt]:
-                size += 1
-                visited[nxt] = True
-                q.append(nxt)
-    return size
-                
 def solution(n, wires):
     answer = 10**9
     # 트리 만들어주기
     graph = [[] for _ in range(n+1)]
+    visited = [False]*(n+1)
     for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-    for a, b in wires:
-        size = bfs(a, (a,b), n, graph) # a를 시작 노드로 하고 (a,b) 간선을 막는 경우
-        answer = min(answer, abs(n-2*size))
+    visited[1] = True
+    def dfs(start):
+        nonlocal answer
+        size = 1 # 자기자신
+        for nxt in graph[start]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                subtree = dfs(nxt) # nxt를 루트로 가지고 있는 subtree의 size
+                # diff값으로 answer 계산
+                answer = min(answer, abs(n-2*subtree))
+                size += subtree
+        return size
+    dfs(1)
     return answer
