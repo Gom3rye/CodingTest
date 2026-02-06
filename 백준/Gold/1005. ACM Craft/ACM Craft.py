9b325c1b84c033ea33a362321afde45e7d891ed1
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-07 07:35:13 +0900

    [Gold III] Title: ACM Craft, Time: 484 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,32 +1,25 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
+from collections import deque
 def solution():
     t = int(input())
     for _ in range(t):
         n, k = map(int, input().split())
-        build = [0]+list(map(int, input().split()))
+        build = [-1]+list(map(int, input().split())) # 1based index
         graph = [[] for _ in range(n+1)]
         for _ in range(k):
-            pre, next = map(int, input().split())
-            graph[next].append(pre) # pre를 다 끝내야먄 next 할 수 있다.
-        w = int(input()) # 세워야 하는 건물
-        dp = [-1] *(n+1) # dp[i]: i번 건물을 세우는데 드는 최소 시간 (0도 될 수 있으니까 -1로 초기화 하자)
-        def dfs(next):
-
-            if dp[next] != -1: # 이미 구한 것은 또 구하지 않기 위해
-                return dp[next]
-            
-            dp[next] = build[next]
-            
-            for pre in graph[next]:
-                if dp[pre] == -1:
-                    dfs(pre)
-                # print(f"next: {next}, pre: {pre}") 
-                dp[next] = max(dp[next], dp[pre]+build[next])
-                    
-                # print(f"dp[{next}]:{dp[next]}, dp[{pre}]: {dp[pre]}")                   
-
-        dfs(w)
-        print(dp[w])
+            pre, nxt = map(int, input().split())
+            graph[nxt].append(pre)
+        w = int(input())
+        result = [-1]*(n+1) # dp
+        def dfs(x):
+            if result[x] != -1:
+                return result[x]
+            max_time = 0
+            for pre in graph[x]:
+                max_time = max(max_time, dfs(pre))
+            result[x] = max_time+build[x]
+            return result[x]
+        print(dfs(w))
 solution()
