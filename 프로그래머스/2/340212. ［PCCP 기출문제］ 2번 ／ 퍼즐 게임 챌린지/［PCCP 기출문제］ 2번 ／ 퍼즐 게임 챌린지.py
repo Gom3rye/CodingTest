006117e57898f22b406e4aa6ad92085bcd1db573
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-06 13:38:22 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 969.46 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,16 +1,16 @@
 from math import log2
 def solution(diffs, times, limit):
-    answer, start, end = -1, 1, 10**15 # answer: level의 최솟값
-    # print(300000*log2(10**15)) -> 약 1500만으로 가능
+    answer, start, end = -1, 1, max(diffs) # answer: level의 최솟값
+    # print(300000*log2(100000)) -> 약 500만으로 가능
     while start <= end:
         mid = (start+end)//2 # level 후보값
         now = 0 # 현재 시간
         time_prev = 0
         for idx, diff in enumerate(diffs):
             time_cur = times[idx]
-            if diff <= mid:
+            if diff <= mid: # 따라서 mid의 최대값은 max(diffs)
                 now += time_cur
-            else: # diff < mid:
+            else: # diff > mid:
                 now += (((diff-mid)*(time_cur+time_prev))+time_cur)
             time_prev = time_cur
         
