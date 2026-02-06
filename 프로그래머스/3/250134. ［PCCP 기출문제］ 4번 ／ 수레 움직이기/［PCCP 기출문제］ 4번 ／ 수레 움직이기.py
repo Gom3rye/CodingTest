f73b817e38be0fc327689d30cbcba8391b164469
Gom3rye <88931238+Gom3rye@users.noreply.github.com>
2026-02-06 21:35:16 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 0.70 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
@@ -1,72 +1,58 @@
-import sys
-sys.setrecursionlimit(10**6)
-INF = float('inf')
+from collections import deque
 def solution(maze):
-    answer = INF # 최소 횟수
-    # R/B가 도착하면 이제 거긴 못 지나감
     n, m = len(maze), len(maze[0])
-    # 1->3, 2->4로 가야 한다.
     red_visited = [[False]*m for _ in range(n)]
     blue_visited = [[False]*m for _ in range(n)]
-    srx = sry = sbx = sby = -1 # 시작점 초기화
-    erx = ery = ebx = eby = -1 # 도착점 초기화
     for i in range(n):
         for j in range(m):
             if maze[i][j] == 1:
-                red_visited[i][j] = True
                 srx, sry = i, j
+                red_visited[i][j] = True
             elif maze[i][j] == 2:
-                blue_visited[i][j] = True
                 sbx, sby = i, j
+                blue_visited[i][j] = True
             elif maze[i][j] == 3:
                 erx, ery = i, j
             elif maze[i][j] == 4:
                 ebx, eby = i, j
-                
-    def backtracking(rx, ry, bx, by, time): # 전 rx, ry, bx, by, 시간
-        nonlocal answer
-        # 가지치기
-        if time >= answer:
-            return
-        
-        # 둘다 도착하면 answer 갱신
+    q = deque([((srx, sry, sbx, sby), red_visited, blue_visited, 0)])
+    visited = set()
+    visited.add((srx, sry, sbx, sby))
+    while q:
+        (rx, ry, bx, by), red_visited, blue_visited, time = q.popleft()
         red_arrived = (rx, ry) == (erx, ery)
         blue_arrived = (bx, by) == (ebx, eby)
         if red_arrived and blue_arrived:
-            answer = min(answer, time)
-            return
-        
-        # r가 갈 수 있는 후보 선정
+            return time
+        # r/b중 도착한 수레가 있다면 그 수레는 항상 그 자리를 지켜야 한다.
         red_candidates = [(rx, ry)] if red_arrived else []
         if not red_arrived:
             for nx, ny in [(rx+1,ry),(rx-1,ry),(rx,ry+1),(rx,ry-1)]:
-                if 0<=nx<n and 0<=ny<m and not red_visited[nx][ny] and maze[nx][ny] != 5:
+                if 0<=nx<n and 0<=ny<m and maze[nx][ny] != 5 and not red_visited[nx][ny]:
                     red_candidates.append((nx, ny))
-        # b가 갈 수 있는 후보 선정
         blue_candidates = [(bx, by)] if blue_arrived else []
         if not blue_arrived:
             for nx, ny in [(bx+1,by),(bx-1,by),(bx,by+1),(bx,by-1)]:
-                if 0<=nx<n and 0<=ny<m and not blue_visited[nx][ny] and maze[nx][ny] != 5:
+                if 0<=nx<n and 0<=ny<m and maze[nx][ny] != 5 and not blue_visited[nx][ny]:
                     blue_candidates.append((nx, ny))
-        
+                    
         for nrx, nry in red_candidates:
             for nbx, nby in blue_candidates:
                 # 동시에 두 수레를 같은 칸으로 움직일 수 없다.
                 if (nrx, nry) == (nbx, nby):
                     continue
-                # 수레끼리 자리 swap할 수 없다.
+                # 수레끼리 자리를 바꾸면 안된다.
                 if (nrx, nry) == (bx, by) and (nbx, nby) == (rx, ry):
                     continue
-                
-                # 방문 처리
-                red_visited[nrx][nry] = True
-                blue_visited[nbx][nby] = True
-                
-                backtracking(nrx, nry, nbx, nby, time+1)
-                
-                # 상태 복구
-                red_visited[nrx][nry] = False
-                blue_visited[nbx][nby] = False
-        
-    backtracking(srx, sry, sbx, sby, 0)
-    return answer if answer != INF else 0
+                # 새로운 방문배열
+                nred_visited = [row[:] for row in red_visited]
+                nblue_visited = [row[:] for row in blue_visited]
+                nred_visited[nrx][nry] = True
+                nblue_visited[nbx][nby] = True
+                # 중복 방문 안된다.
+                state = (nrx, nry, nbx, nby)
+                if state not in visited:
+                    visited.add(state)
+                    q.append((state, nred_visited, nblue_visited, time+1))
+    return 0 # 도달할 수 없는 경우
+    
